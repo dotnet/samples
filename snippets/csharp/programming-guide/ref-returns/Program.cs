0a41c229761d89b63ec119270e32f6ffc72e3595
Bill Wagner <bill.w.wagner@outlook.com>
2018-04-03 18:27:01 -0400

    add ref local reassignment
    And build project around the sample.

---
+++ snippets/csharp/programming-guide/ref-returns/Program.cs
@@ -0,0 +1,18 @@
+// #error version
+
+namespace UseRefReturns
+{
+    using static System.Console;
+    using RefReturns;
+    using RefReturnsVersionTwo;
+
+    public class Example
+    {   
+       static void Main(string[] args)
+       {
+            FirstExample.Tests();
+            WriteLine();
+            SecondExample.Tests();
+       }
+    }
+}
