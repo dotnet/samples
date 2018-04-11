bd817d84e10dee0197b70b02cf8077be75d280a2
Bill Wagner <wiwagn@microsoft.com>
2018-04-11 14:10:06 -0400

    add ref local reassignment (#7)
    * add ref local reassignment
    
    And build project around the sample.
    
    * update code sample
    
    The newer version shows a sample where the ref local reassignment feature creates more efficient code.
    
    * feedback
    
    Use expression bodied members for ToString.
    
    * remove the error pragma comment

---
+++ snippets/csharp/programming-guide/ref-returns/Program.cs
@@ -0,0 +1,16 @@
+namespace UseRefReturns
+{
+    using static System.Console;
+    using RefReturns;
+    using RefReturnsVersionTwo;
+
+    public class Example
+    {   
+       static void Main(string[] args)
+       {
+            FirstExample.Tests();
+            WriteLine();
+            SecondExample.Tests();
+       }
+    }
+}
