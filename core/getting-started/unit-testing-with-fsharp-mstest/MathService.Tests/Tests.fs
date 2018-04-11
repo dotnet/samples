43e1fb66aedc6d5e11dea163fbf6c45016518028
Juraj Plavcan <juraj.plavcan@live.com>
2018-04-11 09:16:01 -0600

    Renamed sumOfSquares() to squaresOfOdds() in f# unittest samples to match updates in docs (#10)

--- core/getting-started/unit-testing-with-fsharp-mstest/MathService.Tests/Tests.fs
+++ core/getting-started/unit-testing-with-fsharp-mstest/MathService.Tests/Tests.fs
@@ -17,17 +17,17 @@ type TestClass () =
     [<TestMethod>]
     member this.TestEvenSequence() = 
         let expected = Seq.empty<int> |> Seq.toList
-        let actual = MyMath.sumOfSquares [2; 4; 6; 8; 10]
+        let actual = MyMath.squaresOfOdds [2; 4; 6; 8; 10]
         Assert.AreEqual(expected, actual)
 
     [<TestMethod>]
-    member public this.SumOnesAndEvens() =
+    member public this.TestOnesAndEvens() =
         let expected = [1; 1; 1; 1]
-        let actual = MyMath.sumOfSquares [2; 1; 4; 1; 6; 1; 8; 1; 10]
+        let actual = MyMath.squaresOfOdds [2; 1; 4; 1; 6; 1; 8; 1; 10]
         Assert.AreEqual(expected, actual)
 
     [<TestMethod>]
     member public this.TestSquaresOfOdds() =
         let expected = [1; 9; 25; 49; 81]
-        let actual = MyMath.sumOfSquares [1; 2; 3; 4; 5; 6; 7; 8; 9; 10]
+        let actual = MyMath.squaresOfOdds [1; 2; 3; 4; 5; 6; 7; 8; 9; 10]
         Assert.AreEqual(expected, actual)
