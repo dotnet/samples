cc723856bb6e18dd1c3ac7e7dfe8241d442d4d16
Mackiovello <elandroe@icloud.com>
2018-04-11 21:19:27 +0200

    Update TryParse sample for readability
    Use an array instead of repeating code.
    Change variable name from "result" to "success" to be more semantic
    Use null coalescing operator instead of null check

--- snippets/csharp/VS_Snippets_CLR_System/system.Byte.TryParse/cs/TryParse.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.Byte.TryParse/cs/TryParse.cs
@@ -5,57 +5,41 @@ public class ByteConversion
 {
    public static void Main()
    {
-      string byteString = null;
-      CallTryParse(byteString);
-      
-      byteString = String.Empty;
-      CallTryParse(byteString);
-      
-      byteString = "1024";
-      CallTryParse(byteString);
-      
-      byteString = "100.1";
-      CallTryParse(byteString);
-      
-      byteString = "100";
-      CallTryParse(byteString);
-      
-      byteString = "+100";
-      CallTryParse(byteString);
-      
-      byteString = "-100";
-      CallTryParse(byteString);
-      
-      byteString = "000000000000000100";
-      CallTryParse(byteString);
-      
-      byteString = "00,100";
-      CallTryParse(byteString);
-      
-      byteString = "   20   ";
-      CallTryParse(byteString);
-      
-      byteString = "FF";
-      CallTryParse(byteString);
-      
-      byteString = "0x1F";
-      CallTryParse(byteString);
+      var byteStrings = new string[] 
+      {
+          null,
+          string.Empty,
+          "1024",
+          "100.1",
+          "100",
+          "+100",
+          "-100",
+          "000000000000000100",
+          "00,100",
+          "   20   ",
+          "FF",
+          "0x1F"
+      };
+
+      foreach (var byteString in byteStrings)
+      {
+          CallTryParse(byteString);
+      }
    }
 
    private static void CallTryParse(string stringToConvert)
    {  
       byte byteValue; 
-      bool result = Byte.TryParse(stringToConvert, out byteValue);
-      if (result)
+      bool success = Byte.TryParse(stringToConvert, out byteValue);
+      if (success)
       {
          Console.WriteLine("Converted '{0}' to {1}", 
                         stringToConvert, byteValue);
       }
       else
       {
-         if (stringToConvert == null) stringToConvert = "";
          Console.WriteLine("Attempted conversion of '{0}' failed.", 
-                           stringToConvert.ToString());
+                           stringToConvert ?? "");
       }
    }    
 }
