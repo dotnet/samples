0348bff13c1e4ed2bb2c458607d1c89b779a3e4b
Bill Wagner <bill.w.wagner@outlook.com>
2018-04-25 17:42:03 -0400

    add samples for attribute usage.

---
+++ snippets/csharp/attributes/NewPropertyOrFieldAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace attributes
+{
+    // <Snippet1>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    class NewPropertyOrFieldAttribute : Attribute { }
+    // </Snippet1>
+}
