1c160111016a7d9beab521067e91d7be2580324e
Bill Wagner <bill.w.wagner@outlook.com>
2018-04-27 10:53:00 -0400

    port old serialization samples
    This is now a console app, because .NET Core support.

---
+++ csharp/serialization/Loan.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace serialization
+{
+    [Serializable()]
+    // <Snippet1>
+   public class Loan : INotifyPropertyChanged
+    {
+        public double LoanAmount { get; set; }
+        public double InterestRate { get; set; }
+        public int Term { get; set; }
+
+        private string customer;
+        public string Customer
+        {
+            get { return customer; }
+            set
+            {
+                customer = value;
+                PropertyChanged(this,
+                  new PropertyChangedEventArgs(nameof(Customer)));
+            }
+        }
+
+        [field: NonSerialized()]
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        public Loan(double loanAmount,
+                    double interestRate,
+                    int term,
+                    string customer)
+        {
+            this.LoanAmount = loanAmount;
+            this.InterestRate = interestRate;
+            this.Term = term;
+            this.customer = customer;
+        }
+
+    }
+    // </Snippet1>
+}
---
+++ csharp/serialization/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // <Snippet3?
+            const string FileName = @"../../../SavedLoan.bin";
+            // </Snippet3>
+
+            // <Snippet2>
+            Loan TestLoan = new Loan(10000.0, 0.075, 36, "Neil Black");
+            // </Snippet2>
+
+            if (File.Exists(FileName))
+            {
+                Console.WriteLine("Reading saved file");
+                Stream openFileStream = File.OpenRead(FileName);
+                BinaryFormatter deserializer = new BinaryFormatter();
+                TestLoan = (Loan)deserializer.Deserialize(openFileStream);
+                openFileStream.Close();
+            }
+            TestLoan.PropertyChanged += (_, __) => Console.WriteLine("New customer value");
+
+            TestLoan.Customer = "Henry Clay";
+            TestLoan.Term--;
+            Console.WriteLine(TestLoan.Term);
+            Stream SaveFileStream = File.Create(FileName);
+            BinaryFormatter serializer = new BinaryFormatter();
+            serializer.Serialize(SaveFileStream, TestLoan);
+            SaveFileStream.Close();
+
+
+        }
+    }
+}
