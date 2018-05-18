c2389125c7842989689d97ddc3cb2f6b45986fcb
Petr Kulikov <15279990+pkulikov@users.noreply.github.com>
2018-05-18 23:23:19 +0200

    Fixed and updated the class example for the C# programming guide (#72)

--- snippets/csharp/programming-guide/classes-and-structs/class-example.cs
+++ snippets/csharp/programming-guide/classes-and-structs/class-example.cs
@@ -1,46 +1,45 @@
-public class Person
-{
-    // Field
-    private string name;
-
-    // Constructor that takes no arguments.
-    public Person()
+    public class Person
     {
-        name = "unknown";
-    }
+        // Constructor that takes no arguments:
+        public Person()
+        {
+            Name = "unknown";
+        }
 
-    // Constructor that takes one argument.
-    public Person(string nm)
+        // Constructor that takes one argument:
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        // Auto-implemented readonly property:
+        public string Name { get; }
+
+        // Method that overrides the base class (System.Object) implementation.
+        public override string ToString()
+        {
+            return Name.ToString();
+        }
+    }
+    class TestPerson
     {
-        name = nm;
+        static void Main()
+        {
+            // Call the constructor that has no parameters.
+            var person1 = new Person();
+            Console.WriteLine(person1.Name);
+
+            // Call the constructor that has one parameter.
+            var person2 = new Person("Sarah Jones");
+            Console.WriteLine(person2.Name);
+            // OR use ToString method to get string representation of the person2 instance:
+            Console.WriteLine(person2.ToString());
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
     }
-
-    // Method
-    public void SetName(string newName)
-    {
-        name = newName;
-    }
-}
-class TestPerson
-{
-    static void Main()
-    {
-        // Call the constructor that has no parameters.
-        Person person1 = new Person();
-        Console.WriteLine(person1.name);
-
-        person1.SetName("John Smith");
-        Console.WriteLine(person1.name);
-
-        // Call the constructor that has one parameter.
-        Person person2 = new Person("Sarah Jones");
-        Console.WriteLine(person2.name);
-
-        Console.WriteLine("Press any key to exit.");
-        Console.ReadKey();
-    }
-}
 // Output:
 // unknown
-// John Smith
+// Sarah Jones
 // Sarah Jones
