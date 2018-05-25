20f0247aefba9df9523c123e08af4f60761e8a20
cartermp <pcarter@fastmail.com>
2018-05-25 08:49:40 -0700

    Update record samples to meet style guide

--- snippets/fsharp/lang-ref-1/snippet1903.fs
+++ snippets/fsharp/lang-ref-1/snippet1903.fs
@@ -1,4 +1,4 @@
-type Point = { x : float; y: float; z: float; }
-type Point3D = { x: float; y: float; z: float }
+type Point = { X: float; Y: float; Z: float; }
+type Point3D = { X: float; Y: float; Z: float }
 // Ambiguity: Point or Point3D?
-let mypoint3D = { x = 1.0; y = 1.0; z = 0.0; }
+let mypoint3D = { X = 1.0; Y = 1.0; Z = 0.0; }
