df318ac0e1d0c43025fb2606cbeb3359163c84f9
Ron Petrusha <ronpet@microsoft.com>
2018-05-29 19:13:12 -0700

    Added Span<T>.Enumerator examples (#85)
    * Added Span<T>.Enumerator examples
    
    * Modified code based on feedback

---
+++ snippets/csharp/api/system/span.enumerator/nosync/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static readonly byte[] _array = new byte[5];
+
+    static void Main()
+    {
+        new Random(42).NextBytes(_array);
+        Span<byte> span = _array;
+
+        Task.Run( () => ClearContents() );
+
+       EnumerateSpan(span);
+    }
+
+    public static void ClearContents()
+    {
+        Task.Delay(20).Wait();
+        lock (_array)
+        {
+           Array.Clear(_array, 0, _array.Length);
+        }
+    }
+
+    public static void EnumerateSpan(Span<byte> span)
+    {
+        foreach (byte element in span)
+        {
+            Console.WriteLine(element);
+            Task.Delay(10).Wait();
+        }
+    }
+}
+// The example displays output like the following:
+//     62
+//     23
+//     186
+//     0
+//     0
---
+++ snippets/csharp/api/system/span.enumerator/sync/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static readonly byte[] _array = new byte[5];
+
+    static void Main()
+    {
+        new Random(42).NextBytes(_array);
+        Span<byte> span = _array;
+
+        Task.Run( () => ClearContents() );
+
+        EnumerateSpan(span);
+    }
+
+    public static void ClearContents()
+    {
+        Task.Delay(20).Wait();
+        lock (_array)
+        {
+           Array.Clear(_array, 0, _array.Length);
+        }
+    }
+
+    // <Snippet1>
+    public static void EnumerateSpan(Span<byte> span)
+    {
+        lock (_array)
+        {
+            foreach (byte element in span)
+            {
+                Console.WriteLine(element);
+                Task.Delay(10).Wait();
+            }
+        }
+    }
+    // The example displays the following output:
+    //    62
+    //    23
+    //    186
+    //    150
+    //    174
+    // </Snippet1>
+}
