fba621eba4fd6cf9f8b34d113056bfdc84555c46
Ron Petrusha <ronpet@microsoft.com>
2018-06-05 08:26:30 -0700

    Used interpolated strings

--- snippets/csharp/api/system.Collections/icomparer/reverse.cs
+++ snippets/csharp/api/system.Collections/icomparer/reverse.cs
@@ -38,42 +38,42 @@ public class Example
    {
       int i = 0;
       foreach (var item in list )
-         Console.WriteLine( "\t[{0}]:\t{1}", i++, item );
+         Console.WriteLine($"   [{i++}]:  {item}");
 
       Console.WriteLine();
    }
 
 }
 // The example displays the following output:
-//        The array initially contains the following values:
-//                [0]:    The
-//                [1]:    quick
-//                [2]:    brown
-//                [3]:    fox
-//                [4]:    jumps
-//                [5]:    over
-//                [6]:    the
-//                [7]:    lazy
-//                [8]:    dog//
-//
+//       The array initially contains the following values:
+//          [0]:  The
+//          [1]:  quick
+//          [2]:  brown
+//          [3]:  fox
+//          [4]:  jumps
+//          [5]:  over
+//          [6]:  the
+//          [7]:  lazy
+//          [8]:  dog
+//       
 //       After sorting with the default comparer:
-//                [0]:    brown
-//                [1]:    dog
-//                [2]:    fox
-//                [3]:    jumps
-//                [4]:    lazy
-//                [5]:    over
-//                [6]:    quick
-//                [7]:    the
-//                [8]:    The//
-//
+//          [0]:  brown
+//          [1]:  dog
+//          [2]:  fox
+//          [3]:  jumps
+//          [4]:  lazy
+//          [5]:  over
+//          [6]:  quick
+//          [7]:  the
+//          [8]:  The
+//       
 //       After sorting with the reverse case-insensitive comparer:
-//                [0]:    the
-//                [1]:    The
-//                [2]:    quick
-//                [3]:    over
-//                [4]:    lazy
-//                [5]:    jumps
-//                [6]:    fox
-//                [7]:    dog
-//                [8]:    brown
+//          [0]:  the
+//          [1]:  The
+//          [2]:  quick
+//          [3]:  over
+//          [4]:  lazy
+//          [5]:  jumps
+//          [6]:  fox
+//          [7]:  dog
+//          [8]:  brown
--- snippets/visualbasic/api/system.collections/icomparer/reverse.vb
+++ snippets/visualbasic/api/system.collections/icomparer/reverse.vb
@@ -32,42 +32,43 @@ Public Class Example
    Public Shared Sub PrintIndexAndValues(list As IEnumerable)
       Dim i As Integer = 0
       For Each item In  list
-         Console.WriteLine(vbTab + "[{0}]:" + vbTab + "{1}", i, item)
-         i = i + 1
+         Console.WriteLine($"   [{i}]:  {item}")
+         i += 1
       Next
       Console.WriteLine()
    End Sub 
 End Class
 ' The example displays the following output:
-'        The array initially contains the following values:
-'                [0]:    The
-'                [1]:    quick
-'                [2]:    brown
-'                [3]:    fox
-'                [4]:    jumps
-'                [5]:    over
-'                [6]:    the
-'                [7]:    lazy
-'                [8]:    dog'
-'
+'       The array initially contains the following values:
+'          [0]:  The
+'          [1]:  quick
+'          [2]:  brown
+'          [3]:  fox
+'          [4]:  jumps
+'          [5]:  over
+'          [6]:  the
+'          [7]:  lazy
+'          [8]:  dog
+'       
 '       After sorting with the default comparer:
-'                [0]:    brown
-'                [1]:    dog
-'                [2]:    fox
-'                [3]:    jumps
-'                [4]:    lazy
-'                [5]:    over
-'                [6]:    quick
-'                [7]:    the
-'                [8]:    The'
-'
+'          [0]:  brown
+'          [1]:  dog
+'          [2]:  fox
+'          [3]:  jumps
+'          [4]:  lazy
+'          [5]:  over
+'          [6]:  quick
+'          [7]:  the
+'          [8]:  The
+'       
 '       After sorting with the reverse case-insensitive comparer:
-'                [0]:    the
-'                [1]:    The
-'                [2]:    quick
-'                [3]:    over
-'                [4]:    lazy
-'                [5]:    jumps
-'                [6]:    fox
-'                [7]:    dog
-'                [8]:    brown
+'          [0]:  the
+'          [1]:  The
+'          [2]:  quick
+'          [3]:  over
+'          [4]:  lazy
+'          [5]:  jumps
+'          [6]:  fox
+'          [7]:  dog
+'          [8]:  brown
+
