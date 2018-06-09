ff0a2d2f67133d2989c10e9fc367b7ec9bb6d488
Petr Onderka <gsvick@gmail.com>
2018-06-09 02:00:11 +0200

    Updated WeatherMicroservice (#114)

--- csharp/getting-started/WeatherMicroservice/Program.cs
+++ csharp/getting-started/WeatherMicroservice/Program.cs
@@ -1,10 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace WeatherMicroservice
 {
@@ -12,20 +14,11 @@ namespace WeatherMicroservice
     {
         public static void Main(string[] args)
         {
-           var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-            
-            var host = new WebHostBuilder()
-                .UseConfiguration(config)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
