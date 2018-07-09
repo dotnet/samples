67b268495a7fda91601d22ddc5e29ac9f3dde68b
Petr Kulikov <15279990+pkulikov@users.noreply.github.com>
2018-07-10 00:11:01 +0200

    Source code for the ML clustering tutorial (#141)
    * Source code for the ML clustering tutorial
    
    * PreserveNewest option for data set files
    
    * Use ML.NET 0.3

---
+++ machine-learning/tutorials/IrisClustering/IrisData.cs
@@ -0,0 +1,32 @@
+﻿// <Snippet1>
+using Microsoft.ML.Runtime.Api;
+// </Snippet1>
+
+namespace IrisClustering
+{
+    // <Snippet2>
+    public class IrisData
+    {
+        [Column("0")]
+        public float SepalLength;
+
+        [Column("1")]
+        public float SepalWidth;
+
+        [Column("2")]
+        public float PetalLength;
+
+        [Column("3")]
+        public float PetalWidth;
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+    // </Snippet2>
+}
