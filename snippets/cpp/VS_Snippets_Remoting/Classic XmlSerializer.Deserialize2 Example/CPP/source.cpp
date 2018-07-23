5eef67cbf9dd52609fbbbbdc023d2a9e061ff682
Sébastien Putier <sputier@gmail.com>
2018-07-23 02:54:52 +0200

    Fix samples indentations for XmlSerializer.Deserialize (#198)
    * Fix indentations for snippet 1 (cs & cpp)
    
    * Fix indentations for snippet 2 (cs & cpp)
    
    * Fix indentations for snippet 3 (cpp)
    
    * Fix a missing indentation

--- snippets/cpp/VS_Snippets_Remoting/Classic XmlSerializer.Deserialize2 Example/CPP/source.cpp
+++ snippets/cpp/VS_Snippets_Remoting/Classic XmlSerializer.Deserialize2 Example/CPP/source.cpp
@@ -14,43 +14,43 @@ using namespace System::Xml::Serialization;
 public ref class OrderedItem
 {
 public:
-   String^ ItemName;
-   String^ Description;
-   Decimal UnitPrice;
-   int Quantity;
-   Decimal LineTotal;
+    String^ ItemName;
+    String^ Description;
+    Decimal UnitPrice;
+    int Quantity;
+    Decimal LineTotal;
 
-   // A custom method used to calculate price per item.
-   void Calculate()
-   {
-      LineTotal = UnitPrice * Quantity;
-   }
+    // A custom method used to calculate price per item.
+    void Calculate()
+    {
+        LineTotal = UnitPrice * Quantity;
+    }
 };
 
 void DeserializeObject( String^ filename )
 {
-   Console::WriteLine( "Reading with XmlReader" );
+    Console::WriteLine( "Reading with XmlReader" );
 
-   // Create an instance of the XmlSerializer specifying type and namespace.
-   XmlSerializer^ serializer = gcnew XmlSerializer( OrderedItem::typeid );
+    // Create an instance of the XmlSerializer specifying type and namespace.
+    XmlSerializer^ serializer = gcnew XmlSerializer( OrderedItem::typeid );
 
-   // A FileStream is needed to read the XML document.
-   FileStream^ fs = gcnew FileStream( filename,FileMode::Open );
-   XmlReader^ reader = gcnew XmlTextReader( fs );
+    // A FileStream is needed to read the XML document.
+    FileStream^ fs = gcnew FileStream( filename,FileMode::Open );
+    XmlReader^ reader = gcnew XmlTextReader( fs );
 
-   // Declare an object variable of the type to be deserialized.
-   OrderedItem^ i;
+    // Declare an object variable of the type to be deserialized.
+    OrderedItem^ i;
 
-   // Use the Deserialize method to restore the object's state.
-   i = dynamic_cast<OrderedItem^>(serializer->Deserialize( reader ));
+    // Use the Deserialize method to restore the object's state.
+    i = dynamic_cast<OrderedItem^>(serializer->Deserialize( reader ));
 
-   // Write out the properties of the object.
-   Console::Write( "{0}\t{1}\t{2}\t{3}\t{4}", i->ItemName, i->Description, i->UnitPrice, i->Quantity, i->LineTotal );
+    // Write out the properties of the object.
+    Console::Write( "{0}\t{1}\t{2}\t{3}\t{4}", i->ItemName, i->Description, i->UnitPrice, i->Quantity, i->LineTotal );
 }
 
 int main()
 {
-   // Read a purchase order.
-   DeserializeObject( "simple.xml" );
+    // Read a purchase order.
+    DeserializeObject( "simple.xml" );
 }
 // </Snippet1>
