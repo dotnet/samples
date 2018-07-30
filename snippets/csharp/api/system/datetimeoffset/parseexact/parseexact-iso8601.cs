cc5627ed9141bc1707a7014a630842424a4b962d
Ron Petrusha <ronpet@microsoft.com>
2018-07-30 12:31:38 -0700

    Moved VB examples, modified formatting

--- snippets/csharp/api/system/datetimeoffset/parseexact/parseexact-iso8601.cs
+++ snippets/csharp/api/system/datetimeoffset/parseexact/parseexact-iso8601.cs
@@ -15,7 +15,7 @@ public class Example
       {
          try {
             var date = DateTimeOffset.ParseExact(dateString, "O", null);
-            Console.WriteLine($"{dateString} --> {date:yyyy-MM-dd HH:mm:ss.FF zzz}");
+            Console.WriteLine($"{dateString,-35} --> {date:yyyy-MM-dd HH:mm:ss.FF zzz}");
          }
          catch (FormatException)
          {
@@ -26,9 +26,9 @@ public class Example
    }
 }
 // The example displays the following output:
-//      2018-08-18T12:45:16.0000000Z --> 2018-08-18 12:45:16 +00:00
+//      2018-08-18T12:45:16.0000000Z        --> 2018-08-18 12:45:16 +00:00
 //      FormatException: Unable to convert '2018/08/18T12:45:16.0000000Z'
 //      FormatException: Unable to convert '2018-18-08T12:45:16.0000000Z'
 //      FormatException: Unable to convert ' 2018-08-18T12:45:16.0000000Z '
-//      2018-08-18T12:45:16.0000000+02:00 --> 2018-08-18 12:45:16 +02:00
-//      2018-08-18T12:45:16.0000000-07:00 --> 2018-08-18 12:45:16 -07:00
+//      2018-08-18T12:45:16.0000000+02:00   --> 2018-08-18 12:45:16 +02:00
+//      2018-08-18T12:45:16.0000000-07:00   --> 2018-08-18 12:45:16 -07:00
--- snippets/visualbasic/api/system/datetimeoffset/parseexact/parseexact-iso8601.vb
+++ snippets/visualbasic/api/system/datetimeoffset/parseexact/parseexact-iso8601.vb
@@ -10,7 +10,7 @@ Public Module Example
       For Each dateStr In dateStrings
          Try 
             Dim dat = DateTimeOffset.ParseExact(dateStr, "O", Nothing)
-            Console.WriteLine($"{dateString} --> {dat:yyyy-MM-dd HH:mm:ss.FF zzz}")
+            Console.WriteLine($"{dateStr,-35} --> {dat:yyyy-MM-dd HH:mm:ss.FF zzz}")
          Catch  e As FormatException
             Console.WriteLine($"FormatException: Unable to convert '{dateStr}'")
          End Try   
@@ -18,9 +18,9 @@ Public Module Example
    End Sub
 End Module
 ' The example displays the following output:
-'      2018-08-18T12:45:16.0000000Z --> 2018-08-18 12:45:16 +00:00
+'      2018-08-18T12:45:16.0000000Z        --> 2018-08-18 12:45:16 +00:00
 '      FormatException: Unable to convert '2018/08/18T12:45:16.0000000Z'
 '      FormatException: Unable to convert '2018-18-08T12:45:16.0000000Z'
 '      FormatException: Unable to convert ' 2018-08-18T12:45:16.0000000Z '
-'      2018-08-18T12:45:16.0000000+02:00 --> 2018-08-18 12:45:16 +02:00
-'      2018-08-18T12:45:16.0000000-07:00 --> 2018-08-18 12:45:16 -07:00
+'      2018-08-18T12:45:16.0000000+02:00   --> 2018-08-18 12:45:16 +02:00
+'      2018-08-18T12:45:16.0000000-07:00   --> 2018-08-18 12:45:16 -07:00
