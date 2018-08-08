479ca02d6e9d32888a75423dcdf867150904e4a8
Ron Petrusha <ronpet@microsoft.com>
2018-08-08 15:46:54 -0700

    Added interpolated strings

--- snippets/visualbasic/VS_Snippets_CLR_System/system.array.getupperbound/vb/source.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.array.getupperbound/vb/source.vb
@@ -6,7 +6,7 @@ Public Module Example
         ' Get the upper and lower bound of the array.
         Dim upper As Integer = integers.GetUpperBound(0)
         Dim lower As Integer = integers.GetLowerBound(0)
-        Console.WriteLine("Elements from index {0} to {1}:", lower, upper)
+        Console.WriteLine($"Elements from index {lower} to {upper}:")
         ' Iterate the array.
         For ctr As Integer = lower To upper
            Console.Write("{0}{1}{2}", If(ctr = lower, "   ", ""), 
@@ -20,18 +20,17 @@ Public Module Example
                                        {6, 36}, {7, 49}, {8, 64}, {9, 81} } 
         ' Get the number of dimensions.                               
         Dim rank As Integer = integers2d.Rank  
-        Console.WriteLine("Number of dimensions: {0}", rank)      
+        Console.WriteLine($"Number of dimensions: {rank}")      
         For ctr As Integer = 0 To rank - 1
-           Console.WriteLine("   Dimension {0}: from {1} to {2}",
-                             ctr, integers2d.GetLowerBound(ctr),
-                             integers2d.GetUpperBound(ctr))
+           Console.WriteLine($"   Dimension {ctr}: " +
+                             $"from {integers2d.GetLowerBound(ctr)} to {integers2d.GetUpperBound(ctr)}")
         Next
         ' Iterate the 2-dimensional array and display its values.
         Console.WriteLine("   Values of array elements:")
         For outer = integers2d.GetLowerBound(0) To integers2d.GetUpperBound(0)
            For inner = integers2d.GetLowerBound(1) To integers2d.GetUpperBound(1)
-              Console.WriteLine("      {3}{0}, {1}{4} = {2}", outer, inner,
-                                integers2d.GetValue(outer, inner), "{", "}")
+              Console.WriteLine($"      {ChrW(&h07b)}{outer}, {inner}{ChrW(&h007d)} = " +
+                                $"{integers2d.GetValue(outer, inner)}")
            Next
         Next
     End Sub
