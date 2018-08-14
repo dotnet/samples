39e54b008164a890006179a28682d5909a7f16d1
Douglas Laudenschlager <douglasl@microsoft.com>
2018-08-14 13:26:54 -0700

    Added using statement to dispose SqlDataReader, fixes 5914.

--- snippets/csharp/VS_Snippets_ADO.NET/Classic WebData SqlDataReader.Close Example/CS/source.cs
+++ snippets/csharp/VS_Snippets_ADO.NET/Classic WebData SqlDataReader.Close Example/CS/source.cs
@@ -21,21 +21,24 @@ class Program
         using (SqlConnection connection =
                    new SqlConnection(connectionString))
         {
-            SqlCommand command =
-                new SqlCommand(queryString, connection);
             connection.Open();
 
-            SqlDataReader reader = command.ExecuteReader();
-
-            // Call Read before accessing data.
-            while (reader.Read())
+            using (SqlCommand command =
+                new SqlCommand(queryString, connection))
             {
-                Console.WriteLine(String.Format("{0}, {1}",
-                    reader[0], reader[1]));
-            }
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    // Call Read before accessing data.
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0}, {1}",
+                            reader[0], reader[1]));
+                    }
 
-            // Call Close when done reading.
-            reader.Close();
+                    // Call Close when done reading.
+                   reader.Close();
+                }
+        }
         }
     }
     // </Snippet1>
