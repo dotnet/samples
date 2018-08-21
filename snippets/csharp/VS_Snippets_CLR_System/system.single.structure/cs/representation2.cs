6268461919bdc0f57d5c1bc6b0eb32c6235dec05
Petr Kulikov <petr.kulikov@gmail.com>
2018-08-22 00:26:26 +0200

    Synced System.Single snippets with the text (#251)

--- snippets/csharp/VS_Snippets_CLR_System/system.single.structure/cs/representation2.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.single.structure/cs/representation2.cs
@@ -6,12 +6,11 @@ public class Example
 {
    public static void Main()
    {
-      Single value = 123456789e4f;
-      Single additional = Single.Epsilon * 1e12f;
-      Console.WriteLine("{0} + {1} = {2}", value, additional, 
-                                           value + additional);
+      Single value = 123.456f;
+      Single additional = Single.Epsilon * 1e15f;
+      Console.WriteLine($"{value} + {additional} = {value + additional}");
    }
 }
 // The example displays the following output:
-//    1.234568E+12 + 1.401298E-33 = 1.234568E+12
+//    123.456 + 1.401298E-30 = 123.456
 // </Snippet4>
