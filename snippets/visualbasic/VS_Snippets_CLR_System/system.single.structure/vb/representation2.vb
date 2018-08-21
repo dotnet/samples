4dfbb6c3b53f6bd1381538be6770d66c5219e6ce
pkulikov <petr.kulikov@gmail.com>
2018-08-21 22:26:58 +0200

    Synced System.Single snippets with the text

--- snippets/visualbasic/VS_Snippets_CLR_System/system.single.structure/vb/representation2.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.single.structure/vb/representation2.vb
@@ -4,13 +4,12 @@ Option Strict On
 ' <Snippet4>
 Module Example
    Public Sub Main()
-      Dim value As Single = 123456789e4
-      Dim additional As Single = Single.Epsilon * 1e12
-      Console.WriteLine("{0} + {1} = {2}", value, additional, 
-                                           value + additional)
+      Dim value As Single = 123.456
+      Dim additional As Single = Single.Epsilon * 1e15
+      Console.WriteLine($"{value} + {additional} = {value + additional}")
    End Sub
 End Module
 ' The example displays the following output:
-'   1.234568E+12 + 1.401298E-33 = 1.234568E+12
+'   123.456 + 1.401298E-30 = 123.456
 ' </Snippet4>
 
