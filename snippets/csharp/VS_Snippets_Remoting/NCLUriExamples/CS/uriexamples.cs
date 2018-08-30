cdfbd74b705ffe661efd1872cafa84292b1950fc
Ron Petrusha <ronpet@microsoft.com>
2018-08-30 11:03:33 -0700

    Added output to examples (#274)

--- snippets/csharp/VS_Snippets_Remoting/NCLUriExamples/CS/uriexamples.cs
+++ snippets/csharp/VS_Snippets_Remoting/NCLUriExamples/CS/uriexamples.cs
@@ -78,6 +78,11 @@ namespace Example
              
             Console.WriteLine("The path of this Uri is {0}", uriAddress.GetLeftPart(UriPartial.Path));
             Console.WriteLine("Hash code {0}", uriAddress.GetHashCode());
+            // The example displays the following output:
+            //        #search
+            //        Uri uses the default port
+            //        The path of this Uri is http://www.contoso.com/index.htm
+            //        Hash code -988419291
         //</snippet4>
         //<snippet5>
             Uri uriAddress1 = new Uri("http://www.contoso.com/title/index.htm");
