27daa952bacea745182f8a5c8f2e02800385d65c
Roel Fauconnier <rfauconn+github@gmail.com>
2018-08-30 21:51:39 +0200

    Updated sample to reflect usage of 4 parameters (#272)
    * Updated sample to reflect usage of 4 parameters
    
    The sample did not show the usage of the `prefix` parameter.
    This is now updated to include an example of how to use the `prefix` parameter.
    
    * Updated ISBN to match other examples on same page
    
    * Updated the visualbasic sample to match csharp
    
    * Fixed output to match example
    
    * Updated cpp example

--- snippets/visualbasic/VS_Snippets_Data/Classic WebData XmlDocument.CreateNode2 Example/VB/source.vb
+++ snippets/visualbasic/VS_Snippets_Data/Classic WebData XmlDocument.CreateNode2 Example/VB/source.vb
@@ -6,23 +6,27 @@ public class Sample
 
   public shared sub Main() 
 
-       Dim doc as XmlDocument = new XmlDocument()
-       doc.LoadXml("<book>" & _
-                   "  <title>Oberon's Legacy</title>" & _
-                   "  <price>5.95</price>" & _
-                   "</book>") 
+        Dim doc as XmlDocument = new XmlDocument()
+        doc.LoadXml("<book>" & _
+                    "  <title>Oberon's Legacy</title>" & _
+                    "  <price>5.95</price>" & _
+                       "</book>") 
  
-       ' Create a new element node.
-       Dim newElem as XmlNode
-       newElem = doc.CreateNode(XmlNodeType.Element, "pages", "")  
-       newElem.InnerText = "290"
+        ' Create a new element node.
+        ' It is possible to supply a prefix for this node, and specify a qualified namespace
+        Dim newElem as XmlNode
+        newElem = doc.CreateNode(XmlNodeType.Element,"g", "ISBN","https://global.ISBN/list")
+        newElem.InnerText = "1-861001-57-5"
      
-       Console.WriteLine("Add the new element to the document...")
-       Dim root as XmlElement = doc.DocumentElement
-       root.AppendChild(newElem)
+        ' Add the new element to the document
+        Dim root as XmlElement = doc.DocumentElement
+        root.AppendChild(newElem)
      
-       Console.WriteLine("Display the modified XML document...")
-       Console.WriteLine(doc.OuterXml)
+        ' Display the modified XML document
+        Console.WriteLine(doc.OuterXml)
+        
+        ' Output:
+        ' <book><title>Oberon's Legacy</title><price>5.95</price><g:ISBN xmlns:g="https://global.ISBN/list">1-861001-57-5</g:ISBN></book>
    end sub
 end class
 ' </Snippet1>
