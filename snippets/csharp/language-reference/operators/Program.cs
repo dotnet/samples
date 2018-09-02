453a6db1f2ec780a81ee045d89a78a283cf222a2
pkulikov <petr.kulikov@gmail.com>
2018-09-02 20:56:14 +0200

    Initialized the project for operator snippets

---
+++ snippets/csharp/language-reference/operators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemainderExamples.Examples();
+        }
+    }
+}
---
+++ snippets/csharp/language-reference/operators/RemainderExamples.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace operators
+{
+    public static class RemainderExamples
+    {
+        public static void Examples()
+        {
+            IntegerRemainder();
+            FloatingPointRemainer();
+        }
+
+        private static void IntegerRemainder()
+        {
+            Console.WriteLine("Integer");
+        }
+
+        private static void FloatingPointRemainer()
+        {
+            Console.WriteLine("Floating-point");
+        }
+    }
+}
