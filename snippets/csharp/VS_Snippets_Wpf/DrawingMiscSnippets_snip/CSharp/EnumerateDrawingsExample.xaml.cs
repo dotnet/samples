afcd76672aa58fe8a40bd417e0abb32d5a2a7024
lindexi <lindexi_gd@163.com>
2018-09-15 09:30:09 +0800

    remove extra blank line

--- snippets/csharp/VS_Snippets_Wpf/DrawingMiscSnippets_snip/CSharp/EnumerateDrawingsExample.xaml.cs
+++ snippets/csharp/VS_Snippets_Wpf/DrawingMiscSnippets_snip/CSharp/EnumerateDrawingsExample.xaml.cs
@@ -5,14 +5,10 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Navigation;
 
-
 namespace SDKSample
 {
-
-
-    public partial class EnumerateDrawingsExample: Page
+    public partial class EnumerateDrawingsExample : Page
     {
-
         public EnumerateDrawingsExample()
         {
             InitializeComponent();
@@ -28,43 +24,40 @@ namespace SDKSample
         {
             DrawingGroup dGroup = VisualTreeHelper.GetDrawing(v);
             EnumDrawingGroup(dGroup);
-
         }
 
-         // Enumerate the drawings in the DrawingGroup.
-         public void EnumDrawingGroup(DrawingGroup drawingGroup)
-         {
-             DrawingCollection dc = drawingGroup.Children;
+        // Enumerate the drawings in the DrawingGroup.
+        public void EnumDrawingGroup(DrawingGroup drawingGroup)
+        {
+            DrawingCollection dc = drawingGroup.Children;
 
-             // Enumerate the drawings in the DrawingCollection.
-             foreach (Drawing drawing in dc)
-             {
-                 // If the drawing is a DrawingGroup, call the function recursively.
-                 if (drawing.GetType() == typeof(DrawingGroup))
-                 {
-                     EnumDrawingGroup((DrawingGroup)drawing);
-                 }
-                 else if (drawing.GetType() == typeof(GeometryDrawing))
-                 {
-                     // Perform action based on drawing type.  
-                 }
-                 else if (drawing.GetType() == typeof(ImageDrawing))
-                 {
-                     // Perform action based on drawing type.
-                 }
-                 else if (drawing.GetType() == typeof(GlyphRunDrawing))
-                 {
-                     // Perform action based on drawing type.
-                 }
-                 else if (drawing.GetType() == typeof(VideoDrawing))
-                 {
-                     // Perform action based on drawing type.
-                 }
-             }
-         }
-         //</SnippetGraphicsMMRetrieveDrawings>
-     
+            // Enumerate the drawings in the DrawingCollection.
+            foreach (Drawing drawing in dc)
+            {
+                // If the drawing is a DrawingGroup, call the function recursively.
+                if (drawing.GetType() == typeof(DrawingGroup))
+                {
+                    EnumDrawingGroup((DrawingGroup)drawing);
+                }
+                else if (drawing.GetType() == typeof(GeometryDrawing))
+                {
+                    // Perform action based on drawing type.  
+                }
+                else if (drawing.GetType() == typeof(ImageDrawing))
+                {
+                    // Perform action based on drawing type.
+                }
+                else if (drawing.GetType() == typeof(GlyphRunDrawing))
+                {
+                    // Perform action based on drawing type.
+                }
+                else if (drawing.GetType() == typeof(VideoDrawing))
+                {
+                    // Perform action based on drawing type.
+                }
+            }
+        }
+
+        //</SnippetGraphicsMMRetrieveDrawings>
     }
-    
-
 }
