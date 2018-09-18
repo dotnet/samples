944685de6763402dc4a3b5ca7cd3933c112a0ee4
Richard Lander <rlander@microsoft.com>
2018-09-18 16:31:25 -0700

    Add SPI implementation

--- iot/trimpot/Program.cs
+++ iot/trimpot/Program.cs
@@ -3,100 +3,33 @@ using System.Devices.Gpio;
 using System.Devices.Spi;
 using System.Threading;
 
-namespace led_pwm
+namespace trimpot
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var SPICLK = 18;
-            var SPIMISO = 23;
-            var SPIMOSI = 24;
-            var SPICS = 25;
+            Console.WriteLine("Hello World!!!");
+            /* 
+            var connection = new SpiConnectionSettings(0,0);
+            connection.ClockFrequency = 1000000;
+            connection.Mode = SpiMode.Mode0;
+            var spi = new UnixSpiDevice(connection);
 
-            Console.WriteLine($"Let's read potentiometer values!!!");
+            var mcp = new Mcp3008(spi);
+*/
             GpioController controller = new GpioController(PinNumberingScheme.Gpio);
-            GpioPin SPICLKPin = controller.OpenPin(SPICLK, PinMode.Output);
-            GpioPin SPIMISOPin = controller.OpenPin(SPIMISO, PinMode.Input);
-            GpioPin SPIMOSIPin = controller.OpenPin(SPIMOSI, PinMode.Output);
-            GpioPin SPICSPin = controller.OpenPin(SPICS, PinMode.Output);
-            Console.WriteLine($"GPIO pins enabled for use: {SPICLK}, {SPIMISO}, {SPIMOSI}, {SPICS}");
+            var mcp = new Mcp3008(controller, 18, 23, 24, 25);
 
-            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs eventArgs) =>
-            {
-                controller.ClosePin(SPICLK);
-                controller.ClosePin(SPIMISO);
-                controller.ClosePin(SPIMOSI);
-                controller.ClosePin(SPICS);
-            };
-
-            var last_read = 0;      // this keeps track of the last potentiometer value
-            var tolerance = 5;      // to keep from being jittery we'll only change
-                                    // volume when the pot has moved more than 5 'counts'
 
             while (true)
             {
-                var commandout = 0;
-                var trim_pot = 0;
-
-
-                SPICSPin.Write(PinValue.High);
-                SPICLKPin.Write(PinValue.Low);
-                SPICSPin.Write(PinValue.Low);
-
-                commandout |= 0x18;
-                commandout <<= 3;
-
-                for (var i = 0; i < 5; i++)
-                {
-                    if ((commandout & 0x80) > 0)
-                    {
-                        SPIMOSIPin.Write(PinValue.High);
-                    }
-                    else
-                    {
-                        SPIMOSIPin.Write(PinValue.Low);
-                    }
-
-                    commandout <<= 1;
-                    SPICLKPin.Write(PinValue.High);
-                    SPICLKPin.Write(PinValue.Low);
-                }
-
-                for (var i = 0; i < 12; i++)
-                {
-                    SPICLKPin.Write(PinValue.High);
-                    SPICLKPin.Write(PinValue.Low);
-                    trim_pot <<= 1;
-
-                    if (SPIMISOPin.Read() == PinValue.High)
-                    {
-                        trim_pot |= 0x1;
-                    }
-                }
-
-                SPICSPin.Write(PinValue.High);
-                
-                trim_pot >>= 1;
-                var pot_adjust = Math.Abs(trim_pot - last_read);
-
-                Console.WriteLine($"trim_pot: {trim_pot}");
-                Console.WriteLine($"pot_adjust: {pot_adjust}");
-                Console.WriteLine($"last_read: {last_read}");
-
-                if (pot_adjust > tolerance)
-                {
-                    last_read = trim_pot;
-                    Console.WriteLine("***Changed***");
-                    var volume = trim_pot / 10.24;
-                    volume = Math.Round(volume);
-                    Console.WriteLine($"Volume: {volume}");
-                }
-                
+                double value = mcp.Read(0);
+                value = value / 10.24;
+                value = Math.Round(value);
+                Console.WriteLine(value);
                 Thread.Sleep(500);
             }
-
         }
-
     }
 }
