7f7e4e23fc7d08bccda2ebc823cce7d261db7033
Ron Petrusha <ronpet@microsoft.com>
2018-09-24 11:55:01 -0700

    Added calls to Task.Wait (#318)
    * Added calls to Task.Wait
    
    * Changed C# examples to use async main

--- snippets/csharp/VS_Snippets_Misc/tpl_continuations/cs/simple1.cs
+++ snippets/csharp/VS_Snippets_Misc/tpl_continuations/cs/simple1.cs
@@ -4,13 +4,13 @@ using System.Threading.Tasks;
 
 public class Example
 {
-   public static void Main()
+   public static async Task Main()
    {
       // Execute the antecedent.
       Task<DayOfWeek> taskA = Task.Run( () => DateTime.Today.DayOfWeek );
 
       // Execute the continuation when the antecedent finishes.
-      Task continuation = taskA.ContinueWith( antecedent => Console.WriteLine("Today is {0}.", antecedent.Result) );
+      await taskA.ContinueWith( antecedent => Console.WriteLine("Today is {0}.", antecedent.Result) );
    }
 }
 // The example displays output like the following output:
