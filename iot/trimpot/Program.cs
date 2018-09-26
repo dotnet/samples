58c7bdb22454e495d20b4b6dcbe29405e4c995e0
Rich Lander <rlander@microsoft.com>
2018-09-26 10:01:33 -0700

    Move GPIO/SPI implementations to local functions

--- iot/trimpot/Program.cs
+++ iot/trimpot/Program.cs
@@ -11,24 +11,37 @@ namespace trimpot
         {
             Console.WriteLine("Hello Trimpot!");
 
-            // This code should be uncommented to enable accessing
-            // the MCP3008 via SPI
-            /* 
-            var connection = new SpiConnectionSettings(0,0);
-            connection.ClockFrequency = 1000000;
-            connection.Mode = SpiMode.Mode0;
-            var spi = new UnixSpiDevice(connection);
+            // This sample implements two different ways of accessing the MCP3008.
+            // The SPI option is enabled in the sample by default, but you can switch
+            // to the GPIO bit-banging option by switching which one is commented out.
+            // The sample uses local functions to make it easier to switch between
+            // the two implementations
 
-            var mcp = new Mcp3008(spi);
-            */
+            // SPI implementation
+            Mcp3008 GetMCP3008WithSPI()
+            {
+                var connection = new SpiConnectionSettings(0,0);
+                connection.ClockFrequency = 1000000;
+                connection.Mode = SpiMode.Mode0;
+                var spi = new UnixSpiDevice(connection);
+                var mcp3008 = new Mcp3008(spi);
+                return mcp3008;
+            }
 
-            // This code should be uncommented to enable accessing
-            // the MCP3008 via GPIO (via bit banging)
 
-            /* 
-            GpioController controller = new GpioController(PinNumberingScheme.Gpio);
-            var mcp = new Mcp3008(controller, 18, 23, 24, 25);
-            */
+            // the GPIO (via bit banging) implementation
+            Mcp3008 GetMCP3008WithGPIO()
+            {
+                GpioController controller = new GpioController(PinNumberingScheme.Gpio);
+                var mcp3008 = new Mcp3008(controller, 18, 23, 24, 25);
+                return mcp3008;
+            }
+
+            // Using SPI implementation
+            var mcp = GetMCP3008WithSPI();
+
+            // Using GPIO implementation
+            // var mcp = GetMCP3008WithGPIO();
 
             while (true)
             {
