d81742e91236ab07ed165a9e23ea7c265a7c2277
Mackiovello <elandroe@icloud.com>
2018-10-13 09:47:12 +0800

    Remove unecessary try..catch for process_getprocessbyname with c++ and VB

--- snippets/visualbasic/VS_Snippets_CLR/Process_GetProcessesByName2_2/VB/process_getprocessesbyname2_2.vb
+++ snippets/visualbasic/VS_Snippets_CLR/Process_GetProcessesByName2_2/VB/process_getprocessesbyname2_2.vb
@@ -19,29 +19,21 @@ Class GetProcessesByNameClass
    End Sub
 
    Public Overloads Shared Sub Main(ByVal args() As String)
-      Try
-
-         Console.Writeline("Create notepad processes on remote computer")
-         Console.Write("Enter remote computer name : ")
-         Dim remoteMachineName As String = Console.ReadLine()
-         ' Get all notepad processess into Process array.
-         Dim myProcesses As Process() = Process.GetProcessesByName _
-                                             ("notepad", remoteMachineName)
-         If myProcesses.Length = 0 Then
-            Console.WriteLine("Could not find notepad processes on remote computer.")
-         End If
-         Dim myProcess As Process
-         For Each myProcess In myProcesses
-            Console.WriteLine("Process Name : " & myProcess.ProcessName & _
-                          "  Process ID : " & myProcess.Id & _
-                          "  MachineName : " & myProcess.MachineName)
-         Next myProcess
-
-      Catch e As SystemException
-         Console.Write("Caught Exception .... : " & e.Message)
-      Catch e As Exception
-         Console.Write("Caught Exception .... : " & e.Message)
-      End Try
+      Console.Writeline("Create notepad processes on remote computer")
+      Console.Write("Enter remote computer name : ")
+      Dim remoteMachineName As String = Console.ReadLine()
+      ' Get all notepad processess into Process array.
+      Dim myProcesses As Process() = Process.GetProcessesByName _
+                                          ("notepad", remoteMachineName)
+      If myProcesses.Length = 0 Then
+         Console.WriteLine("Could not find notepad processes on remote computer.")
+      End If
+      Dim myProcess As Process
+      For Each myProcess In myProcesses
+         Console.WriteLine("Process Name : " & myProcess.ProcessName & _
+                       "  Process ID : " & myProcess.Id & _
+                       "  MachineName : " & myProcess.MachineName)
+      Next myProcess
    End Sub 'Main
 End Class 'GetProcessesByNameClass
 ' </Snippet1>
