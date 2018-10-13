f4fe72159362d856090251f5c2bbb826a8a2eb11
Mackiovello <elandroe@icloud.com>
2018-10-13 09:20:29 +0800

    Remove unecessary try..catch for getprocessbyname
    For https://github.com/dotnet/docs/issues/6271

--- snippets/csharp/VS_Snippets_CLR/Process_GetProcessesByName2_2/CS/process_getprocessesbyname2_2.cs
+++ snippets/csharp/VS_Snippets_CLR/Process_GetProcessesByName2_2/CS/process_getprocessesbyname2_2.cs
@@ -14,34 +14,21 @@ using System.Diagnostics;
 
 class GetProcessesByNameClass
 {
-   public static void Main(string[] args)
-   {
-      try
-      {
-
-         Console.Write("Create notepad processes on remote computer \n");
-         Console.Write("Enter remote computer name : ");
-         string remoteMachineName = Console.ReadLine();
-         // Get all notepad processess into Process array.
-         Process[] myProcesses = Process.GetProcessesByName("notepad",remoteMachineName);
-         if(myProcesses.Length == 0)
+    public static void Main(string[] args)
+    {
+        Console.Write("Create notepad processes on remote computer \n");
+        Console.Write("Enter remote computer name : ");
+        string remoteMachineName = Console.ReadLine();
+        // Get all notepad processess into Process array.
+        Process[] myProcesses = Process.GetProcessesByName("notepad", remoteMachineName);
+        if (myProcesses.Length == 0)
             Console.WriteLine("Could not find notepad processes on remote computer.");
-         foreach(Process myProcess in myProcesses)
-         {
+        foreach (Process myProcess in myProcesses)
+        {
             Console.Write("Process Name : " + myProcess.ProcessName + "  Process ID : "
-               + myProcess.Id + "  MachineName : " + myProcess.MachineName + "\n");
-         }
-
-      }
-      catch(SystemException e)
-      {
-         Console.Write("Caught Exception .... : " + e.Message);
-      }
-      catch(Exception e)
-      {
-         Console.Write("Caught Exception .... : " + e.Message);
-      }
-   }
+            + myProcess.Id + "  MachineName : " + myProcess.MachineName + "\n");
+        }
+    }
 }
 // </Snippet1> 
 // </Snippet2> 
