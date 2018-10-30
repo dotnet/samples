804ec0a4a8b0cca099e40bb9a3e8862edfafe267
Erlend <elandroe@icloud.com>
2018-10-30 17:21:55 +0100

    Use the appropriate loop and bracket on newline (#428)
    * Use the appropriate loop and bracket on newline
    
    * Handle executable name properly
    
    * Use async Main method and await

--- snippets/csharp/language-reference/keywords/await/await2.cs
+++ snippets/csharp/language-reference/keywords/await/await2.cs
@@ -1,30 +1,36 @@
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
 class Example
 {
-   static void Main()
+   static async Task Main()
    {
       string[] args = Environment.GetCommandLineArgs();
       if (args.Length < 2)
-         throw new ArgumentNullException("No URLs specified on the command line.");
-      
-      long characters = GetPageLengthsAsync(args).Result;
-      Console.WriteLine($"{args.Length - 1} pages, {characters:N0} characters");
+         throw new ArgumentNullException("No URIs specified on the command line.");
+
+      // Don't pass the executable file name
+      var uris = args.Skip(1).ToArray();
+
+      long characters = await GetPageLengthsAsync(uris);
+      Console.WriteLine($"{uris.Length} pages, {characters:N0} characters");
    }
 
-   private static async Task<long> GetPageLengthsAsync(string[] args)
+   private static async Task<long> GetPageLengthsAsync(string[] uris)
    {
       var client = new HttpClient();
       long pageLengths = 0;
 
-      for (int ctr = 1; ctr < args.Length; ctr++) {
-         var uri = new Uri(Uri.EscapeUriString(args[ctr]));
-         string pageContents = await client.GetStringAsync(uri);
+      foreach (var uri in uris)
+      {
+         var escapedUri = new Uri(Uri.EscapeUriString(uri));
+         string pageContents = await client.GetStringAsync(escapedUri);
          Interlocked.Add(ref pageLengths, pageContents.Length);
       }
+      
       return pageLengths;
    }
 }
