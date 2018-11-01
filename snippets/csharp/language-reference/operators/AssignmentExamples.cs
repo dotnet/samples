8b2a2ec7435dcbbff712950eee2a00b16a12ccb2
Petr Kulikov <petr.kulikov@gmail.com>
2018-11-01 16:04:54 +0100

    Snippets for the assignment operator reference (#431)
    * Added the assignment operator snippets
    
    * Refactoring

---
+++ snippets/csharp/language-reference/operators/AssignmentExamples.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace operators
+{
+    public static class AssignmentExamples
+    {
+        public static void Examples()
+        {
+            VariablePropertyIndexerExample();
+        }
+
+        private static void VariablePropertyIndexerExample()
+        {
+            // <SnippetAssignments>
+            var numbers = new List<double>() { 1.0, 2.0, 3.0 };
+
+            Console.WriteLine(numbers.Capacity);
+            numbers.Capacity = 100;
+            Console.WriteLine(numbers.Capacity);
+            // Output:
+            // 4
+            // 100
+
+            int newFirstElement;
+            double originalFirstElement = numbers[0];
+            newFirstElement = 5;
+            numbers[0] = newFirstElement;
+            Console.WriteLine(originalFirstElement);
+            Console.WriteLine(numbers[0]);
+            // Output:
+            // 1
+            // 5
+            // </SnippetAssignments>
+        }
+    }
+}
