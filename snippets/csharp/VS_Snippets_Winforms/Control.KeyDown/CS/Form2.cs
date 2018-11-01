f974ccb6815e6ff4c101e9efa84aca05d43d7ef4
hunbernd <hunbernd@outlook.com>
2018-11-01 11:24:39 +0100

    Fixed typo + removed usings

--- snippets/csharp/VS_Snippets_Winforms/Control.KeyDown/CS/Form2.cs
+++ snippets/csharp/VS_Snippets_Winforms/Control.KeyDown/CS/Form2.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
@@ -26,7 +18,7 @@ namespace WindowsFormsApp1
 			textBox1.KeyUp += TextBox1_KeyUp;
 		}
 
-		// Handle the KeyUP event to print the type of character entered into the control.
+		// Handle the KeyUp event to print the type of character entered into the control.
 		private void TextBox1_KeyUp(object sender, KeyEventArgs e)
 		{
 			textBox2.AppendText( $"KeyUp code: {e.KeyCode}, value: {e.KeyValue}, modifiers: {e.Modifiers}" + "\r\n");
