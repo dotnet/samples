01ed4c4daf4a7b82e2a1f86c6c67fb4292d1d6f1
pkulikov <petr.kulikov@gmail.com>
2018-11-03 13:28:51 +0100

    Minor refactoring

--- snippets/csharp/language-reference/operators/BitwiseComplementExamples.cs
+++ snippets/csharp/language-reference/operators/BitwiseComplementExamples.cs
@@ -12,11 +12,11 @@ namespace operators
         private static void BitwiseComplement()
         {
             // <SnippetExample>
-            uint a = 0b_0000_1111_0000_1111_0000_1111_0000_0011;
+            uint a = 0b_0000_1111_0000_1111_0000_1111_0000_1100;
             uint b = ~a;
             Console.WriteLine(Convert.ToString(b, toBase: 2));
             // Output:
-            // 11110000111100001111000011111100
+            // 11110000111100001111000011110011
             // </SnippetExample>
         }
     }
