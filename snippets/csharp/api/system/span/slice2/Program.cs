bec9049aabb64545b780dd6a7ba1a61b1a754920
Ron Petrusha <ronpet@microsoft.com>
2018-11-07 08:06:33 -0800

    Span examples (#444)

---
+++ snippets/csharp/api/system/span/slice2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string contentLength = "Content-Length: 132";
+        var length = GetContentLength(contentLength.ToCharArray());	
+        Console.WriteLine($"Content length: {length}"); 
+    }
+
+    private static int GetContentLength(ReadOnlySpan<char> span)
+    {
+        var slice = span.Slice(16);
+	return Int32.Parse(slice);	
+    }
+}
+// Output:
+//      Content length: 132
