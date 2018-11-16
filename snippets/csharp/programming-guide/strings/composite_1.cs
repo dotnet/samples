5ac13d6f9b2740bf4089be188023a2d660672bf1
hDoubleE <trevor.heehs@gmail.com>
2018-11-15 20:22:03 -0800

    string snippets

---
+++ snippets/csharp/programming-guide/strings/composite_1.cs
@@ -0,0 +1,9 @@
+string name = "Mark";
+var date = DateTime.Now;
+
+// Composite formatting:
+Console.WriteLine("Hello, {0}! Today is {1}, it's {2:HH:mm} now.", name, date.DayOfWeek, date);
+// String interpolation:
+Console.WriteLine($"Hello, {name}! Today is {date.DayOfWeek}, it's {date:HH:mm} now.");
+// Both calls produce the same output that is similar to:
+// Hello, Mark! Today is Wednesday, it's 19:40 now.
