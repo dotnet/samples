f0ee6872d2453d576a365d1afad613be48e57f7e
hDoubleE <trevor.heehs@gmail.com>
2018-11-16 18:21:56 -0800

    added-named-tuple-linked-files

--- snippets/csharp/programming-guide/strings/string_program_1.cs
+++ snippets/csharp/programming-guide/strings/string_program_1.cs
@@ -6,40 +6,28 @@ namespace StringCompare
     {
         static void Main()
         {
-            string jupiterFirst = "Jupiter";
-            string jupiterLast = "Hammon";
-            int jupiterBorn = 1711;
-            int jupiterPub = 1761;
-            // Variables
-            Console.WriteLine($"{jupiterFirst} {jupiterLast} was an African American poet born in {jupiterBorn}.");
-            // Simple expression
-            Console.WriteLine($"He was first published in {jupiterPub} at the age of {jupiterPub - jupiterBorn}.");
-            // Complex Expressions
-            Console.WriteLine($"He'd be over {Math.Round((2018d - jupiterBorn) / 100d, 0) * 100d} years old today.");
+            // Declare Named Tuple
+            var pw = (firstName: "Phillis", lastName: "Wheatley", born: 1753, published: 1773);
+            Console.WriteLine("{0} {1} was an African American poet born in {2}.", pw.firstName, pw.lastName, pw.born);
+            Console.WriteLine("She was first published in {0} at the age of {1}.", pw.published, pw.published - pw.born);
+            Console.WriteLine("She'd be over {0} years old today.", Math.Round((2018d - pw.born) / 100d) * 100d);
+
+            // Output:
+            //Phillis Wheatley was an African American poet born in 1753.
+            //She was first published in 1773 at the age of 20.
+            //She'd be over 300 years old today.
+
+            Console.WriteLine();
+
+            var jh = (firstName: "Jupiter", lastName: "Hammon", born: 1711, published: 1761);
+            Console.WriteLine($"{jh.firstName} {jh.lastName} was an African American poet born in {jh.born}.");
+            Console.WriteLine($"He was first published in {jh.published} at the age of {jh.published - jh.born}.");
+            Console.WriteLine($"He'd be over {Math.Round((2018d - jh.born) / 100d) * 100d} years old today.");
 
             // Output:
             // Jupiter Hammon was an African American poet born in 1711.
             // He was first published in 1761 at the age of 50.
             // He'd be over 300 years old today. 
-
-            System.Console.WriteLine();
-
-            string phillisFirst = "Phillis";
-            string phillisLast = "Wheatley";
-            int phillisBorn = 1753;
-            int phillisPub = 1773;
-
-            // Variables
-            Console.WriteLine("{0} {1} was an African American poet born in {2}.", phillisFirst, phillisLast, phillisBorn);
-            // Simple expression
-            Console.WriteLine("She was first published in {0} at the age of {1}.", phillisPub, phillisPub - phillisBorn);
-            // Complex Expressions
-            Console.WriteLine("She'd be over {0} years old today.", Math.Round((2018d - phillisBorn) / 100d) * 100d);
-
-            // Output:
-            // Phillis Wheatley was an African American poet born in 1753.
-            // She was first published in 1773 at the age of 20.
-            // She'd be over 300 years old today.
         }
     }
 }
