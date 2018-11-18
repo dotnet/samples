833e56a6375b59ec945211adaec86189e91052ba
谭九鼎 <109224573@qq.com>
2018-11-18 23:24:02 +0800

    Update bom1.vb

--- snippets/visualbasic/VS_Snippets_CLR_System/system.Text.UnicodeEncoding Example/VB/bom1.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.Text.UnicodeEncoding Example/VB/bom1.vb
@@ -45,7 +45,7 @@ Class Example
         fs.Read(bytes, 0, fs.Length)
         fs.Close()
 
-        Dim decodedString As String = unicode.GetString(encodedBytes)
+        Dim decodedString As String = unicode.GetString(bytes)
         Console.WriteLine("Decoded bytes:")
         Console.WriteLine(decodedString)
     End Sub
