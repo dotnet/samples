a6d77fa28828637990b2b2510b2c54613d10be1b
Mike Rousos <mikerou@microsoft.com>
2018-11-19 11:58:24 -0500

    WinForms DataGridView sample (#456)
    * Initial copy from https://code.msdn.microsoft.com/windowsdesktop/CSWinFormDataGridView-29783221
    
    * Upgrade sln
    
    * Migrate csproj and remove a couple unused files
    
    * Work around dotnet/corefx#26745
    
    * Replace description.html with README.md
    
    * Remove MSLPL license since this will now be covered by the MIT license in the repository root
    
    * Add screenshots to README
    
    * Remove license references in source comments since the sample now uses the repo's MIT license
    
    * Reformat old ReadMe.txt files into markdown and cleanup links
    
    *  Small style improvements based on PR feedback

---
+++ windowsforms/datagridview/CSWinFormDataGridView/MultipleLayeredColumnHeader/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CSWinFormDataGridView.MultipleLayeredColumnHeader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(621, 389);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(626, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "This sample demonstrates how to display multiple layer column headers on the Data" +
+                "GridView.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 473);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
