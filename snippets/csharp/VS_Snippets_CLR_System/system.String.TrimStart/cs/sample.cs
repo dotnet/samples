2098cf3eb681b1807c8cbac6419f6ca818c21e53
Jonathan Markman <jmarkman@cleverdevices.com>
2018-11-20 17:09:07 -0500

    made a larger TrimStart example in C#, mirrored in VB.NET - covered individual trimming - covered trimming multiple items - covered trimming a singular item with multiple leading chars

--- snippets/csharp/VS_Snippets_CLR_System/system.String.TrimStart/cs/sample.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.String.TrimStart/cs/sample.cs
@@ -60,16 +60,45 @@ public class TrimExample
    // <Snippet1>
    public static void Main(string[] args)
    {
+      // TrimStart examples
 	string lineWithLeadingSpaces = "   Hello World!";
-		
-	Console.WriteLine($"This line has leading spaces: {lineWithLeadingSpaces}");
-	// This line has leading spaces:    Hello World!
-	
-	// Apply String.TrimStart to the variable in question
-	string lineAfterTrimStart = lineWithLeadingSpaces.TrimStart(' ');
-	
+	string lineWithLeadingSymbols = "$$$$Hello World!";
+      string lineWithLeadingUnderscores = "_____Hello World!";
+      string lineWithLeadingLetters = "xxxxHello World!";
+      string lineAfterTrimStart = string.Empty; 
+
+      // Make it easy to print out and work with all of the examples
+      string[] lines = { lineWithLeadingSpaces, lineWithLeadingSymbols, lineWithLeadingUnderscores, lineWithLeadingLetters };
+          
+      foreach (var line in lines)
+      {
+          Console.WriteLine($"This line has leading characters: {line}");
+      }
+      // Output:
+      // This line has leading characters:    Hello World!
+      // This line has leading characters: $$$$Hello World!
+      // This line has leading characters: _____Hello World!
+      // This line has leading characters: xxxxHello World!
+			
+      // A basic demonstration of TrimStart in action
+	lineAfterTrimStart = lineWithLeadingSpaces.TrimStart(' ');
 	Console.WriteLine($"This is the result after calling TrimStart: {lineAfterTrimStart}");
-	// This is the result after calling TrimStart: Hello World!    
+	// This is the result after calling TrimStart: Hello World!   
+
+      // Since TrimStart accepts a character array of leading items to be removed as an argument,
+      // it's possible to do things like trim multiple pieces of data that each have different 
+      // leading characters,
+      foreach (var lineToEdit in lines)
+      {
+          Console.WriteLine(lineToEdit.TrimStart(' ', '$', '_', 'x'));
+      }
+      // Result for each: Hello World!
+
+      // or handle pieces of data that have multiple kinds of leading characters 
+      var lineToBeTrimmed = "__###__ John Smith";
+      lineAfterTrimStart = lineToBeTrimmed.TrimStart('_', '#', ' ');
+      Console.WriteLine(lineAfterTrimStart);
+      // Result: John Smith
    }
    // </Snippet1>
 }
--- snippets/visualbasic/VS_Snippets_CLR_System/system.String.TrimStart/vb/sample.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.String.TrimStart/vb/sample.vb
@@ -50,17 +50,47 @@ Public Class TrimExample
    ' </Snippet2>
    ' <Snippet1>
    Public Sub Main()
-      Dim lineWithLeadingSpaces = "   Hello World!"
+      ' TrimStart Examples
+      Dim lineWithLeadingSpaces as String = "   Hello World!";
+	    Dim lineWithLeadingSymbols as String = "$$$$Hello World!";
+      Dim lineWithLeadingUnderscores as String = "_____Hello World!";
+      Dim lineWithLeadingLetters as String = "xxxxHello World!";
       Dim lineAfterTrimStart = String.Empty
+
+      ' Make it easy to print out and work with all of the examples
+      Dim lines As String() = { lineWithLeadingSpaces, line lineWithLeadingSymbols, lineWithLeadingUnderscores, lineWithLeadingLetters }
+
+      For Each line As String in lines
+        Console.WriteLine($"This line has leading characters: {line}")
+      Next
+      ' Output:
+      ' This line has leading characters:    Hello World!
+      ' This line has leading characters: $$$$Hello World!
+      ' This line has leading characters: _____Hello World!
+      ' This line has leading characters: xxxxHello World!
 
       Console.WriteLine($"This line has leading spaces: {lineWithLeadingSpaces}")
       ' This line has leading spaces:   Hello World!
 
-      ' Apply String.TrimStart to the variable in question
+      ' A basic demonstration of TrimStart in action
       lineAfterTrimStart = lineWithLeadingSpaces.TrimStart(" "c)
-
       Console.WriteLine($"This is the result after calling TrimStart: {lineAfterTrimStart}")
       ' This is the result after calling TrimStart: Hello World!
+
+      ' Since TrimStart accepts a character array of leading items to be removed as an argument,
+      ' it's possible to do things like trim multiple pieces of data that each have different 
+      ' leading characters,
+      For Each lineToEdit As String in lines
+        Console.WriteLine(lineToEdit.TrimStart(" "c, "$"c, "_"c, "x"c ))
+      Next
+      ' Result for each: Hello World!
+
+      ' or handle pieces of data that have multiple kinds of leading characters
+      Dim lineToBeTrimmed as String = "__###__ John Smith"
+      lineAfterTrimStart = lineToBeTrimmed.TrimStart("_"c , "#"c , " "c)
+      Console.WriteLine(lineAfterTrimStart)
+      ' Result: John Smith
+
     End Sub
    '</Snippet1>   
 End Class
