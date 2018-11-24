1fb2790bfafb43d628130090a31ad555aedd4aa3
Petr Kulikov <petr.kulikov@gmail.com>
2018-11-24 18:12:12 +0100

    Made a namespace example less confusing

--- snippets/csharp/VS_Snippets_VBCSharp/csrefKeywordsNamespace/CS/csrefKeywordsNamespace.cs
+++ snippets/csharp/VS_Snippets_VBCSharp/csrefKeywordsNamespace/CS/csrefKeywordsNamespace.cs
@@ -1,11 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
 
 //<snippet1>
-
 namespace SampleNamespace
 {
     class SampleClass { }
@@ -18,7 +17,7 @@ namespace SampleNamespace
 
     delegate void SampleDelegate(int i);
 
-    namespace SampleNamespace.Nested
+    namespace Nested
     {
         class SampleClass2 { }
     }
