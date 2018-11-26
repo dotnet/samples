8691448d68ec82ebdad128cd3305618029add4d9
Jonathan Markman <jmarkman446@gmail.com>
2018-11-26 10:21:45 -0500

    Update Code Example: Add elementary usage of TrimStart (#473)
    * clarification alignment with PR #9127
    - changed Program.cs to use anonymous object to match markdown snippet
    - added comment to logging extension method about AppendText behavior
    
    * added elementary usage of TrimStart method for c# and VB.NET
    
    * made a larger TrimStart example in C#, mirrored in VB.NET
    - covered individual trimming
    - covered trimming multiple items
    - covered trimming a singular item with multiple leading chars
    
    * used vs code's "format document"

--- snippets/visualbasic/VS_Snippets_CLR_System/system.String.TrimStart/vb/sample.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.String.TrimStart/vb/sample.vb
@@ -4,7 +4,7 @@ Option Strict On
 Imports System.Collections.Generic
 
 Public Class TrimExample
-   ' <Snippet2>
+   ' <Snippet3>
    Public Shared Sub Main()
       Dim lines() As String = {"Public Module HelloWorld", _
                                "   Public Sub Main()", _
@@ -36,8 +36,8 @@ Public Class TrimExample
    '    Code after call to StripComments:
    '       This code displays a simple greeting
    '       to the console.   
-   ' </Snippet2>
-   ' <Snippet1>
+   ' </Snippet3>
+   ' <Snippet2>
    Public Shared Function StripComments(lines() As String) As String()
       Dim lineList As New List(Of String)
       For Each line As String In lines
@@ -47,6 +47,51 @@ Public Class TrimExample
       Next
       Return lineList.ToArray()
    End Function   
-   ' </Snippet1>   
+   ' </Snippet2>
+   ' <Snippet1>
+   Public Sub Main()
+      ' TrimStart Examples
+      Dim lineWithLeadingSpaces as String = "   Hello World!";
+	    Dim lineWithLeadingSymbols as String = "$$$$Hello World!";
+      Dim lineWithLeadingUnderscores as String = "_____Hello World!";
+      Dim lineWithLeadingLetters as String = "xxxxHello World!";
+      Dim lineAfterTrimStart = String.Empty
+
+      ' Make it easy to print out and work with all of the examples
+      Dim lines As String() = { lineWithLeadingSpaces, line lineWithLeadingSymbols, lineWithLeadingUnderscores, lineWithLeadingLetters }
+
+      For Each line As String in lines
+        Console.WriteLine($"This line has leading characters: {line}")
+      Next
+      ' Output:
+      ' This line has leading characters:    Hello World!
+      ' This line has leading characters: $$$$Hello World!
+      ' This line has leading characters: _____Hello World!
+      ' This line has leading characters: xxxxHello World!
+
+      Console.WriteLine($"This line has leading spaces: {lineWithLeadingSpaces}")
+      ' This line has leading spaces:   Hello World!
+
+      ' A basic demonstration of TrimStart in action
+      lineAfterTrimStart = lineWithLeadingSpaces.TrimStart(" "c)
+      Console.WriteLine($"This is the result after calling TrimStart: {lineAfterTrimStart}")
+      ' This is the result after calling TrimStart: Hello World!
+
+      ' Since TrimStart accepts a character array of leading items to be removed as an argument,
+      ' it's possible to do things like trim multiple pieces of data that each have different 
+      ' leading characters,
+      For Each lineToEdit As String in lines
+        Console.WriteLine(lineToEdit.TrimStart(" "c, "$"c, "_"c, "x"c ))
+      Next
+      ' Result for each: Hello World!
+
+      ' or handle pieces of data that have multiple kinds of leading characters
+      Dim lineToBeTrimmed as String = "__###__ John Smith"
+      lineAfterTrimStart = lineToBeTrimmed.TrimStart("_"c , "#"c , " "c)
+      Console.WriteLine(lineAfterTrimStart)
+      ' Result: John Smith
+
+    End Sub
+   '</Snippet1>   
 End Class
 
