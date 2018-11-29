a2056cb9132f217ada224dd2ac4938652cd96ea2
Ricardo Minguez (Rido) <rido-min@users.noreply.github.com>
2018-11-28 17:35:19 -0800

    Add WinRT Sample (#387)
    * Add WinRT Sample
    
    * Add SDK instructions to README
    
    * Update README.md

---
+++ wpf/WPF-WinRT/WPF-WinRT-NetFx/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_WinRT_NetFx
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            label1.Content = new Windows.Devices.Input.TouchCapabilities().TouchPresent == 1 ?
+                                    "Touch Present" : "Touch not present";
+        }
+    }
+}
