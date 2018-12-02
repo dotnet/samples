e23ca5518285a675b5f16ee063604cc2956c626a
Bill Wagner <bill.w.wagner@outlook.com>
2018-12-02 14:21:18 -0500

    first draft of code sample
    Contributes to dotnet/docs#8208

---
+++ csharp/NullableIntroduction/NullableIntroduction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+#nullable enable
+namespace NullableIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var surveyRun = new SurveyRun();
+            surveyRun.AddQuestion(QuestionType.YesNo, "Has your code ever thrown a NullReferenceException?");
+            surveyRun.AddQuestion(new SurveyQuestion(QuestionType.Number, "How many times (to the nearest 100) has that happened?"));
+            surveyRun.AddQuestion(QuestionType.Text, "What is your favorite color?");
+
+            surveyRun.PerformSurvey(50);
+
+            // snippet
+            foreach (var participant in surveyRun.AllParticipants)
+            {
+                Console.WriteLine($"Participant: {participant.Id}:");
+                if (participant.AnsweredSurvey)
+                {
+                    for (int i = 0; i < surveyRun.Questions.Count; i++)
+                    {
+                        var answer = participant.Answer(i);
+                        Console.WriteLine($"{surveyRun.GetQuestion(i)} : {answer}");
+                    }
+                }
+                else
+                    Console.WriteLine("No responses");
+            }
+            // end
+
+        }
+    }
+}
---
+++ csharp/NullableIntroduction/NullableIntroduction/SurveyResponse.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#nullable enable
+namespace NullableIntroduction
+{
+    public class SurveyResponse
+    {
+        private static readonly Random randomGenerator = new Random();
+        public static SurveyResponse GetRandomId() => new SurveyResponse(randomGenerator.Next());
+
+        public int Id { get; }
+
+        public bool AnsweredSurvey => surveyResponses != null;
+        public string Answer(int index) => surveyResponses.GetValueOrDefault(index, "No answer");
+
+        public SurveyResponse(int id) => Id = id;
+
+        private Dictionary<int, string>? surveyResponses;
+        public bool AnswerSurvey(IEnumerable<SurveyQuestion> questions)
+        {
+            if (ConsentToSurvey())
+            {
+                surveyResponses = new Dictionary<int, string>();
+                int index = 0;
+                foreach (var question in questions)
+                {
+                    var answer = GenerateAnswer(question);
+                    if (answer != null)
+                    {
+                        surveyResponses.Add(index, answer);
+                    }
+                    index++;
+                }
+            }
+            return surveyResponses != null;
+        }
+
+        private bool ConsentToSurvey() => randomGenerator.Next(0, 2) == 1;
+
+        private string? GenerateAnswer(SurveyQuestion question)
+        {
+            switch (question.TypeOfQuestion)
+            {
+                case QuestionType.YesNo:
+                    int n = randomGenerator.Next(-1, 2);
+                    return (n == -1) ? default : (n == 0) ? "No" : "Yes";
+                case QuestionType.Number:
+                    n = randomGenerator.Next(-30, 101);
+                    return (n < 0) ? default : n.ToString();
+                case QuestionType.Text:
+                default:
+                    switch (randomGenerator.Next(0, 5))
+                    {
+                        case 0:
+                            return default;
+                        case 1:
+                            return "Red";
+                        case 2:
+                            return "Green";
+                        case 3:
+                            return "Blue";
+                    }
+                    return "Red. No, Green. Wait.. Blue... AAARGGGGGHHH!";
+            }
+        }
+    }
+}
