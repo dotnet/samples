f51c379ff109d94dd8904310337bff8e50df4125
Petr Onderka <gsvick@gmail.com>
2018-12-02 16:57:38 +0100

    Correctly dispose HttpClient in its example

--- snippets/csharp/VS_Snippets_Misc/system.net.http.httpclient/cs/source.cs
+++ snippets/csharp/VS_Snippets_Misc/system.net.http.httpclient/cs/source.cs
@@ -9,29 +9,26 @@ class HttpClient_Example
 // <Snippet1>
    static async Task Main()
    {
-      // Create a New HttpClient object.
-      HttpClient client = new HttpClient();
-    
-      // Call asynchronous network methods in a try/catch block to handle exceptions
-      try	
+      // Create a New HttpClient object and dispose it when done, so the app doesn't leak resources
+      using (HttpClient client = new HttpClient())
       {
-         HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
-         response.EnsureSuccessStatusCode();
-         string responseBody = await response.Content.ReadAsStringAsync();
-         // Above three lines can be replaced with new helper method below
-         // string responseBody = await client.GetStringAsync(uri);
+         // Call asynchronous network methods in a try/catch block to handle exceptions
+         try	
+         {
+            HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            // Above three lines can be replaced with new helper method below
+            // string responseBody = await client.GetStringAsync(uri);
 
-         Console.WriteLine(responseBody);
-      }  
-      catch(HttpRequestException e)
-      {
-         Console.WriteLine("\nException Caught!");	
-         Console.WriteLine("Message :{0} ",e.Message);
+            Console.WriteLine(responseBody);
+         }  
+         catch(HttpRequestException e)
+         {
+            Console.WriteLine("\nException Caught!");	
+            Console.WriteLine("Message :{0} ",e.Message);
+         }
       }
-
-      // Need to call dispose on the HttpClient object
-      // when done using it, so the app doesn't leak resources
-      client.Dispose(true);
    }
 // </Snippet1>			
 }
