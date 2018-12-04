b6476e4b3ab323619c5677602662c019e0b64048
Aaron Robinson <arobins@microsoft.com>
2018-12-04 14:21:52 -0800

    Add demo for Excel automation in .NET Core 3.0 (#487)
    * Add demo for Excel automation in .NET Core 3.0
    
    * Feedback
    
    * Update core/extensions/ExcelDemo/ReadMe.md
    
    Co-Authored-By: AaronRobinsonMSFT <arobins@microsoft.com>
    
    * Update core/extensions/ExcelDemo/ReadMe.md
    
    Co-Authored-By: AaronRobinsonMSFT <arobins@microsoft.com>
    
    * Feedback

---
+++ core/extensions/ExcelDemo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Excel.Application excel;
+            Excel.Workbook workbook;
+            Excel.Worksheet sheet;
+            Excel.Range range;
+
+            try
+            {
+                // Start Excel and get Application object.
+                excel = new Excel.Application();
+                excel.Visible = true;
+
+                // Get a new workbook.
+                workbook = excel.Workbooks.Add(Missing.Value);
+                sheet = (Excel.Worksheet)workbook.ActiveSheet;
+
+                // Add table headers going cell by cell.
+                sheet.Cells[1, 1] = "First Name";
+                sheet.Cells[1, 2] = "Last Name";
+                sheet.Cells[1, 3] = "Full Name";
+                sheet.Cells[1, 4] = "Salary";
+
+                // Format A1:D1 as bold, vertical alignment = center.
+                sheet.get_Range("A1", "D1").Font.Bold = true;
+                sheet.get_Range("A1", "D1").VerticalAlignment =
+                Excel.XlVAlign.xlVAlignCenter;
+
+                // Create an array to multiple values at once.
+                string[,] saNames = new string[5, 2];
+
+                saNames[0, 0] = "John";
+                saNames[0, 1] = "Smith";
+                saNames[1, 0] = "Tom";
+                saNames[1, 1] = "Brown";
+                saNames[2, 0] = "Sue";
+                saNames[2, 1] = "Thomas";
+                saNames[3, 0] = "Jane";
+                saNames[3, 1] = "Jones";
+                saNames[4, 0] = "Adam";
+                saNames[4, 1] = "Johnson";
+
+                // Fill A2:B6 with an array of values (First and Last Names).
+                sheet.get_Range("A2", "B6").Value2 = saNames;
+
+                // Fill C2:C6 with a relative formula (=A2 & " " & B2).
+                range = sheet.get_Range("C2", "C6");
+                range.Formula = "=A2 & \" \" & B2";
+
+                // Fill D2:D6 with a formula(=RAND()*100000) and apply format.
+                range = sheet.get_Range("D2", "D6");
+                range.Formula = "=RAND()*100000";
+                range.NumberFormat = "$0.00";
+
+                // AutoFit columns A:D.
+                range = sheet.get_Range("A1", "D1");
+                range.EntireColumn.AutoFit();
+
+                // Make sure Excel is visible and give the user control
+                // of Microsoft Excel's lifetime.
+                excel.Visible = true;
+                excel.UserControl = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message} Line: {e.Source}");
+            }
+        }
+    }
+}
---
+++ core/extensions/ExcelDemo/ReadMe.md
@@ -0,0 +1,30 @@
+Excel Demo
+================
+
+This is a basic Excel demo sample in .NET Core. It is designed to work with .NET Core 3.0. It is based on the [How to automate Microsoft Excel from Microsoft Visual C#.NET](https://support.microsoft.com/en-us/help/302084/how-to-automate-microsoft-excel-from-microsoft-visual-c-net) sample for .NET Framework.
+
+Key Features
+------------
+
+Demonstrates how to consume Office Primary Interop Assemblies (PIA) with .NET Core 3.0 Preview 1.
+
+- Embedding of Interop types (i.e. [No-PIA](https://docs.microsoft.com/en-us/dotnet/framework/interop/type-equivalence-and-embedded-interop-types)).
+- Support for the [`IDispatch`](https://docs.microsoft.com/en-us/windows/desktop/winauto/idispatch-interface) interface.
+
+**Note** Adding COM references to .NET Core projects from Visual Studio is not currently supported. The workaround is to create a .NET Framework project, add the COM references, and then copy the relevant `COMReference` elements in the project. See `ExcelDemo.csproj` for further details.
+
+Build and Run
+-------------
+
+To build and run the sample, the project must be loaded in Visual Studio 2017. Build support for `COMReference` elements is not supported in the `dotnet` tool for the Preview 1 release, but the scenario is demoable from within Visual Studio 2017.
+
+1) Install .NET Core 3.0 Preview 1.
+
+1) Load `ExcelDemo.csproj` in Visual Studio 2017.
+    - Double click on `ExcelDemo.csproj` in File Explorer.
+
+    or
+
+    - Open a Developer Command prompt and open with `devenv.exe ExcelDemo.csproj`.
+
+1) Press F5 to build and debug the project.
