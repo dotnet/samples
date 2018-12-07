cf49273a0c715c99c9c69eb82b17bffc66a48c4d
Andy De George <thraka.junk@outlook.com>
2018-12-07 13:36:14 -0800

    Tighten up CS regex email sample

--- snippets/csharp/VS_Snippets_CLR/RegularExpressions.Examples.Email/cs/example4.cs
+++ snippets/csharp/VS_Snippets_CLR/RegularExpressions.Examples.Email/cs/example4.cs
@@ -5,52 +5,48 @@ using System.Text.RegularExpressions;
 
 public class RegexUtilities
 {
-   bool invalid = false;
+    public static bool IsValidEmail(string email)
+    {
+        if (string.IsNullOrEmpty(email))
+            return false;
 
-   public bool IsValidEmail(string strIn)
-   {
-       invalid = false;
-       if (String.IsNullOrEmpty(strIn))
-          return false;
+        // Use IdnMapping class to convert Unicode domain names.
+        try
+        {
+            // Examines the domain part of the email and normalizes it.
+            string DomainMapper(Match match)
+            {
+                // IdnMapping class with default property values.
+                var idn = new IdnMapping();
 
-       // Use IdnMapping class to convert Unicode domain names.
-       try {
-          strIn = Regex.Replace(strIn, @"(@)(.+)$", this.DomainMapper,
-                                RegexOptions.None, TimeSpan.FromMilliseconds(200));
-       }
-       catch (RegexMatchTimeoutException) {
-         return false;
-       }
+                // Pull out and process domain name (throws ArgumentException on invalid)
+                var domainName = idn.GetAscii(match.Groups[2].Value);
 
-        if (invalid)
-           return false;
+                return match.Groups[1].Value + domainName;
+            }
 
-       // Return true if strIn is in valid email format.
-       try {
-          return Regex.IsMatch(strIn,
+            // Normalize the domain
+            email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                  RegexOptions.None, TimeSpan.FromMilliseconds(200));
+        }
+        catch (Exception e) when (e is RegexMatchTimeoutException || e is ArgumentException)
+        {
+            return false;
+        }
+
+        // Return true if email is valid
+        try
+        {
+            return Regex.IsMatch(email,
                 @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
                 @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
                 RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
-       }
-       catch (RegexMatchTimeoutException) {
-          return false;
-       }
-   }
-
-   private string DomainMapper(Match match)
-   {
-      // IdnMapping class with default property values.
-      IdnMapping idn = new IdnMapping();
-
-      string domainName = match.Groups[2].Value;
-      try {
-         domainName = idn.GetAscii(domainName);
-      }
-      catch (ArgumentException) {
-         invalid = true;
-      }
-      return match.Groups[1].Value + domainName;
-   }
+        }
+        catch (RegexMatchTimeoutException)
+        {
+            return false;
+        }
+    }
 }
 // </Snippet7>
 
@@ -59,7 +55,6 @@ public class Application
 {
    public static void Main()
    {
-      RegexUtilities util = new RegexUtilities();
       string[] emailAddresses = { "david.jones@proseware.com", "d.j@server1.proseware.com",
                                   "jones@ms1.proseware.com", "j.@server1.proseware.com",
                                   "j@proseware.com9", "js#internal@proseware.com",
@@ -69,7 +64,7 @@ public class Application
                                    "\"j\\\"s\\\"\"@proseware.com", "js@contoso.中国" };
 
       foreach (var emailAddress in emailAddresses) {
-         if (util.IsValidEmail(emailAddress))
+         if (RegexUtilities.IsValidEmail(emailAddress))
             Console.WriteLine("Valid: {0}", emailAddress);
          else
             Console.WriteLine("Invalid: {0}", emailAddress);
