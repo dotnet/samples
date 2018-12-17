b4adcf78318e91467dbb1424a8044d833e31816f
Petr Kulikov <petr.kulikov@gmail.com>
2018-12-17 17:25:06 +0100

    Source code for updated clustering tutorial (#521)

---
+++ machine-learning/tutorials/IrisFlowerClustering/IrisData.cs
@@ -0,0 +1,32 @@
+﻿// <SnippetUsings>
+using Microsoft.ML.Runtime.Api;
+// </SnippetUsings>
+
+namespace IrisFlowerClustering
+{
+    // <SnippetClassDefinitions>
+    public class IrisData
+    {
+        [Column("0")]
+        public float SepalLength;
+
+        [Column("1")]
+        public float SepalWidth;
+
+        [Column("2")]
+        public float PetalLength;
+
+        [Column("3")]
+        public float PetalWidth;
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+    // </SnippetClassDefinitions>
+}
