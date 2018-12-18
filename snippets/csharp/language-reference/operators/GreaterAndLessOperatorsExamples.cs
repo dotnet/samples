9e14721eb2f68e14cfaa9743a39299252315c4ed
Petr Kulikov <petr.kulikov@gmail.com>
2018-12-18 17:14:07 +0100

    Added snippets for greater and less operators (#523)

---
+++ snippets/csharp/language-reference/operators/GreaterAndLessOperatorsExamples.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace operators
+{
+    public static class GreaterAndLessOperatorsExamples
+    {
+        public static void Examples()
+        {
+            Console.WriteLine("--- >");
+            Greater();
+            Console.WriteLine("--- <");
+            Less();
+            Console.WriteLine("--- >=");
+            GreaterOrEqual();
+            Console.WriteLine("--- <=");
+            LessOrEqual();
+        }
+
+        private static void Greater()
+        {
+            // <SnippetGreater>
+            Console.WriteLine(7.0 > 5.1);   // output: True
+            Console.WriteLine(5.1 > 5.1);   // output: False
+            Console.WriteLine(0.0 > 5.1);   // output: False
+            // </SnippetGreater>
+        }
+
+        private static void Less()
+        {
+            // <SnippetLess>
+            Console.WriteLine(7.0 < 5.1);   // output: False
+            Console.WriteLine(5.1 < 5.1);   // output: False
+            Console.WriteLine(0.0 < 5.1);   // output: True
+            // </SnippetLess>
+        }
+
+        private static void GreaterOrEqual()
+        {
+            // <SnippetGreaterOrEqual>
+            Console.WriteLine(7.0 >= 5.1);   // output: True
+            Console.WriteLine(5.1 >= 5.1);   // output: True
+            Console.WriteLine(0.0 >= 5.1);   // output: False
+            // </SnippetGreaterOrEqual>
+        }
+
+        private static void LessOrEqual()
+        {
+            // <SnippetLessOrEqual>
+            Console.WriteLine(7.0 <= 5.1);   // output: False
+            Console.WriteLine(5.1 <= 5.1);   // output: True
+            Console.WriteLine(0.0 <= 5.1);   // output: True
+            // </SnippetLessOrEqual>
+        }
+    }
+}
