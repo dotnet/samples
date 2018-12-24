f143f75700be42dd7b7d3ea2432395191f6b2183
lindexi <lindexi_gd@163.com>
2018-12-24 19:59:17 +0800

    Format code
    In [coding-style.md](https://github.com/dotnet/corefx/blob/master/Documentation/coding-guidelines/coding-style.md ) NO.1, we use Allman style braces, where each brace begins on a new line.

--- snippets/csharp/VS_Snippets_Wpf/MessageBoxSnippets/CSharp/Show3Window.xaml.cs
+++ snippets/csharp/VS_Snippets_Wpf/MessageBoxSnippets/CSharp/Show3Window.xaml.cs
@@ -1,23 +1,27 @@
 using System;
 using System.Windows;
 
-namespace CSharp {
+namespace CSharp
+{
 
-  public partial class Show3Window : Window {
+    public partial class Show3Window : Window
+    {
 
-    public Show3Window() {
-      InitializeComponent();
+        public Show3Window()
+        {
+            InitializeComponent();
+        }
+
+        //<SnippetMessageBoxShow3CODE>
+        void showMessageBoxButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure message box
+            string message = "Hello, MessageBox!";
+            string caption = "Caption text";
+            MessageBoxButton buttons = MessageBoxButton.OKCancel;
+            // Show message box
+            MessageBoxResult result = MessageBox.Show(message, caption, buttons);
+        }
+        //</SnippetMessageBoxShow3CODE>
     }
-
-    //<SnippetMessageBoxShow3CODE>
-    void showMessageBoxButton_Click(object sender, RoutedEventArgs e) {
-      // Configure message box
-      string message = "Hello, MessageBox!";
-      string caption = "Caption text";
-      MessageBoxButton buttons = MessageBoxButton.OKCancel;
-      // Show message box
-      MessageBoxResult result = MessageBox.Show(message, caption, buttons);
-    }
-    //</SnippetMessageBoxShow3CODE>
-  }
 }
