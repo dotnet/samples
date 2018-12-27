0eb9631e4dadce48fcd0bcfc48e3f5d47c6c21a6
Bill Wagner <wiwagn@microsoft.com>
2018-12-27 10:09:16 -0500

    change tag name

--- snippets/csharp/new-in-6/initializers.cs
+++ snippets/csharp/new-in-6/initializers.cs
@@ -4,14 +4,14 @@ namespace Initializers
 {
     public class Examples
     {
-        // <ListInitializer>
+        // <CollectionInitializer>
         private Dictionary<int, string> messages = new Dictionary<int, string>
         {
             { 404, "Page not Found"},
             { 302, "Page moved, but left a forwarding address."},
             { 500, "The web server can't come out to play today."}
         };
-        // </ListInitializer>
+        // </CollectionInitializer>
 
         // <DictionaryInitializer>
         private Dictionary<int, string> webErrors = new Dictionary<int, string>
