36dcfaba44bd60ba41a5cf781cfe96f3a9eb8a03
volkan-yazilim <volkan.akkus@metu.edu.tr>
2019-01-05 20:09:47 +0300

    Change to improve readability

--- snippets/csharp/VS_Snippets_VBCSharp/csrefKeywordsModifiers/CS/csrefKeywordsModifiers.cs
+++ snippets/csharp/VS_Snippets_VBCSharp/csrefKeywordsModifiers/CS/csrefKeywordsModifiers.cs
@@ -716,10 +716,12 @@ namespace csrefKeywordsModifiers
         static void Main()
         {
             double r = 3.0, h = 5.0;
-            Shape c = new Circle(r);
-            Shape s = new Sphere(r);
-            Shape l = new Cylinder(r, h);
+            Shape t = new Shape(r, h);
+            Circle c = new Circle(r);
+            Sphere s = new Sphere(r);
+            Cylinder l = new Cylinder(r, h);
             // Display results:
+            Console.WriteLine("Area of Shape   = {0:F2}", t.Area());
             Console.WriteLine("Area of Circle   = {0:F2}", c.Area());
             Console.WriteLine("Area of Sphere   = {0:F2}", s.Area());
             Console.WriteLine("Area of Cylinder = {0:F2}", l.Area());
@@ -727,9 +729,10 @@ namespace csrefKeywordsModifiers
     }
     /*
     Output:
-    Area of Circle   = 28.27
-    Area of Sphere   = 113.10
-    Area of Cylinder = 150.80
+    Area of Shape   = 15,00
+    Area of Circle   = 28,27
+    Area of Sphere   = 113,10
+    Area of Cylinder = 150,80
     */
     //</snippet23>
 
