ad09bd6c1f09ca4f8b0e50e52949e7b3cedcfdd2
Ron Petrusha <ronpet@microsoft.com>
2019-01-11 15:39:11 -0800

    Minor code revisions

--- snippets/csharp/framework/migration-guide/versions-installed1.cs
+++ snippets/csharp/framework/migration-guide/versions-installed1.cs
@@ -10,56 +10,62 @@ public static class VersionTest
     
     private static void GetVersionFromRegistry()
     {
-         // Opens the registry key for the .NET Framework entry.
-            using (RegistryKey ndpKey = 
-                RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "").
+        // Opens the registry key for the .NET Framework entry.
+        using (RegistryKey ndpKey = 
+                RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).
                 OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+        {
+            foreach (var versionKeyName in ndpKey.GetSubKeyNames())
             {
-                // As an alternative, if you know the computers you will query are running .NET Framework 4.5 
-                // or later, you can use:
-                // using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, 
-                // RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
-            foreach (string versionKeyName in ndpKey.GetSubKeyNames())
-            {
+                // Skip .NET Framework 4.5 version information.
+                if (versionKeyName == "v4")
+                {
+                    continue;
+                }
+
                 if (versionKeyName.StartsWith("v"))
                 {
 
                     RegistryKey versionKey = ndpKey.OpenSubKey(versionKeyName);
-                    string name = (string)versionKey.GetValue("Version", "");
-                    string sp = versionKey.GetValue("SP", "").ToString();
-                    string install = versionKey.GetValue("Install", "").ToString();
-                    if (install == "") //no install info, must be later.
-                        Console.WriteLine(versionKeyName + "  " + name);
+                    // Get the .NET Framework version value.
+                    var name = (string)versionKey.GetValue("Version", "");
+                    // Get the service pack (SP) number.
+                    var sp = versionKey.GetValue("SP", "").ToString();
+
+                    // Get the installation flag, or an empty string if there is none.
+                    var install = versionKey.GetValue("Install", "").ToString();
+                    if (install == "") // No install info; it must be in a child subkey.
+                        Console.WriteLine($"{versionKeyName}  {name}");
                     else
                     {
                         if (sp != "" && install == "1")
                         {
-                            Console.WriteLine(versionKeyName + "  " + name + "  SP" + sp);
+                            Console.WriteLine($"{versionKeyName}  {name}  SP{sp}");
                         }
-
                     }
                     if (name != "")
                     {
                         continue;
                     }
-                    foreach (string subKeyName in versionKey.GetSubKeyNames())
+                    foreach (var subKeyName in versionKey.GetSubKeyNames())
                     {
                         RegistryKey subKey = versionKey.OpenSubKey(subKeyName);
                         name = (string)subKey.GetValue("Version", "");
                         if (name != "")
                             sp = subKey.GetValue("SP", "").ToString();
+                        
                         install = subKey.GetValue("Install", "").ToString();
-                        if (install == "") //no install info, must be later.
-                            Console.WriteLine(versionKeyName + "  " + name);
+                        if (install == "") //No install info; it must be later.
+                            Console.WriteLine($"{versionKeyName}  {name}");
                         else
                         {
                             if (sp != "" && install == "1")
                             {
-                                Console.WriteLine("  " + subKeyName + "  " + name + "  SP" + sp);
+                                Console.WriteLine($"{subKeyName}  {name}  SP{sp}");
                             }
                             else if (install == "1")
                             {
-                                Console.WriteLine("  " + subKeyName + "  " + name);
+                                Console.WriteLine($"  {subKeyName}  {name}");
                             }
                         }
                     }
@@ -68,3 +74,9 @@ public static class VersionTest
         }
     }
 }
+// The example displays output similar to the following:
+//        v2.0.50727  2.0.50727.4927  SP2
+//        v3.0  3.0.30729.4926  SP2
+//        v3.5  3.5.30729.4926  SP1
+//        v4.0
+//        Client  4.0.0.0
