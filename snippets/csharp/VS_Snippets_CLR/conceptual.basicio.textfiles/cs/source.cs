849a53e6ad33896672135eb9b2399ed0b480cd50
Theano Petersen <v-thepet@microsoft.com>
2019-01-16 17:25:52 -0800

    New [replaces Consolidating] samples for how-to-write-text-to-a-file (#543)
    * consolidating samples
    
    * another fix in my opinion
    
    * putting back the snippet markers
    
    * new text-file samples
    
    * remove My
    
    * changes
    
    * corrections
    
    * add the vb code
    
    * Updated comment.

--- snippets/csharp/VS_Snippets_CLR/conceptual.basicio.textfiles/cs/source.cs
+++ snippets/csharp/VS_Snippets_CLR/conceptual.basicio.textfiles/cs/source.cs
@@ -8,92 +8,95 @@ class WriteTextFiles
     static void Main(string[] args)
     {
 
-        // Example 1: shows how to write synchronously
-        // to a text file using StreamWriter
+        // Example 1: Shows how to synchronously write to
+        // a text file using StreamWriter
         // line by line
         WriteLineByLine();
 
-        // Example 2: Appending text to
+        // Example 2: Shows how to synchronously append text to
         // an existing file using StreamWriter
         AppendTextSW();
 
-        // Example 3: shows how to write a 
-        // a simple string asynchronously
-        // to a text file using StreamWriter
-        string text = "This is a sentence.";
-        WriteTextAsync(text);
+        // Example 3: Shows how to asynchronously write to
+        // a text file using StreamWriter
+        WriteTextAsync();
 
-        // Example 4: shows how to write synchronously
-        // to a text file using File and then
-        // adding additional lines 
+        // Example 4: Shows how to synchronously write and
+        // append to a text file using File  
         WriteFile();
     }
 
+    // Example 1: Write line by line to a new text file with StreamWriter
     static void WriteLineByLine()
     {
         // <SnippetWriteLine>
+        
         // Create a string array with the lines of text
         string[] lines = { "First line", "Second line", "Third line" };
 
-        // Set a variable to the My Documents path.
-        string mydocpath =
+        // Set a variable to the Documents path.
+        string docPath =
             Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
         // Write the string array to a new file named "WriteLines.txt".
-        using (StreamWriter outputFile = new StreamWriter(Path.Combine(mydocpath,"WriteLines.txt"))) {
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath,"WriteLines.txt"))) {
             foreach (string line in lines)
                 outputFile.WriteLine(line);
         }
         // </SnippetWriteLine>
-
     }
 
+    // Example 2: Append a line to a text file with StreamWriter
     static void AppendTextSW()
     {
         // <SnippetAppendText>
-        // Set a variable to the My Documents path.
-        string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        
+        // Set a variable to the Documents path.
+        string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
         // Append text to an existing file named "WriteLines.txt".
-        using (StreamWriter outputFile = new StreamWriter(Path.Combine(mydocpath,"WriteLines.txt"), true)) {
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath,"WriteLines.txt"), true)) {
             outputFile.WriteLine("Fourth Line");
         }
         // </SnippetAppendText>
-
     }
 
-    // <SnippetWriteAsync>
-    static async void WriteTextAsync(string text)
+    // Example 3: Write text asynchronously with StreamWriter
+    static async void WriteTextAsync()
     {
-        // Set a variable to the My Documents path.
-        string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        // <SnippetWriteAsync>
+        
+        // Set a variable to the Documents path.
+        string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
-        // Write the text asynchronously to a new file named "WriteTextAsync.txt".
-        using (StreamWriter outputFile = new StreamWriter(Path.Combine(mydocpath,"WriteTextAsync.txt"))) {
-            await outputFile.WriteAsync(text);
+        // Write the specified text asynchronously to a new file named "WriteTextAsync.txt".
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath,"WriteTextAsync.txt"))) {
+            await outputFile.WriteAsync("This is a sentence.");
         }
+        // </SnippetWriteAsync>
     }
-    // </SnippetWriteAsync>
 
+    // Example 4: Write and append text using File
     static void WriteFile()
     {
         // <SnippetWriteFile>
+        
         // Create a string array with the lines of text
         string text = "First line" + Environment.NewLine;
 
-        // Set a variable to the My Documents path.
-        string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        // Set a variable to the Documents path.
+        string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
         // Write the text to a new file named "WriteFile.txt".
-        File.WriteAllText(Path.Combine(mydocpath,"WriteFile.txt"), text);
+        File.WriteAllText(Path.Combine(docPath,"WriteFile.txt"), text);
 
         // Create a string array with the additional lines of text
         string[] lines = { "New line 1", "New line 2" };
 
         // Append new lines of text to the file
-        File.AppendAllLines(Path.Combine(mydocpath,"WriteFile.txt"), lines);
+        File.AppendAllLines(Path.Combine(docPath,"WriteFile.txt"), lines);
+        
         // </SnippetWriteFile>
-
     }
 
 }
