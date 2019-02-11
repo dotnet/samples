f77203122b093e850ac4f8b732edac851c22335e
Theano Petersen <v-thepet@microsoft.com>
2019-02-10 16:16:03 -0800

    code changes for 10411

--- snippets/visualbasic/VS_Snippets_Winforms/System.Windows.Forms.DataGridViewBoundEditable/VB/datagridviewboundeditable.vb
+++ snippets/visualbasic/VS_Snippets_Winforms/System.Windows.Forms.DataGridViewBoundEditable/VB/datagridviewboundeditable.vb
@@ -1,19 +1,17 @@
 '<snippet00>
-Imports System
-Imports System.Data
 Imports System.Data.SqlClient
-Imports System.Windows.Forms
+Imports System.Windows
 
 Public Class Form1
-    Inherits System.Windows.Forms.Form
+    Inherits Forms.Form
 
     Private dataGridView1 As New DataGridView()
     Private bindingSource1 As New BindingSource()
     Private dataAdapter As New SqlDataAdapter()
-    Private WithEvents reloadButton As New Button()
-    Private WithEvents submitButton As New Button()
+    Private WithEvents ReloadButton As New Button()
+    Private WithEvents SubmitButton As New Button()
 
-    <STAThreadAttribute()> _
+    <STAThreadAttribute()>
     Public Shared Sub Main()
         Application.Run(New Form1())
     End Sub
@@ -23,83 +21,79 @@ Public Class Form1
 
         Me.dataGridView1.Dock = DockStyle.Fill
 
-        Me.reloadButton.Text = "reload"
-        Me.submitButton.Text = "submit"
+        Me.ReloadButton.Text = "Reload"
+        Me.SubmitButton.Text = "Submit"
 
-        Dim panel As New FlowLayoutPanel()
-        panel.Dock = DockStyle.Top
-        panel.AutoSize = True
-        panel.Controls.AddRange(New Control() {Me.reloadButton, Me.submitButton})
+        Dim panel As New FlowLayoutPanel With {
+            .Dock = DockStyle.Top,
+            .AutoSize = True
+        }
+        panel.Controls.AddRange(New Control() {ReloadButton, SubmitButton})
 
-        Me.Controls.AddRange(New Control() {Me.dataGridView1, panel})
-        Me.Text = "DataGridView databinding and updating demo"
-
-    End Sub
-
-    '<snippet10>
-    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) _
-        Handles Me.Load
-
-        ' Bind the DataGridView to the BindingSource
-        ' and load the data from the database.
-        Me.dataGridView1.DataSource = Me.bindingSource1
-        GetData("select * from Customers")
-
-    End Sub
-    '</snippet10>
-
-    Private Sub reloadButton_Click(ByVal sender As Object, ByVal e As System.EventArgs) _
-        Handles reloadButton.Click
-
-        ' Reload the data from the database.
-        GetData(Me.dataAdapter.SelectCommand.CommandText)
+        Controls.AddRange(New Control() {dataGridView1, panel})
+        Text = "DataGridView data binding and updating demo"
 
     End Sub
 
-    Private Sub submitButton_Click(ByVal sender As Object, ByVal e As System.EventArgs) _
-        Handles submitButton.Click
-
-        ' Update the database with the user's changes.
-        Me.dataAdapter.Update(CType(Me.bindingSource1.DataSource, DataTable))
-
-    End Sub
-
-    '<snippet20>
     Private Sub GetData(ByVal selectCommand As String)
 
         Try
-            ' Specify a connection string. Replace the given value with a 
-            ' valid connection string for a Northwind SQL Server sample
-            ' database accessible to your system.
-            Dim connectionString As String = _
-                "Integrated Security=SSPI;Persist Security Info=False;" + _
-                "Initial Catalog=Northwind;Data Source=localhost"
+            ' Specify a connection string.  
+            ' Replace <SQL Server> with the SQL Server for your Northwind sample database.
+            ' Replace "Integrated Security=True" with user login information if necessary.
+            Dim connectionString As String =
+                "Data Source=<SQL Server>;Initial Catalog=Northwind;" +
+                "Integrated Security=True;"
 
             ' Create a new data adapter based on the specified query.
-            Me.dataAdapter = New SqlDataAdapter(selectCommand, connectionString)
+            dataAdapter = New SqlDataAdapter(selectCommand, connectionString)
 
             ' Create a command builder to generate SQL update, insert, and
-            ' delete commands based on selectCommand. These are used to
-            ' update the database.
-            Dim commandBuilder As New SqlCommandBuilder(Me.dataAdapter)
+            ' delete commands based on selectCommand. 
+            Dim commandBuilder As New SqlCommandBuilder(dataAdapter)
 
             ' Populate a new data table and bind it to the BindingSource.
-            Dim table As New DataTable()
-            table.Locale = System.Globalization.CultureInfo.InvariantCulture
-            Me.dataAdapter.Fill(table)
-            Me.bindingSource1.DataSource = table
+            Dim table As New DataTable With {
+                .Locale = Globalization.CultureInfo.InvariantCulture
+            }
+            dataAdapter.Fill(table)
+            bindingSource1.DataSource = table
 
             ' Resize the DataGridView columns to fit the newly loaded content.
-            Me.dataGridView1.AutoResizeColumns( _
+            dataGridView1.AutoResizeColumns(
                 DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader)
         Catch ex As SqlException
-            MessageBox.Show("To run this example, replace the value of the " + _
-                "connectionString variable with a connection string that is " + _
+            MessageBox.Show("To run this example, replace the value of the " +
+                "connectionString variable with a connection string that is " +
                 "valid for your system.")
         End Try
 
     End Sub
-    '</snippet20>
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) _
+        Handles Me.Load
+
+        ' Bind the DataGridView to the BindingSource
+        ' and load the data from the database.
+        dataGridView1.DataSource = bindingSource1
+        GetData("select * from Customers")
+
+    End Sub
+
+    Private Sub ReloadButton_Click(ByVal sender As Object, ByVal e As EventArgs) _
+        Handles ReloadButton.Click
+
+        ' Reload the data from the database.
+        GetData(dataAdapter.SelectCommand.CommandText)
+
+    End Sub
+
+    Private Sub SubmitButton_Click(ByVal sender As Object, ByVal e As EventArgs) _
+        Handles SubmitButton.Click
+
+        ' Update the database with changes.
+        dataAdapter.Update(CType(bindingSource1.DataSource, DataTable))
+
+    End Sub
 
 End Class
 '</snippet00>
