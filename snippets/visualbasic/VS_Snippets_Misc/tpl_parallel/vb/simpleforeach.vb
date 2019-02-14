0312d33ac3f11a3b19e1bbc674e54a88c562676e
Ron Petrusha <ronpet@microsoft.com>
2019-02-14 09:52:08 -0800

    Update snippets/visualbasic/VS_Snippets_Misc/tpl_parallel/vb/simpleforeach.vb
    commit from PR comment
    
    Co-Authored-By: MarkMcGee1 <46461816+MarkMcGee1@users.noreply.github.com>

--- snippets/visualbasic/VS_Snippets_Misc/tpl_parallel/vb/simpleforeach.vb
+++ snippets/visualbasic/VS_Snippets_Misc/tpl_parallel/vb/simpleforeach.vb
@@ -7,7 +7,7 @@ Module ForEachDemo
 
     Sub Main()
         ' A simple source for demonstration purposes. Modify this path as necessary.
-        Dim files As String() = IO.Directory.GetFiles("C:\Users\Public\Pictures\Sample Pictures", "*.jpg")
+        Dim files As String() = Directory.GetFiles("C:\Users\Public\Pictures\Sample Pictures", "*.jpg")
         Dim newDir As String = "C:\Users\Public\Pictures\Sample Pictures\Modified"
         IO.Directory.CreateDirectory(newDir)
 
