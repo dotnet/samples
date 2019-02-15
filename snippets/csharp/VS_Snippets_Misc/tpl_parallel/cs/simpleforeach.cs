c594f3c5363ed4e85a7447cfba58eb48d3320610
Mark McGee <46461816+MarkMcGee1@users.noreply.github.com>
2019-02-14 17:28:47 -0800

    edits for parallel foreach loop snippets (#648)
    * edits for parallel foreach loop article
    
    * Update snippets/visualbasic/VS_Snippets_Misc/tpl_parallel/vb/simpleforeach.vb
    
    commit from PR comment
    
    Co-Authored-By: MarkMcGee1 <46461816+MarkMcGee1@users.noreply.github.com>
    
    * Update snippets/visualbasic/VS_Snippets_Misc/tpl_parallel/vb/simpleforeach.vb
    
    Co-Authored-By: MarkMcGee1 <46461816+MarkMcGee1@users.noreply.github.com>
    
    * Update snippets/visualbasic/VS_Snippets_Misc/tpl_parallel/vb/simpleforeach.vb
    
    Co-Authored-By: MarkMcGee1 <46461816+MarkMcGee1@users.noreply.github.com>
    
    * Update snippets/visualbasic/VS_Snippets_Misc/tpl_parallel/vb/simpleforeach.vb
    
    Co-Authored-By: MarkMcGee1 <46461816+MarkMcGee1@users.noreply.github.com>
    
    * Update snippets/visualbasic/VS_Snippets_Misc/tpl_parallel/vb/simpleforeach.vb
    
    Co-Authored-By: MarkMcGee1 <46461816+MarkMcGee1@users.noreply.github.com>
    
    * remove comments
    
    * remove extra line
    
    * Update simpleforeach.cs

--- snippets/csharp/VS_Snippets_Misc/tpl_parallel/cs/simpleforeach.cs
+++ snippets/csharp/VS_Snippets_Misc/tpl_parallel/cs/simpleforeach.cs
@@ -1,6 +1,4 @@
 // <snippet03>
-//
-// IMPORTANT!!!: Add a reference to System.Drawing.dll
 using System;
 using System.IO;
 using System.Threading;
@@ -12,17 +10,16 @@ public class Example
     public static void Main()
     {
         // A simple source for demonstration purposes. Modify this path as necessary.
-        String[] files = System.IO.Directory.GetFiles(@"C:\Users\Public\Pictures\Sample Pictures", "*.jpg");
-        String newDir = @"C:\Users\Public\Pictures\Sample Pictures\Modified";
-        System.IO.Directory.CreateDirectory(newDir);
+        string[] files = Directory.GetFiles(@"C:\Users\Public\Pictures\Sample Pictures", "*.jpg");
+        string newDir = @"C:\Users\Public\Pictures\Sample Pictures\Modified";
+        Directory.CreateDirectory(newDir);
 
         // Method signature: Parallel.ForEach(IEnumerable<TSource> source, Action<TSource> body)
-        // Be sure to add a reference to System.Drawing.dll.
         Parallel.ForEach(files, (currentFile) => 
                                 {
                                     // The more computational work you do here, the greater 
                                     // the speedup compared to a sequential foreach loop.
-                                    String filename = System.IO.Path.GetFileName(currentFile);
+                                    string filename = Path.GetFileName(currentFile);
                                     var bitmap = new Bitmap(currentFile);
 
                                     bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
@@ -31,7 +28,7 @@ public class Example
                                     // Peek behind the scenes to see how work is parallelized.
                                     // But be aware: Thread contention for the Console slows down parallel loops!!!
 
-                                    Console.WriteLine("Processing {0} on thread {1}", filename, Thread.CurrentThread.ManagedThreadId);
+                                    Console.WriteLine($"Processing {filename} on thread {Thread.CurrentThread.ManagedThreadId}");
                                     //close lambda expression and method invocation
                                 });
 
