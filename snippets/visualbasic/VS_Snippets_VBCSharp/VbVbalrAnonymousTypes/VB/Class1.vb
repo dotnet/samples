8073995827361e78ded299a6df12a908be062321
Maira Wenzel <mairaw@microsoft.com>
2019-03-05 22:11:17 -0800

    Port changes (#689)
    * Port changes
    
    * Port changes

--- snippets/visualbasic/VS_Snippets_VBCSharp/VbVbalrAnonymousTypes/VB/Class1.vb
+++ snippets/visualbasic/VS_Snippets_VBCSharp/VbVbalrAnonymousTypes/VB/Class1.vb
@@ -95,7 +95,7 @@ Module Module1
         '</Snippet9>
 
         '<Snippet10>
-        ' Dim thirdProd2 = New With {Key .Nmae = "stapler", Key .Price = 5.09}
+        ' Dim thirdProd2 = New With {Key .Name = "stapler", Key .Price = 5.09}
         ' Dim thirdProd2 = New With {Key .Name = "stapler", Key .Price = "5.09"}
         ' Dim thirdProd2 = New With {Key .Name = "stapler", .Price = 5.09}
         '</Snippet10>
