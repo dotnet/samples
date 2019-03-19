1e5675b813da69e96be5388e14687f124f06b513
Ron Petrusha <ronpet@microsoft.com>
2019-03-19 08:14:25 -0700

    Examples for redirecting process output (#725)

---
+++ snippets/visualbasic/api/system.diagnostics/process/standarderror/stderror-sync.vb
@@ -0,0 +1,20 @@
+Imports System.Diagnostics'
+
+Public Module Example
+   Public Sub Main()
+      Dim p As New Process()
+      p.StartInfo.UseShellExecute = False  
+      p.StartInfo.RedirectStandardError = True  
+      p.StartInfo.FileName = "Write500Lines.exe"  
+      p.Start() 
+
+      ' To avoid deadlocks, always read the output stream first and then wait.  
+      Dim output As String = p.StandardError.ReadToEnd()  
+      p.WaitForExit()
+
+      Console.WriteLine($"\nError stream: {output}");
+   End Sub
+End Module
+' The end of the output produced by the example includes the following:
+'      Error stream:
+'      Successfully wrote 500 lines.
