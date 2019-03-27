175583852f8ba9b59f1bc37ab616c139ec6cd80d
dksimpson <v-deasim@microsoft.com>
2019-03-27 11:24:14 -0700

    Move snippet to samples repo and add line highlighting

---
+++ snippets/csharp/VS_Snippets_VBCSharp/VbRadconService/CS/MyNewService-input-parameter.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+
+   public MyNewService(string[] args)
+   {
+       InitializeComponent();
+
+       string eventSourceName = "MySource";
+       string logName = "MyNewLog";
+
+       if (args.Length > 0)
+       {
+          eventSourceName = args[0];
+       }
+
+       if (args.Length > 1)
+       {
+           logName = args[1];
+       }
+
+       eventLog1 = new EventLog();
+
+       if (!EventLog.SourceExists(eventSourceName))
+       {
+           EventLog.CreateEventSource(eventSourceName, logName);
+       }
+
+       eventLog1.Source = eventSourceName;
+       eventLog1.Log = logName;
+   }
