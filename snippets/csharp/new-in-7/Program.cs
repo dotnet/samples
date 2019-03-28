f092292ef86b3fa36a836f57c7a5faf4c8cd973f
Bill Wagner <wiwagn@microsoft.com>
2019-03-28 11:20:32 -0400

    Updates for interactive in C# (#754)
    Many of the samples changed.  This sample now supports "What's new in C# 7", and the exploratory tutorial for C# 7.
    
    Required for dotnet/docs#11442

--- snippets/csharp/new-in-7/Program.cs
+++ snippets/csharp/new-in-7/Program.cs
@@ -1,101 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace new_in_7
 {
     class Numerics
     {
-        #region 32_BinaryConstants
-        public const int One =  0b0001;
-        public const int Two =  0b0010;
-        public const int Four = 0b0100;
-        public const int Eight = 0b1000;
-        #endregion
-
-        #region 33_ThousandSeparators
+        // <SnippetThousandSeparators>
         public const int Sixteen =   0b0001_0000;
         public const int ThirtyTwo = 0b0010_0000;
         public const int SixtyFour = 0b0100_0000;
         public const int OneHundredTwentyEight = 0b1000_0000;
-        #endregion
+        // </SnippetThousandSeparators>
 
-        #region 34_LargeIntegers
+        // <SnippetLargeIntegers>
         public const long BillionsAndBillions = 100_000_000_000;
-        #endregion
+        // </SnippetLargeIntegers>
 
-        #region 35_OtherConstants
+        // <SnippetOtherConstants>
         public const double AvogadroConstant = 6.022_140_857_747_474e23;
         public const decimal GoldenRatio = 1.618_033_988_749_894_848_204_586_834_365_638_117_720_309_179M;
-        #endregion
+        // </SnippetOtherConstants>
 
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var util = new FactorialExample();
-            util.LocalFunctionFactorial(100);
-            return;
-            
             OutVariableDeclarations();
             TupleDeclarations();
             TupleMethod();
 
-            #region 12_DeconstructPoint
+            // <SnippetDeconstructPoint>
             var p = new Point(3.14, 2.71);
             (double X, double Y) = p;
-            #endregion
+            // </SnippetDeconstructPoint>
             Console.WriteLine(X);
             Console.WriteLine(Y);
 
-            #region 13_DeconstructNames
-            (double horizontalDistance, double verticalDistance) = p;
-            #endregion
-            Console.WriteLine(horizontalDistance);
-            Console.WriteLine(verticalDistance);
+            Patterns.IsPatternTestOne();
+
+            Patterns.SwitchPatternSimple();
+            Patterns.SwitchPatternFull();
+
+            MatrixSearch.EverythingByValue();
 
             int[,] matrix = new int[10, 10];
             for (int x = 0; x < 10; x++)
                 for (int y = 0; y < 10; y++)
                     matrix[x, y] = x * 10 + y;
 
-            #region 21_UpdateItemFromIndices
-            var indices = MatrixSearch.Find(matrix, (val) => val == 42);
-            Console.WriteLine(indices);
-            matrix[indices.i, indices.j] = 24;
-            #endregion
-
-            matrix[indices.i, indices.j] = 42;
-
-            #region 23_AssignRefReturnToValue
-            var valItem = MatrixSearch.Find3(matrix, (val) => val == 42);
-            Console.WriteLine(valItem);
-            valItem = 24;
-            Console.WriteLine(matrix[4, 2]);
-            #endregion
-
-            #region 24_AssignRefReturn
-            ref var item = ref MatrixSearch.Find3(matrix, (val) => val == 42);
+            // <SnippetAssignRefReturn>
+            ref var item = ref MatrixSearch.Find(matrix, (val) => val == 42);
             Console.WriteLine(item);
             item = 24;
             Console.WriteLine(matrix[4, 2]);
-            #endregion
+            // <SnippetAssignRefReturn>
 
-            try
-            {
-                #region 26_CallIteratorMethod
-                var resultSet = Iterator.AlphabetSubset('f', 'a');
-                Console.WriteLine("iterator created");
-                foreach (var thing in resultSet)
-                    Console.Write($"{thing}, ");
-                #endregion 
-            } catch (ArgumentException e)
-            {
-                Console.WriteLine(e);
-            }
+            Iterator.IteratorTest();
+            Iterator.IteratorTestLocal();
 
             Console.WriteLine(MathUtilities.LocalFunctionFactorial(1));
             Console.WriteLine(MathUtilities.LocalFunctionFactorial(2));
@@ -113,76 +75,74 @@ namespace new_in_7
 
         private static void TupleMethod()
         {
-            var numbers = new int[] { 1, 2, 3, 5, 8, 13, 21, 34, 55 };
-            #region 09_CallingTupleMethod
-            var range = Range(numbers);
-            #endregion
-            Console.WriteLine(range);
+            // <SnippetTupleReturningMethod>
+            (int Max, int Min) Range(IEnumerable<int> sequence)
+            {
+                int Min = int.MaxValue;
+                int Max = int.MinValue;
+                foreach (var n in sequence)
+                {
+                    Min = (n < Min) ? n : Min;
+                    Max = (n > Max) ? n : Max;
+                }
+                return (Max, Min);
+            }
 
-            #region 10_CallingWithDeconstructor
+            var numbers = new int[] { 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+            var range = Range(numbers);
+            Console.WriteLine(range);
+            // </SnippetTupleReturningMethod>
+
+            // <SnippetCallingWithDeconstructor>
             (int max, int min) = Range(numbers);
-            #endregion
             Console.WriteLine(max);
             Console.WriteLine(min);
-        }
+            // </SnippetCallingWithDeconstructor>
 
-        #region 08_TupleReturningMethod
-        private static (int Max, int Min) Range(IEnumerable<int> numbers)
-        {
-            int min = int.MaxValue;
-            int max = int.MinValue;
-            foreach(var n in numbers)
-            {
-                min = (n < min) ? n : min;
-                max = (n > max) ? n : max;
-            }
-            return (max, min);
-        }
-        #endregion
+            // <SnippetDiscardMember>
+            (int maxValue, _) = Range(numbers);
+            Console.WriteLine(max);
+            // </SnippetDiscardMember>
 
+        }
 
         private static void TupleDeclarations()
         {
-            #region 04_UnnamedTuple
-            var letters = ("a", "b");
-            #endregion
 
-            #region 05_NamedTuple
+            // <SnippetNamedTuple>
             (string Alpha, string Beta) namedLetters = ("a", "b");
-            #endregion
+            Console.WriteLine(namedLetters.Alpha, namedLetters.Beta);
+            // </SnippetNamedTuple>
 
-            #region 06_ImplicitNamedTuple
+            // <SnippetImplicitNamedTuple>
             var alphabetStart = (Alpha: "a", Beta: "b");
-            #endregion
-
-            #region 07_NamedTupleConflict
-            (string First, string Second) firstLetters = (Alpha: "a", Beta: "b");
-            #endregion
+            // </SnippetImplicitNamedTuple>
         }
 
         private static void OutVariableDeclarations()
         {
+            // <SnippetOutVariableOldStyle>
             var input = "1234";
-            #region 01_OutVariableDeclarations
-            if (int.TryParse(input, out int result))
-                Console.WriteLine(result);
-            else
-                Console.WriteLine("Could not parse input");
-            #endregion
-            #region 02_OutVarVariableDeclarations
-            if (int.TryParse(input, out var answer))
-                Console.WriteLine(answer);
-            else
-                Console.WriteLine("Could not parse input");
-            #endregion
-            #region 03_OutVariableOldStyle
             int numericResult;
             if (int.TryParse(input, out numericResult))
                 Console.WriteLine(numericResult);
             else
                 Console.WriteLine("Could not parse input");
+            // </SnippetOutVariableOldStyle>
 
-            #endregion
+            // <SnippetOutVariableDeclarations>
+            if (int.TryParse(input, out int result))
+                Console.WriteLine(result);
+            else
+                Console.WriteLine("Could not parse input");
+            // </SnippetOutVariableDeclarations>
+
+            // <SnippetOutVarVariableDeclarations>
+            if (int.TryParse(input, out var answer))
+                Console.WriteLine(answer);
+            else
+                Console.WriteLine("Could not parse input");
+            // </SnippetOutVarVariableDeclarations>
         }
     }
 }
