ac527108cae8c6708ff1774d3350bcb7e8ef8a7b
Günther Foidl <gue@korporal.at>
2019-04-10 16:16:16 +0200

    Removed double extern "C" declaration (#752)
    `extern "C"` is already part of the macro `EXPORT`, so after expansion it would be
    
    ```c++
    extern "C" extern "C" __declspec(dllexport) int NativeSum(int a, int b)
    ```
    (for VC++).
    
    Also removed the unneeded header `stdio.h`.

--- core/extensions/DllMapDemo/NewLib.cpp
+++ core/extensions/DllMapDemo/NewLib.cpp
@@ -1,12 +1,10 @@
-#include <stdio.h>
-
 #if defined(__GNUC__)
 #define EXPORT extern "C" __attribute__((visibility("default")))
 #elif defined(_MSC_VER)
 #define EXPORT extern "C" __declspec(dllexport)
 #endif 
 
-extern "C" EXPORT int NativeSum(int a, int b)
+EXPORT int NativeSum(int a, int b)
 {
     return a + b;
 }
