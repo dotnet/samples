8c3654c9eb0ef617fecffb0a2fbc2ec2a9f2e556
nnpcYvIVl <nnpcYvIVl@users.noreply.github.com>
2019-04-17 08:37:28 -0500

    Update ipaddress.cpp

--- snippets/cpp/VS_Snippets_Remoting/System.Net.IPAddress/CPP/ipaddress.cpp
+++ snippets/cpp/VS_Snippets_Remoting/System.Net.IPAddress/CPP/ipaddress.cpp
@@ -39,7 +39,7 @@ void IPAddresses( String^ server )
          
          //<Snippet3>
          // Display the type of address family supported by the server. If the
-         // server is IPv6-enabled this value is: InternNetworkV6. If the server
+         // server is IPv6-enabled this value is: InterNetworkV6. If the server
          // is also IPv4-enabled there will be an additional value of InterNetwork.
          Console::WriteLine( "AddressFamily: {0}", curAdd->AddressFamily );
          
