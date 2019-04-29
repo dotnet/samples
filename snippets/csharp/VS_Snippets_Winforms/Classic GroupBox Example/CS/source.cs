f43180c01db8f8c74b8a0c32f74665bd4f5f17e8
RandomGuyJCI <carleisaguirre@gmail.com>
2019-04-30 03:50:44 +0800

    Fix code layout issue (#855)

--- snippets/csharp/VS_Snippets_Winforms/Classic GroupBox Example/CS/source.cs
+++ snippets/csharp/VS_Snippets_Winforms/Classic GroupBox Example/CS/source.cs
@@ -10,34 +10,34 @@ public class Form1: Form
 // <Snippet1>
 private void InitializeMyGroupBox()
 {
-   // Create and initialize a GroupBox and a Button control.
-            GroupBox groupBox1 = new GroupBox();
-            Button button1 = new Button();
-            button1.Location = new Point(20,10);
-               
-            // Set the FlatStyle of the GroupBox.
-            groupBox1.FlatStyle = FlatStyle.Flat;
+    // Create and initialize a GroupBox and a Button control.
+    GroupBox groupBox1 = new GroupBox();
+    Button button1 = new Button();
+    button1.Location = new Point(20,10);
 
-            // Add the Button to the GroupBox.
-            groupBox1.Controls.Add(button1);
+    // Set the FlatStyle of the GroupBox.
+    groupBox1.FlatStyle = FlatStyle.Flat;
 
-            // Add the GroupBox to the Form.
-            Controls.Add(groupBox1);
+    // Add the Button to the GroupBox.
+    groupBox1.Controls.Add(button1);
 
-            // Create and initialize a GroupBox and a Button control.
-            GroupBox groupBox2 = new GroupBox();
-            Button button2 = new Button();
-            button2.Location = new Point(20, 10);
-            groupBox2.Location = new Point(0, 120);
-           
-            // Set the FlatStyle of the GroupBox.
-            groupBox2.FlatStyle = FlatStyle.Standard;
+    // Add the GroupBox to the Form.
+    Controls.Add(groupBox1);
 
-            // Add the Button to the GroupBox.
-            groupBox2.Controls.Add(button2);
+    // Create and initialize a GroupBox and a Button control.
+    GroupBox groupBox2 = new GroupBox();
+    Button button2 = new Button();
+    button2.Location = new Point(20, 10);
+    groupBox2.Location = new Point(0, 120);
 
-            // Add the GroupBox to the Form.
-            Controls.Add(groupBox2);
+    // Set the FlatStyle of the GroupBox.
+    groupBox2.FlatStyle = FlatStyle.Standard;
+
+    // Add the Button to the GroupBox.
+    groupBox2.Controls.Add(button2);
+
+    // Add the GroupBox to the Form.
+    Controls.Add(groupBox2);
 }
  
 // </Snippet1>
