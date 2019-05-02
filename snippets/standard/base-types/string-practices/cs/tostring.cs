40a0ba08cee973ccf2907095ff20be32785322c6
Ron Petrusha <ronpet@microsoft.com>
2019-05-02 11:12:59 -0700

    Formatting examples for string best practices

---
+++ snippets/standard/base-types/string-practices/cs/tostring.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // <Snippet2>
+        string value = "The amount is " + 126.03.ToString(CultureInfo.InvariantCulture) + ".";
+        Console.WriteLine(value);
+        // </Snippet2>
+
+        // <Snippet1>
+        value = "The amount is " + 126.03 + ".";
+        Console.WriteLine(value);
+        // </Snippet1>
+    }
+}
