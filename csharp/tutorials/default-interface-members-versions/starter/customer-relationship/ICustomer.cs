43e579695b19d12d0abbbff04cb7628922a7c0d5
Bill Wagner <bill.w.wagner@outlook.com>
2019-05-03 17:37:59 -0400

    First draft of sample done.

---
+++ csharp/tutorials/default-interface-members-versions/starter/customer-relationship/ICustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customer_relationship
+{
+    public interface ICustomer
+    {
+        IEnumerable<IOrder> PreviousOrders { get; }
+
+        DateTime DateJoined { get; }
+        DateTime? LastOrder { get; }
+        string Name { get; }
+        IDictionary<DateTime, string> Reminders { get; }
+    }
+}
---
+++ csharp/tutorials/default-interface-members-versions/starter/customer-relationship/SampleCustomer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customer_relationship
+{
+    public class SampleCustomer : ICustomer
+    {
+        public SampleCustomer(string name, DateTime dateJoined) => 
+            (Name, DateJoined) = (name, dateJoined);
+
+        private List<IOrder> allOrders = new List<IOrder>();
+
+        public IEnumerable<IOrder> PreviousOrders => allOrders;
+
+        public DateTime DateJoined { get; }
+
+        public DateTime? LastOrder { get; private set; }
+
+        public string Name { get; }
+
+        private Dictionary<DateTime, string> reminders = new Dictionary<DateTime, string>();
+        public IDictionary<DateTime, string> Reminders => reminders;
+
+        public void AddOrder(IOrder order)
+        {
+            if (order.Purchased > (LastOrder ?? DateTime.MinValue))
+                LastOrder = order.Purchased;
+            allOrders.Add(order);
+        }
+    }
+}
