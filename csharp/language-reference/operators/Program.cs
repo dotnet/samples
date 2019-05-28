c5217ae2d739440b483c4e0a03678c9307c227a4
Petr Kulikov <petr.kulikov@gmail.com>
2019-05-28 16:58:13 +0200

    Added snippets for the subtraction operator article (#929)

---
+++ csharp/language-reference/operators/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============== - operator examples =============");
+            SubtractionOperator.Examples();
+            Console.WriteLine();
+        }
+    }
+}
