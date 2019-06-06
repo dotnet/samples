74a30cdc710ebc5a02302a65cb0803c5fb77c806
Bill Wagner <bill.w.wagner@outlook.com>
2019-06-06 16:33:21 -0400

    make snippets for samples
    Contributes to dotnet/dotnet-api-docs#2539
    
    For `String.Split`, almost all the samples had been changed to interactive. However, most were full programs. These were turned into snippets.
    
    In one case, a static function was changed to a local function.
    
    In two other cases, The `System.Text.RegularExpressions` namespace was added to variable declarations.
    
    The dotnet-api-docs content does not need to be changed, so this may need to be verified after merging by forcing a new build in the portal.

--- snippets/csharp/VS_Snippets_CLR_System/system.String.Split/cs/splitalt1.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.String.Split/cs/splitalt1.cs
@@ -1,17 +1,17 @@
-// <Snippet8>
 using System;
-using System.Text.RegularExpressions;
 
 public class Example
 {
    public static void Main()
    {
+      // <Snippet8>
       String[] expressions = { "16 + 21", "31 * 3", "28 / 3",
                                "42 - 18", "12 * 7",
                                "2, 4, 6, 8" };
       String pattern = @"(\d+)\s+([-+*/])\s+(\d+)";
       foreach (var expression in expressions)
-         foreach (Match m in Regex.Matches(expression, pattern)) {
+         foreach (System.Text.RegularExpressions.Match m in 
+         System.Text.RegularExpressions.Regex.Matches(expression, pattern)) {
             int value1 = Int32.Parse(m.Groups[1].Value);
             int value2 = Int32.Parse(m.Groups[3].Value);
             switch (m.Groups[2].Value)
@@ -30,12 +30,12 @@ public class Example
                   break;
             }
          }
+   // The example displays the following output:
+   //       16 + 21 = 37
+   //       31 * 3 = 93
+   //       28 / 3 = 9.33
+   //       42 - 18 = 24
+   //       12 * 7 = 84
+   // </Snippet8>
    }
 }
-// The example displays the following output:
-//       16 + 21 = 37
-//       31 * 3 = 93
-//       28 / 3 = 9.33
-//       42 - 18 = 24
-//       12 * 7 = 84
-// </Snippet8>
