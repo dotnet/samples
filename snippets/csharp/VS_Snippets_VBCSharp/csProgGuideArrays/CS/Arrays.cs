4899d2210042a69d508d6d2a4b2abbc335b651a1
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-06-06 21:13:33 +0200

    Follow comments coding conventions
    https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/inside-a-program/coding-conventions
    The coding conventions in previous link says:
    "End comment text with a period."

--- snippets/csharp/VS_Snippets_VBCSharp/csProgGuideArrays/CS/Arrays.cs
+++ snippets/csharp/VS_Snippets_VBCSharp/csProgGuideArrays/CS/Arrays.cs
@@ -343,22 +343,22 @@ class TestArraysClass
         // Declare a single-dimensional array 
         int[] array1 = new int[5];
 
-        // Declare and set array element values
+        // Declare and set array element values.
         int[] array2 = new int[] { 1, 3, 5, 7, 9 };
 
-        // Alternative syntax
+        // Alternative syntax.
         int[] array3 = { 1, 2, 3, 4, 5, 6 };
 
-        // Declare a two dimensional array
+        // Declare a two dimensional array.
         int[,] multiDimensionalArray1 = new int[2, 3];
 
-        // Declare and set array element values
+        // Declare and set array element values.
         int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };
 
-        // Declare a jagged array
+        // Declare a jagged array.
         int[][] jaggedArray = new int[6][];
 
-        // Set the values of the first array in the jagged array structure
+        // Set the values of the first array in the jagged array structure.
         jaggedArray[0] = new int[4] { 1, 2, 3, 4 };
     }
 }
