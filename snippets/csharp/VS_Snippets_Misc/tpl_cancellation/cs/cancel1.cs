efadc8c91590c7b88141e379928d9db3c7b6ab59
Mackiovello <elandroe@icloud.com>
2019-06-06 06:58:40 +0200

    Use modern async constructs
    async/await and Task.Run

--- snippets/csharp/VS_Snippets_Misc/tpl_cancellation/cs/cancel1.cs
+++ snippets/csharp/VS_Snippets_Misc/tpl_cancellation/cs/cancel1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 public class Example
 {
-   public static void Main()
+   public static async Task Main()
    {
       var tokenSource = new CancellationTokenSource();
       var token = tokenSource.Token;
@@ -24,28 +24,28 @@ public class Example
       // Request cancellation of a single task when the token source is canceled. 
       // Pass the token to the user delegate, and also to the task so it can  
       // handle the exception correctly.
-      t = Task.Factory.StartNew( () => DoSomeWork(1, token), token);
+      t = Task.Run( () => DoSomeWork(1, token), token);
       Console.WriteLine("Task {0} executing", t.Id);
       tasks.Add(t);
 
       // Request cancellation of a task and its children. Note the token is passed 
-      // to (1) the user delegate and (2) as the second argument to StartNew, so  
+      // to (1) the user delegate and (2) as the second argument to Task.Run, so
       // that the task instance can correctly handle the OperationCanceledException.
-      t = Task.Factory.StartNew( () => {
-                                     // Create some cancelable child tasks.  
-                                     Task tc;
-                                     for (int i = 3; i <= 10; i++) {
-                                        // For each child task, pass the same token 
-                                        // to each user delegate and to StartNew.
-                                        tc = Task.Factory.StartNew( iteration => DoSomeWork((int)iteration, token), i, token);
-                                        Console.WriteLine("Task {0} executing", tc.Id);
-                                        tasks.Add(tc);
-                                        // Pass the same token again to do work on the parent task.  
-                                        // All will be signaled by the call to tokenSource.Cancel below.
-                                        DoSomeWork(2, token);
-                                     } 
-                                  }, 
-                                  token);
+      t = Task.Run( () => {
+                            // Create some cancelable child tasks.
+                            Task tc;
+                            for (int i = 3; i <= 10; i++) {
+                                // For each child task, pass the same token
+                                // to each user delegate and to Task.Run.
+                                tc = Task.Run( () => DoSomeWork(i, token), token);
+                                Console.WriteLine("Task {0} executing", tc.Id);
+                                tasks.Add(tc);
+                                // Pass the same token again to do work on the parent task.
+                                // All will be signaled by the call to tokenSource.Cancel below.
+                                DoSomeWork(2, token);
+                            }
+                        },
+                        token);
                                            
         Console.WriteLine("Task {0} executing", t.Id);
         tasks.Add(t);
@@ -61,23 +61,14 @@ public class Example
             // if you do wait, you must enclose the call in a try-catch block to 
             // catch the TaskCanceledExceptions that are thrown. If you do  
             // not wait, no exception is thrown if the token that was passed to the  
-            // StartNew method is the same token that requested the cancellation. 
+            // Task.Run method is the same token that requested the cancellation.
         } 
 
         try {
-            Task.WaitAll(tasks.ToArray());
+            await Task.WhenAll(tasks.ToArray());
         }
-        catch (AggregateException e) {
-           Console.WriteLine("\nAggregateException thrown with the following inner exceptions:");
-           // Display information about each exception. 
-           foreach (var v in e.InnerExceptions) {
-              if (v is TaskCanceledException)
-                 Console.WriteLine("   TaskCanceledException: Task {0}", 
-                                   ((TaskCanceledException) v).Task.Id);
-              else
-                 Console.WriteLine("   Exception: {0}", v.GetType().Name);
-           } 
-           Console.WriteLine();
+        catch (OperationCanceledException) {
+           Console.WriteLine($"\n{nameof(OperationCanceledException)} thrown\n");
         }
         finally
         {
@@ -135,10 +126,7 @@ public class Example
 //    Task 2 cancelled
 //    Task 7 cancelled
 //    
-//    AggregateException thrown with the following inner exceptions:
-//       TaskCanceledException: Task 2
-//       TaskCanceledException: Task 8
-//       TaskCanceledException: Task 7
+//    OperationCanceledException thrown
 //    
 //    Task 2 status is now Canceled
 //    Task 1 status is now RanToCompletion
