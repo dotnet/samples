a828fb2717ec13383a289c07e24bd7279f0fa590
Bill Wagner <bill.w.wagner@outlook.com>
2019-06-07 17:37:55 -0400

    update samples for interactive csharp
    Update the samples and snippets for the Math.Round page to conform to the snippet limitation on C# interactive.

--- snippets/csharp/VS_Snippets_CLR_System/system.math.round.overload/cs/mean1.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.math.round.overload/cs/mean1.cs
@@ -1,10 +1,10 @@
-// <Snippet2>
 using System;
 
 public class Example
 {
    public static void Main()
    {
+      // <Snippet2>
       decimal[] values = { 1.15m, 1.25m, 1.35m, 1.45m, 1.55m, 1.65m };
       decimal sum = 0;
       
@@ -27,10 +27,10 @@ public class Example
          sum += Math.Round(value, 1, MidpointRounding.ToEven);
 
       Console.WriteLine("ToEven:        {0:N2}", sum/values.Length);
+      // The example displays the following output:
+      //       True mean:     1.40
+      //       AwayFromZero:  1.45
+      //       ToEven:        1.40
+      // </Snippet2>
    }
 }
-// The example displays the following output:
-//       True mean:     1.40
-//       AwayFromZero:  1.45
-//       ToEven:        1.40
-// </Snippet2>
