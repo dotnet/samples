1a1e8afecf53dbefe30325280ab2bc50e16b737e
Bill Wagner <wiwagn@microsoft.com>
2019-06-07 18:48:34 -0400

    update samples for interactive csharp (#971)
    Update the samples and snippets for the Math.Round page to conform to the snippet limitation on C# interactive.

--- snippets/csharp/VS_Snippets_CLR_System/system.math.round.overload/cs/source.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.math.round.overload/cs/source.cs
@@ -1,8 +1,8 @@
-// <Snippet3>
 using System;
 
 public class Sample {
    static void Main() {
+      // <Snippet3>
       Console.WriteLine(Math.Round(3.44m, 1)); 
       Console.WriteLine(Math.Round(3.45m, 1)); 
       Console.WriteLine(Math.Round(3.46m, 1)); 
@@ -11,15 +11,15 @@ public class Sample {
       Console.WriteLine(Math.Round(4.34m, 1)); 
       Console.WriteLine(Math.Round(4.35m, 1)); 
       Console.WriteLine(Math.Round(4.36m, 1)); 
+      // The example displays the following output:
+      //       3.4
+      //       3.4
+      //       3.5
+      //       
+      //       4.3
+      //       4.4
+      //       4.4
+      // </Snippet3>
    }
 }
-// The example displays the following output:
-//       3.4
-//       3.4
-//       3.5
-//       
-//       4.3
-//       4.4
-//       4.4
-// </Snippet3>
 
