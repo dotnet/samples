52b12410e4ad7eca82d5ae06e638ad10968d0a57
Petr Kulikov <petr.kulikov@gmail.com>
2019-06-11 16:11:23 +0200

    Removed obsolete snippets (#978)

--- snippets/csharp/keywords/Program.cs
+++ snippets/csharp/keywords/Program.cs
@@ -6,8 +6,6 @@ namespace keywords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("=================    Stack Alloc Examples ======================");
-            StackAllocExamples.Examples();
             Console.WriteLine("=================    Generic Where Constraints Examples ======================");
             GenericWhereConstraints.Examples();
             Console.WriteLine("=================    Fixed Memory Examples ======================");
