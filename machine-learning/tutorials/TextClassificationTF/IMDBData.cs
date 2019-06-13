55cb72a8929d33ea7ff128c06193529a81dd33c7
John Alexander (MSFT) <john.alexander@microsoft.com>
2019-06-13 08:56:05 -0700

    Add TextClassificationTF Sample (#935)
    * Add TextClassifficationTF Sample
    
    * Add Snippets
    
    * Update machine-learning/tutorials/TextClassificationTF/IMDBData.cs
    
    Co-Authored-By: Luis Quintanilla <46974588+luisquintanilla@users.noreply.github.com>

---
+++ machine-learning/tutorials/TextClassificationTF/IMDBData.cs
@@ -0,0 +1,33 @@
+﻿// <SnippetAddUsings>
+using Microsoft.ML.Data;
+// </SnippetAddUsings>
+
+namespace TextClassificationTF
+{
+    // <SnippetDeclareTypes>
+    /// <summary>
+    /// Class to hold original sentiment data.
+    /// </summary>
+    public class IMDBSentiment
+    {
+        public string Sentiment_Text { get; set; }
+
+        /// <summary>
+        /// This is a variable length vector designated by VectorType attribute.
+        /// Variable length vectors are produced by applying operations such as 'TokenizeWords' on strings
+        /// resulting in vectors of tokens of variable lengths.
+        /// </summary>
+        [VectorType]
+        public int[] VariableLengthFeatures { get; set; }
+    }  
+
+    /// <summary>
+    /// Class to contain the output values from the transformation.
+    /// </summary>
+    public class IMDBSentimentPrediction : IMDBSentiment
+    {
+        [VectorType(2)]
+        public float[] Prediction { get; set; }
+    }
+    // <SnippetDeclareTypes>
+}
