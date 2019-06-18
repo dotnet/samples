303f0b80f9f665de1204449cf56785bdb36ea129
pkulikov <petr.kulikov@gmail.com>
2019-06-18 21:08:27 +0200

    Added is operator snippets

--- csharp/language-reference/operators/TypeTestingAndAssignmentOperators.cs
+++ csharp/language-reference/operators/TypeTestingAndAssignmentOperators.cs
@@ -10,6 +10,9 @@ namespace operators
             Assignment();
             RefAssignment();
             Cast();
+            IsOperatorExample.Main();
+            IsOperatorWithInt();
+            IsOperatorTypePattern();
         }
 
         private static void Assignment()
@@ -70,6 +73,51 @@ namespace operators
             Console.WriteLine(list.Count);  // output: 3
             Console.WriteLine(list[1]);  // output: 20
             // </SnippetCast>
+        }
+
+        // <SnippetIsWithReferenceConversion>
+        public class Base { }
+
+        public class Derived : Base { }
+
+        public static class IsOperatorExample
+        {
+            public static void Main()
+            {
+                object b = new Base();
+                Console.WriteLine(b is Base);  // output: True
+                Console.WriteLine(b is Derived);  // output: False
+
+                object d = new Derived();
+                Console.WriteLine(d is Base);  // output: True
+                Console.WriteLine(d is Derived); // output: True
+            }
+        }
+        // </SnippetIsWithReferenceConversion>
+
+        private static void IsOperatorWithInt()
+        {
+            // <SnippetIsWithInt>
+            int i = 27;
+            Console.WriteLine(i is System.IFormattable);  // output: True
+            
+            object iBoxed = i;
+            Console.WriteLine(iBoxed is int);  // output: True
+            Console.WriteLine(iBoxed is long);  // output: False
+            // </SnippetIsWithInt>
+        }
+
+        private static void IsOperatorTypePattern()
+        {
+            // <SnippetIsTypePattern>
+            int i = 23;
+            object iBoxed = i;
+            int? j = 7;
+            if (iBoxed is int a && j is int b)
+            {
+                Console.WriteLine(a + b);  // output 30
+            }
+            // </SnippetIsTypePattern>
         }
     }
 }
