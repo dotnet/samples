ca9945bb62e67cfd341fa2389e8996649543a36e
Bill Wagner <bill.w.wagner@outlook.com>
2019-06-28 16:42:55 -0400

    remove sample from language reference
    This sample is no longer used now that all the floating point reference pages are in one article.

--- snippets/csharp/VS_Snippets_VBCSharp/csrefKeywordsTypes/CS/keywordsTypes.cs
+++ snippets/csharp/VS_Snippets_VBCSharp/csrefKeywordsTypes/CS/keywordsTypes.cs
@@ -226,23 +226,6 @@ namespace csrefKeywordsMethodParams
     }
     //</snippet8>
 
-    //<snippet9>
-    // Mixing types in expressions
-    class MixedTypes
-    {
-        static void Main()
-        {
-            int x = 3;
-            float y = 4.5f;
-            short z = 5;
-            double w = 1.7E+3;
-            // Result of the 2nd argument is a double:
-            Console.WriteLine("The sum is {0}", x + y + z + w);
-        }
-    }
-    // Output: The sum is 1712.5
-    //</snippet9>
-
     //<snippet10>
     public class EnumTest
     {
