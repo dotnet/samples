f974ec53d8109488388760a42b771d87dbe52fb8
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-04 09:09:55 +0200

    Add missing space

--- snippets/visualbasic/VS_Snippets_VBCSharp/VbVbalrStatements/VB/Class1.vb
+++ snippets/visualbasic/VS_Snippets_VBCSharp/VbVbalrStatements/VB/Class1.vb
@@ -85,7 +85,7 @@ End Class
 Public Module thisModule
     Sub Main()
         Dim userName As String = InputBox("What is your name?")
-        MsgBox("User name is" & userName)
+        MsgBox("User name is " & userName)
     End Sub
     ' Insert variable, property, procedure, and event declarations.
 End Module
