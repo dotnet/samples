0307a012623f6821c11d18c6c137de0775590131
Oriol Mesa <urimr26cat@gmail.com>
2019-07-09 11:29:08 +0200

    Indentation and cleanup how-to-create-thumbnail-images

--- snippets/csharp/VS_Snippets_Winforms/System.Drawing.WorkingWithImages/CS/Class1.cs
+++ snippets/csharp/VS_Snippets_Winforms/System.Drawing.WorkingWithImages/CS/Class1.cs
@@ -185,28 +185,15 @@ public class SystemDrawingWorkingWithImages : Form
     // How to: Create Thumbnail Images
 
    
-        // <snippet71>
-	public bool ThumbnailCallback() {
-            return true;
-        }
-       
-        private void GetThumbnail(PaintEventArgs e)
-        {
+    // <snippet71>
+    private void GetThumbnail(PaintEventArgs e)
+    {
+        Image image = new Bitmap(@"c:\FakePhoto.jpg");
+        Image thumbnail = image.GetThumbnailImage(100, 100, () => true, new IntPtr());
 
-            Image.GetThumbnailImageAbort callback = 
-                new Image.GetThumbnailImageAbort(ThumbnailCallback);
-            Image image = new Bitmap(@"c:\FakePhoto.jpg");
-            Image pThumbnail = image.GetThumbnailImage(100, 100, callback, new
-               IntPtr());
-            e.Graphics.DrawImage(
-               pThumbnail,
-               10,
-               10,
-               pThumbnail.Width,
-               pThumbnail.Height);
-        }
-      
-        // </snippet71>
+        e.Graphics.DrawImage(thumbnail, 10, 10, thumbnail.Width, thumbnail.Height);
+    }   
+    // </snippet71>
     
     // fde9bccf-8aa5-4b0d-ba4b-788740627b02
     // How to: Use Interpolation Mode to Control Image Quality During Scaling
