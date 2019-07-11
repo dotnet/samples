032b906f8c5b974d00ba29e29e159cba78cb9ba5
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-11 20:40:51 +0200

    Update source.vb

--- snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.Add Example/VB/source.vb
+++ snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.Add Example/VB/source.vb
@@ -8,11 +8,9 @@ Public Class Form1
     
 ' <Snippet1>
  Private Sub AddConstraint(table As DataTable)
-     Dim uniqueConstraint As UniqueConstraint
      ' Assuming a column named "UniqueColumn" exists, and 
      ' its Unique property is true.
-     uniqueConstraint = _
-        New UniqueConstraint(table.Columns("UniqueColumn"))
+        Dim uniqueConstraint As New UniqueConstraint(table.Columns("UniqueColumn"))
      table.Constraints.Add(uniqueConstraint)
  End Sub
 ' </Snippet1>
