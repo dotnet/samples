1a2445eb86bbafac9bcac81e8433e45fd460dc6a
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-11 20:36:12 +0200

    Update source.cs

--- snippets/csharp/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection Example/CS/source.cs
+++ snippets/csharp/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection Example/CS/source.cs
@@ -10,25 +10,23 @@ public class Form1: Form
     // <Snippet1>
     private void MakeTableWithUniqueConstraint()
     {
-        DataTable table = new DataTable("table");
-        DataColumn column = new DataColumn("UniqueColumn");
-        column.Unique=true;
+        var table = new DataTable("table");
+        var column = new DataColumn("UniqueColumn");
+        column.Unique = true;
         table.Columns.Add(column);
 
         // Print count, name, and type.
-        Console.WriteLine("Constraints.Count " 
-            + table.Constraints.Count);
+        Console.WriteLine($"Constraints.Count {table.Constraints.Count}");
         Console.WriteLine(table.Constraints[0].ConstraintName);
         Console.WriteLine(table.Constraints[0].GetType() );
 
         // Add a second unique column.
         column = new DataColumn("UniqueColumn2");
-        column.Unique=true;
+        column.Unique = true;
         table.Columns.Add(column);
 
         // Print info again.
-        Console.WriteLine("Constraints.Count " 
-            + table.Constraints.Count);
+        Console.WriteLine($"Constraints.Count {table.Constraints.Count}");
         Console.WriteLine(table.Constraints[1].ConstraintName);
         Console.WriteLine(table.Constraints[1].GetType() );
     }
@@ -36,17 +34,17 @@ public class Form1: Form
     private void MakeTableWithForeignConstraint()
     {
         // Create a DataSet.
-        DataSet dataSet = new DataSet("dataSet");
+        var dataSet = new DataSet("dataSet");
 
         // Make two tables.
-        DataTable customersTable= new DataTable("Customers");
-        DataTable ordersTable = new DataTable("Orders");
+        var customersTable = new DataTable("Customers");
+        var ordersTable = new DataTable("Orders");
 
         // Create four columns, two for each table.
-        DataColumn name = new DataColumn("Name");
-        DataColumn id = new DataColumn("ID");
-        DataColumn orderId = new DataColumn("OrderID");
-        DataColumn cDate = new DataColumn("OrderDate");
+        var name = new DataColumn("Name");
+        var id = new DataColumn("ID");
+        var orderId = new DataColumn("OrderID");
+        var cDate = new DataColumn("OrderDate");
  
         // Add columns to tables.
         customersTable.Columns.Add(name);
@@ -59,22 +57,18 @@ public class Form1: Form
         dataSet.Tables.Add(ordersTable); 
 
         // Create a DataRelation for two of the columns.
-        DataRelation myRelation = new 
-            DataRelation("CustomersOrders",id,orderId,true);
+        var myRelation = new DataRelation("CustomersOrders", id, orderId, true);
         dataSet.Relations.Add(myRelation);
 
         // Print TableName, Constraints.Count, 
         // ConstraintName and Type.
-        foreach(DataTable t in dataSet.Tables)
+        foreach (DataTable t in dataSet.Tables)
         {
             Console.WriteLine(t.TableName);
-            Console.WriteLine("Constraints.Count " 
-                + t.Constraints.Count);
-            Console.WriteLine("ParentRelations.Count " 
-                + t.ParentRelations.Count);
-            Console.WriteLine("ChildRelations.Count " 
-                + t.ChildRelations.Count);
-            foreach(Constraint cstrnt in t.Constraints)
+            Console.WriteLine($"Constraints.Count {t.Constraints.Count}");
+            Console.WriteLine($"ParentRelations.Count {t.ParentRelations.Count}");
+            Console.WriteLine($"ChildRelations.Count {t.ChildRelations.Count}");
+            foreach (Constraint cstrnt in t.Constraints)
             {
                 Console.WriteLine(cstrnt.ConstraintName);
                 Console.WriteLine(cstrnt.GetType());
