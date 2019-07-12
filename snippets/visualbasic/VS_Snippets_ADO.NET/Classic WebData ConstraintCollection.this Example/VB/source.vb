3d1cd9a9cd559ebc4273b1a8dc03835fb9a262ae
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-12 09:14:59 +0200

    Update source.vb

--- snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.this Example/VB/source.vb
+++ snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.this Example/VB/source.vb
@@ -8,8 +8,7 @@ Public Class Form1
     
 ' <Snippet1>
 Private Sub GetConstraint(table As DataTable)
-    Dim i As Integer
-    For i = 0 To table.Constraints.Count - 1
+    For i As Integer = 0 To table.Constraints.Count - 1
         Console.WriteLine(table.Constraints(i).ConstraintName)
         Console.WriteLine(table.Constraints(i).GetType())
     Next i
