e032a0a13a3e95aa169a2e4a9960066b9e39d3c5
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-12 08:22:01 +0200

    Update source.vb

--- snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.CanRemove Example/VB/source.vb
+++ snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.CanRemove Example/VB/source.vb
@@ -11,13 +11,11 @@ Private Sub TryRemove(dataSet As DataSet)
     Try
         Dim customersTable As DataTable = dataSet.Tables("Customers")
         Dim constraint As Constraint = customersTable.Constraints(0)
-        Console.WriteLine("Can remove? " & _
-            customersTable.Constraints.CanRemove(constraint).ToString())
+        Console.WriteLine($"Can remove? {customersTable.Constraints.CanRemove(constraint).ToString()}")
 
     Catch ex As Exception
         ' Process exception and return.
-        Console.WriteLine("Exception of type {0} occurred.", _
-            ex.GetType().ToString())
+        Console.WriteLine("Exception of type {ex.GetType().ToString()} occurred.")
     End Try
 End Sub
 ' </Snippet1>
