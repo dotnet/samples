b3cb0eb16e594a16e6db2635339d25544fc1b33c
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-12 08:43:23 +0200

    Update source.vb

--- snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.IndexOf1 Example/VB/source.vb
+++ snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.IndexOf1 Example/VB/source.vb
@@ -10,17 +10,14 @@ Public Class Form1
 Private Sub RemoveConstraint _
     (constraints As ConstraintCollection, constraint As Constraint)
     Try
-        If constraints.Contains(constraint.ConstraintName) Then
-            If constraints.CanRemove(constraint) Then
+        If constraints.Contains(constraint.ConstraintName) AndAlso constraints.CanRemove(constraint) Then
                 constraints.RemoveAt _
                 (constraints.IndexOf(constraint))
-            End If
         End If
 
     Catch e As Exception
 	' Process exception and return.
-        Console.WriteLine("Exception of type {0} occurred.", _
-            e.GetType().ToString())
+        Console.WriteLine($"Exception of type {e.GetType().ToString()} occurred.")
     End Try
 End Sub
 ' </Snippet1>
