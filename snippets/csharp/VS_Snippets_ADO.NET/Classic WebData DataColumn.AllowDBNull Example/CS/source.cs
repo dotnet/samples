77c122d923dcecf56de03e5b4d1152cff24660ff
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-12 09:25:34 +0200

    Update source.cs

--- snippets/csharp/VS_Snippets_ADO.NET/Classic WebData DataColumn.AllowDBNull Example/CS/source.cs
+++ snippets/csharp/VS_Snippets_ADO.NET/Classic WebData DataColumn.AllowDBNull Example/CS/source.cs
@@ -10,14 +10,11 @@ public class Form1: Form
     // <Snippet1>
     private void AddNullAllowedColumn()
     {
-        DataColumn column;
-        column = new DataColumn("classID", 
-            System.Type.GetType("System.Int32"));
+        var column = new DataColumn("classID", System.Type.GetType("System.Int32"));
         column.AllowDBNull = true;
 
         // Add the column to a new DataTable.
-        DataTable table;
-        table = new DataTable();
+        var table = new DataTable();
         table.Columns.Add(column);
     }
     // </Snippet1>
