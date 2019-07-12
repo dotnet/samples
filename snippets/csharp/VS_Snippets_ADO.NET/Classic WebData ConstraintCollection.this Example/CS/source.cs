d152d87bc6fe2be39026485a8e043dcc142cd853
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-12 09:14:44 +0200

    Update source.cs

--- snippets/csharp/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.this Example/CS/source.cs
+++ snippets/csharp/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.this Example/CS/source.cs
@@ -10,7 +10,7 @@ public class Form1: Form
     // <Snippet1>
     private void GetConstraint(DataTable table)
     {
-        for(int i = 0; i < table.Constraints.Count; i++)
+        for (int i = 0; i < table.Constraints.Count; i++)
         {
             Console.WriteLine(table.Constraints[i].ConstraintName);
             Console.WriteLine(table.Constraints[i].GetType());
