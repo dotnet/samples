6cba78e343f7f53bfaabfb59ab067100f23e60f6
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-12 08:24:33 +0200

    Update source.vb

--- snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.Clear Example/VB/source.vb
+++ snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData ConstraintCollection.Clear Example/VB/source.vb
@@ -5,8 +5,7 @@ Public Class Sample
 
 ' <Snippet1>
  Public Shared Sub ClearConstraints(dataSet As DataSet) 
-    Dim table As DataTable
-    For Each table In dataSet.Tables
+    For Each table As DataTable In dataSet.Tables
       table.Constraints.Clear()
     Next
  End Sub
