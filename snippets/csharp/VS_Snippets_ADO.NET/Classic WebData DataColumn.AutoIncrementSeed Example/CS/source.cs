db29925ab14c185c84f5d12604c6271fda5f60e0
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-12 09:27:12 +0200

    Update source.cs

--- snippets/csharp/VS_Snippets_ADO.NET/Classic WebData DataColumn.AutoIncrementSeed Example/CS/source.cs
+++ snippets/csharp/VS_Snippets_ADO.NET/Classic WebData DataColumn.AutoIncrementSeed Example/CS/source.cs
@@ -10,14 +10,14 @@ public class Form1: Form
     // <Snippet1>
     private void AddAutoIncrementColumn()
     {
-        DataColumn column = new DataColumn();
+        var column = new DataColumn();
         column.DataType = System.Type.GetType("System.Int32");
         column.AutoIncrement = true;
         column.AutoIncrementSeed = 1000;
         column.AutoIncrementStep = 10;
 
         // Add the column to a new DataTable.
-        DataTable table = new DataTable("table");
+        var table = new DataTable("table");
         table.Columns.Add(column);
     }
     // </Snippet1>
