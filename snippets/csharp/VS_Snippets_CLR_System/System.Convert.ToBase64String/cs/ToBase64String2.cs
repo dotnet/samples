6508dc071b864433d7312bd146449aabaeaa4cc7
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-22 08:42:25 +0200

    Fix typo

--- snippets/csharp/VS_Snippets_CLR_System/System.Convert.ToBase64String/cs/ToBase64String2.cs
+++ snippets/csharp/VS_Snippets_CLR_System/System.Convert.ToBase64String/cs/ToBase64String2.cs
@@ -10,8 +10,8 @@ public class Example
        Console.WriteLine("The byte array: ");
        Console.WriteLine("   {0}\n", BitConverter.ToString(bytes));
        
-       // Convert the array to a base 64 sring.
-       String s = Convert.ToBase64String(bytes);
+       // Convert the array to a base 64 string.
+       string s = Convert.ToBase64String(bytes);
        Console.WriteLine("The base 64 string:\n   {0}\n", s);
        
        // Restore the byte array.
