abbad12491ecec8a4b40fc6c5cb7653458ac9451
Andy De George <thraka.junk@outlook.com>
2019-07-23 11:30:41 -0700

    Change to bill's suggestion

--- snippets/csharp/VS_Snippets_VBCSharp/csProgGuideEnums/CS/Enums.cs
+++ snippets/csharp/VS_Snippets_VBCSharp/csProgGuideEnums/CS/Enums.cs
@@ -14,14 +14,14 @@ namespace Enums
     [Flags]
     enum Days
     {
-        None        = 0,
-        Sunday      = 1,
-        Monday      = 2,
-        Tuesday     = 4,
-        Wednesday   = 8,
-        Thursday    = 16,
-        Friday      = 32,
-        Saturday    = 64
+        None      = 0b_0000_0000, // 0
+        Sunday    = 0b_0000_0001, // 1
+        Monday    = 0b_0000_0010, // 2
+        Tuesday   = 0b_0000_0100, // 4
+        Wednesday = 0b_0000_1000, // 8
+        Thursday  = 0b_0001_0000, // 16
+        Friday    = 0b_0010_0000, // 32
+        Saturday  = 0b_0100_0000  // 64 
     }
     class MyClass
     {
