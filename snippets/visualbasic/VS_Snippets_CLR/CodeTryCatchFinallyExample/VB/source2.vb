a3a107a713ce1095f89464a949b259d97998d39d
Youssef1313 <31348972+Youssef1313@users.noreply.github.com>
2019-07-26 14:56:51 +0200

    Fix bad example

--- snippets/visualbasic/VS_Snippets_CLR/CodeTryCatchFinallyExample/VB/source2.vb
+++ snippets/visualbasic/VS_Snippets_CLR/CodeTryCatchFinallyExample/VB/source2.vb
@@ -1,4 +1,4 @@
-﻿'<snippet3>
+'<snippet3>
 Imports System
 
 Class ArgumentOutOfRangeExample
@@ -10,6 +10,7 @@ Class ArgumentOutOfRangeExample
             Array.Copy(array1, array2 , -1)
         Catch e As ArgumentOutOfRangeException
             Console.WriteLine("Error: {0}", e)
+            Throw
         Finally
             Console.WriteLine("This statement is always executed.")
         End Try
