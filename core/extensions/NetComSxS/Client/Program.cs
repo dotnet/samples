8bf935cb8a52054313062d73764e8c28af17b9e3
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-07-29 15:19:21 -0700

    Add sample showing COM interop between .NET Framework and .NET… (#1120)
    * Add sample showing COM interop between .NET Framework and .NET Core
    
    * PR Feedback.

---
+++ core/extensions/NetComSxS/Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contracts.IServer server = new Contracts.Server();
+
+            Console.WriteLine($"5 + 5 = {server.Add(5, 5)}");
+        }
+    }
+}
