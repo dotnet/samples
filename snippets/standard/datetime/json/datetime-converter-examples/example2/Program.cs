2255cf49c12efc0a5a3bc80d80ea3cb4358b49c4
Layomi Akinrinade <laakinri@microsoft.com>
2019-07-30 16:28:39 -0400

    Clean up DateTime converter samples

--- snippets/standard/datetime/json/datetime-converter-examples/example2/Program.cs
+++ snippets/standard/datetime/json/datetime-converter-examples/example2/Program.cs
@@ -8,7 +8,9 @@ using System.Text.Json.Serialization;
 
 namespace DateTimeConverterExamples
 {
-    public class DateTimeConverterExample2 : JsonConverter<DateTime>
+    // This converter reads and writes DateTime values according to the "R" standard format specifier:
+    // https://docs.microsoft.com/dotnet/standard/base-types/standard-date-and-time-format-strings#the-rfc1123-r-r-format-specifier.
+    public class DateTimeConverterForCustomStandardFormatR : JsonConverter<DateTime>
     {
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
@@ -19,7 +21,6 @@ namespace DateTimeConverterExamples
 
             if (Utf8Parser.TryParse(reader.ValueSpan, out DateTime value, out _, 'R'))
             {
-                reader.Read();
                 return value;
             }
 
@@ -28,34 +29,38 @@ namespace DateTimeConverterExamples
 
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
         {
+            // The R standard format will always be 29 tokens.
             Span<byte> destination = new byte[29];
 
             bool result = Utf8Formatter.TryFormat(value, destination, out _, new StandardFormat('R'));
             Debug.Assert(result);
 
-            writer.WriteStringValue(Encoding.UTF8.GetString(destination));
+            writer.WriteStringValue(destination);
         }
     }
 
     class Program
     {
-        private static void ParseDateTimeWithDefaultOptions_Example2()
+        private static void ParseDateTimeWithDefaultOptions()
         {
-            var _ = JsonSerializer.Deserialize<DateTime>(@"""Thu, 25 Jul 2019 13:36:07 GMT"""); // Throws JsonException
+            var _ = JsonSerializer.Deserialize<DateTime>(@"""Thu, 25 Jul 2019 13:36:07 GMT""");
+            // Throws JsonException
         }
 
-        private static void ProcessDateTimeWithCustomConverter_Example2()
+        private static void ProcessDateTimeWithCustomConverter()
         {
             var options = new JsonSerializerOptions();
-            options.Converters.Add(new DateTimeConverterExample2());
+            options.Converters.Add(new DateTimeConverterForCustomStandardFormatR());
 
             var testDateTimeStr = "Thu, 25 Jul 2019 13:36:07 GMT";
             var testDateTimeJson = @"""" + testDateTimeStr + @"""";
 
             var resultDateTime = JsonSerializer.Deserialize<DateTime>(testDateTimeJson, options);
-            Console.WriteLine(resultDateTime); // 7/25/2019 1:36:07 PM
+            Console.WriteLine(resultDateTime);
+            // 7/25/2019 1:36:07 PM
 
-            Console.WriteLine(JsonSerializer.Serialize(DateTime.Parse(testDateTimeStr), options)); // "Thu, 25 Jul 2019 09:36:07 GMT"
+            Console.WriteLine(JsonSerializer.Serialize(DateTime.Parse(testDateTimeStr), options));
+            // "Thu, 25 Jul 2019 09:36:07 GMT"
         }
 
         static void Main(string[] args)
@@ -63,15 +68,16 @@ namespace DateTimeConverterExamples
             // Parsing non-compliant format as DateTime fails by default.
             try
             {
-                ParseDateTimeWithDefaultOptions_Example2();
+                ParseDateTimeWithDefaultOptions();
             }
             catch (JsonException e)
             {
-                Console.WriteLine(e.Message); // The JSON value could not be converted to System.DateTime. Path: $ | LineNumber: 0 | BytePositionInLine: 31.
+                Console.WriteLine(e.Message);
+                // The JSON value could not be converted to System.DateTime. Path: $ | LineNumber: 0 | BytePositionInLine: 31.
             }
 
             // Using converters gives you control over the serializers parsing and formatting.
-            ProcessDateTimeWithCustomConverter_Example2();
+            ProcessDateTimeWithCustomConverter();
         }
     }
 }
--- snippets/standard/datetime/json/datetime-converter-examples/example3/Program.cs
+++ snippets/standard/datetime/json/datetime-converter-examples/example3/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Buffers;
+using System.Buffers.Text;
+using System.Diagnostics;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
 
 namespace DateTimeConverterExamples
 {
-    public class DateTimeConverterUsingDateTimeParse : JsonConverter<DateTime>
+    public class DateTimeConverterUsingDateTimeParseAsFallback : JsonConverter<DateTime>
     {
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
@@ -14,12 +17,18 @@ namespace DateTimeConverterExamples
                 throw new JsonException();
             }
 
-            return DateTime.Parse(reader.GetString());
+            if (!reader.TryGetDateTime(out DateTime value))
+            {
+                value = DateTime.Parse(reader.GetString());
+            }
+
+            return value;
         }
 
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(value.ToString());
+            // Converters allow support for custom formats.
+            writer.WriteStringValue(value.ToString("dd/MM/yyy"));
         }
     }
 
@@ -27,31 +36,24 @@ namespace DateTimeConverterExamples
     {
         private static void ParseDateTimeWithDefaultOptions()
         {
-            var _ = JsonSerializer.Deserialize<DateTime>(@"""04-10-2008 6:30 AM""");
+            var _ = JsonSerializer.Deserialize<DateTime>(@"""2019-07-16 16:45:27.4937872+00:00""");
             // Throws JsonException
-        }
-
-        private static void FormatDateTimeWithDefaultOptions()
-        {
-            Console.WriteLine(JsonSerializer.Serialize(DateTime.Parse("04-10-2008 6:30 AM -4")));
-            // "2008-04-10T06:30:00-04:00"
         }
 
         private static void ProcessDateTimeWithCustomConverter()
         {
             var options = new JsonSerializerOptions();
-            options.Converters.Add(new DateTimeConverterUsingDateTimeParse());
+            options.Converters.Add(new DateTimeConverterUsingDateTimeParseAsFallback());
 
-            var testDateTimeStr = "04-10-2008 6:30 AM";
+            var testDateTimeStr = "2019-07-16 16:45:27.4937872+00:00";
             var testDateTimeJson = @"""" + testDateTimeStr + @"""";
 
             var resultDateTime = JsonSerializer.Deserialize<DateTime>(testDateTimeJson, options);
             Console.WriteLine(resultDateTime);
-            // 4/10/2008 6:30:00 AM
+            // 7/16/2019 4:45:27 PM
 
-            var resultDateTimeJson = JsonSerializer.Serialize(DateTime.Parse(testDateTimeStr), options);
-            Console.WriteLine(Regex.Unescape(resultDateTimeJson));
-            // "4/10/2008 6:30:00 AM"
+            Console.WriteLine(JsonSerializer.Serialize(DateTime.Parse(testDateTimeStr), options));
+            // "16.07.2019"
         }
 
         static void Main(string[] args)
@@ -64,11 +66,8 @@ namespace DateTimeConverterExamples
             catch (JsonException e)
             {
                 Console.WriteLine(e.Message);
-                // The JSON value could not be converted to System.DateTime. Path: $ | LineNumber: 0 | BytePositionInLine: 20.
+                // The JSON value could not be converted to System.DateTime. Path: $ | LineNumber: 0 | BytePositionInLine: 31.
             }
-
-            // Formatting with default options prints according to extended ISO 8601 profile.
-            FormatDateTimeWithDefaultOptions();
 
             // Using converters gives you control over the serializers parsing and formatting.
             ProcessDateTimeWithCustomConverter();
@@ -77,7 +76,6 @@ namespace DateTimeConverterExamples
 }
 
 // The example displays the following output:
-// The JSON value could not be converted to System.DateTime. Path: $ | LineNumber: 0 | BytePositionInLine: 20.
-// "2008-04-10T06:30:00-04:00"
-// 4/10/2008 6:30:00 AM
-// "4/10/2008 6:30:00 AM"
+// The JSON value could not be converted to System.DateTime.Path: $ | LineNumber: 0 | BytePositionInLine: 31.
+// 7/25/2019 1:36:07 PM
+// "Thu, 25 Jul 2019 09:36:07 GMT"
