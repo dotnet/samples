89b09af6287f8151c36d6219281f2ff170321f0e
Next Turn <45985406+NextTurn@users.noreply.github.com>
2019-08-08 02:29:14 +0800

    Replace Marshall... with Marshal... (#1144)

---
+++ core/interop/pinvoke/marshaling/src/MarshalingSample/EnumMarshaling.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MarshalingSample
+{
+    class EnumMarshaling
+    {
+        public static void Run()
+        {
+            Console.WriteLine("----- Enum marshaling samples -----");
+
+            // return 2
+            int count = MarshalingSampleNative.CountEnumFlags(
+                MarshalingSampleNative.EnumFlags.A | MarshalingSampleNative.EnumFlags.C);
+        }
+    }
+}
