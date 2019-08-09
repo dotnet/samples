2bd17c6422cf20058e7e5b3b2db0294925241672
Maira Wenzel <mairaw@microsoft.com>
2019-08-08 18:46:06 -0700

    undo change + indentation

--- snippets/csharp/VS_Snippets_CLR_System/system.String.IndexOf/CS/ignorable24.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.String.IndexOf/CS/ignorable24.cs
@@ -1,24 +1,26 @@
-﻿using System;
+﻿
+// <Snippet24>
+using System;
 
 public class Example
 {
-   public static void Main()
-   {
-      // <Snippet24>
-      string searchString = "\u00ADm";
-      string s1 = "ani\u00ADmal" ;
-      string s2 = "animal";
+    public static void Main()
+    {
 
-      Console.WriteLine(s1.IndexOf(searchString, 2, 4, StringComparison.CurrentCulture));
-      Console.WriteLine(s1.IndexOf(searchString, 2, 4, StringComparison.Ordinal));
-      Console.WriteLine(s2.IndexOf(searchString, 2, 4, StringComparison.CurrentCulture));
-      Console.WriteLine(s2.IndexOf(searchString, 2, 4, StringComparison.Ordinal));
+        string searchString = "\u00ADm";
+        string s1 = "ani\u00ADmal" ;
+        string s2 = "animal";
 
-      // The example displays the following output:
-      //       4
-      //       3
-      //       3
-      //       -1
-      // </Snippet24>
-   }
+        Console.WriteLine(s1.IndexOf(searchString, 2, 4, StringComparison.CurrentCulture));
+        Console.WriteLine(s1.IndexOf(searchString, 2, 4, StringComparison.Ordinal));
+        Console.WriteLine(s2.IndexOf(searchString, 2, 4, StringComparison.CurrentCulture));
+        Console.WriteLine(s2.IndexOf(searchString, 2, 4, StringComparison.Ordinal));
+
+        // The example displays the following output:
+        //       4
+        //       3
+        //       3
+        //       -1
+    }
 }
+// </Snippet24>
