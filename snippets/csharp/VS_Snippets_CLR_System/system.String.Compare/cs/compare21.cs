4dd81a2cd066687a5134b9fe76b9956e4c46df8f
Maira Wenzel <mairaw@microsoft.com>
2019-08-08 18:33:45 -0700

    fix indentation

--- snippets/csharp/VS_Snippets_CLR_System/system.String.Compare/cs/compare21.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.String.Compare/cs/compare21.cs
@@ -2,18 +2,17 @@
 
 public class Example
 {
-   public static void Main()
-   {
-      // <Snippet21>
-      string s1 = "ani\u00ADmal";
-      string s2 = "animal";
-      
-      Console.WriteLine("Comparison of '{0}' and '{1}': {2}", 
+    public static void Main()
+    {
+        // <Snippet21>
+        string s1 = "ani\u00ADmal";
+        string s2 = "animal";
+
+        Console.WriteLine("Comparison of '{0}' and '{1}': {2}", 
                         s1, s2, String.Compare(s1, s2));
 
-      // The example displays the following output:
-      //       Comparison of 'ani-mal' and 'animal': 0
-      // </Snippet21>
-   }
+        // The example displays the following output:
+        //       Comparison of 'ani-mal' and 'animal': 0
+        // </Snippet21>
+    }
 }
-
