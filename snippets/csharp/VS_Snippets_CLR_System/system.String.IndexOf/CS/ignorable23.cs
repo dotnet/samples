f614b71796a6a7d02248b29d3839cd6d21af2599
Maira Wenzel <mairaw@microsoft.com>
2019-08-08 18:36:49 -0700

    fix indentation

--- snippets/csharp/VS_Snippets_CLR_System/system.String.IndexOf/CS/ignorable23.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.String.IndexOf/CS/ignorable23.cs
@@ -2,19 +2,19 @@
 
 public class Example
 {
-   public static void Main()
-   {
-      // <Snippet23>
-      string searchString = "\u00ADm";
-      string s1 = "ani\u00ADmal" ;
-      string s2 = "animal";
+    public static void Main()
+    {
+        // <Snippet23>
+        string searchString = "\u00ADm";
+        string s1 = "ani\u00ADmal" ;
+        string s2 = "animal";
 
-      Console.WriteLine(s1.IndexOf(searchString, 2, 4));
-      Console.WriteLine(s2.IndexOf(searchString, 2, 4));
+        Console.WriteLine(s1.IndexOf(searchString, 2, 4));
+        Console.WriteLine(s2.IndexOf(searchString, 2, 4));
 
-      // The example displays the following output:
-      //       4
-      //       3
-      // </Snippet23>
-   }
+        // The example displays the following output:
+        //       4
+        //       3
+        // </Snippet23>
+    }
 }
