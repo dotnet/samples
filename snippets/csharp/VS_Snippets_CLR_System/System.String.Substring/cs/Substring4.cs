192bcec757cb4b4ba70d517198c70e4698eb5c57
Maira Wenzel <mairaw@microsoft.com>
2019-08-08 18:29:30 -0700

    fix indentation

--- snippets/csharp/VS_Snippets_CLR_System/System.String.Substring/cs/Substring4.cs
+++ snippets/csharp/VS_Snippets_CLR_System/System.String.Substring/cs/Substring4.cs
@@ -2,17 +2,17 @@
 
 public class Example
 {
-   public static void Main()
-   {
-      // <Snippet4>
-      String value = "This is a string.";
-      int startIndex = 5;
-      int length = 2;
-      String substring = value.Substring(startIndex, length);
-      Console.WriteLine(substring);
+    public static void Main()
+    {
+        // <Snippet4>
+        String value = "This is a string.";
+        int startIndex = 5;
+        int length = 2;
+        String substring = value.Substring(startIndex, length);
+        Console.WriteLine(substring);
 
-      // The example displays the following output:
-      //       is
-      // </Snippet4>
-   }
+        // The example displays the following output:
+        //       is
+        // </Snippet4>
+    }
 }
