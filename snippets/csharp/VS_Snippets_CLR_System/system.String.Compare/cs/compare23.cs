4a85e8b37eced855cff1b97fc6133785ddf176e3
Maira Wenzel <mairaw@microsoft.com>
2019-08-08 18:34:54 -0700

    fix indentation

--- snippets/csharp/VS_Snippets_CLR_System/system.String.Compare/cs/compare23.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.String.Compare/cs/compare23.cs
@@ -3,18 +3,18 @@ using System.Globalization;
 
 public class Example
 {
-   public static void Main()
-   {
-      // <Snippet23>
-      string s1 = "Ani\u00ADmal";
-      string s2 = "animal";
+    public static void Main()
+    {
+        // <Snippet23>
+        string s1 = "Ani\u00ADmal";
+        string s2 = "animal";
       
-      Console.WriteLine("Comparison of '{0}' and '{1}': {2}", 
+        Console.WriteLine("Comparison of '{0}' and '{1}': {2}", 
                         s1, s2, String.Compare(s1, s2, true,
                         CultureInfo.InvariantCulture));
 
-      // The example displays the following output:
-      //       Comparison of 'ani-mal' and 'animal': 0
-      // </Snippet23>
-   }
+        // The example displays the following output:
+        //       Comparison of 'ani-mal' and 'animal': 0
+        // </Snippet23>
+    }
 }
