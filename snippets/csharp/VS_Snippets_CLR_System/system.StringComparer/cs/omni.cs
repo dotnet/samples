38a8e7de9b386113e990a11f13a2d73f0e7d22ed
Yuyang Dong <yudomsft@users.noreply.github.com>
2019-08-12 12:49:54 -0700

    Fix indentation (#1154)

--- snippets/csharp/VS_Snippets_CLR_System/system.StringComparer/cs/omni.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.StringComparer/cs/omni.cs
@@ -12,81 +12,81 @@ class Sample
 {
     public static void Main() 
     {
-// Create a list of string.
-    List<string> list = new List<string>();
+        // Create a list of string.
+        List<string> list = new List<string>();
 
-// Get the tr-TR (Turkish-Turkey) culture.
-    CultureInfo turkish = new CultureInfo("tr-TR");
+        // Get the tr-TR (Turkish-Turkey) culture.
+        CultureInfo turkish = new CultureInfo("tr-TR");
 
-// Get the culture that is associated with the current thread.
-    CultureInfo thisCulture = Thread.CurrentThread.CurrentCulture;
+        // Get the culture that is associated with the current thread.
+        CultureInfo thisCulture = Thread.CurrentThread.CurrentCulture;
 
-// Get the standard StringComparers.
-    StringComparer invCmp =   StringComparer.InvariantCulture;
-    StringComparer invICCmp = StringComparer.InvariantCultureIgnoreCase;
-    StringComparer currCmp = StringComparer.CurrentCulture;
-    StringComparer currICCmp = StringComparer.CurrentCultureIgnoreCase;
-    StringComparer ordCmp = StringComparer.Ordinal;
-    StringComparer ordICCmp = StringComparer.OrdinalIgnoreCase;
+        // Get the standard StringComparers.
+        StringComparer invCmp =   StringComparer.InvariantCulture;
+        StringComparer invICCmp = StringComparer.InvariantCultureIgnoreCase;
+        StringComparer currCmp = StringComparer.CurrentCulture;
+        StringComparer currICCmp = StringComparer.CurrentCultureIgnoreCase;
+        StringComparer ordCmp = StringComparer.Ordinal;
+        StringComparer ordICCmp = StringComparer.OrdinalIgnoreCase;
 
-// Create a StringComparer that uses the Turkish culture and ignores case.
-    StringComparer turkICComp = StringComparer.Create(turkish, true);
+        // Create a StringComparer that uses the Turkish culture and ignores case.
+        StringComparer turkICComp = StringComparer.Create(turkish, true);
 
-// Define three strings consisting of different versions of the letter I.
-// LATIN CAPITAL LETTER I (U+0049)
-    string capitalLetterI = "I";  
+        // Define three strings consisting of different versions of the letter I.
+        // LATIN CAPITAL LETTER I (U+0049)
+        string capitalLetterI = "I";  
 
-// LATIN SMALL LETTER I (U+0069)
-    string smallLetterI   = "i";
+        // LATIN SMALL LETTER I (U+0069)
+        string smallLetterI   = "i";
 
-// LATIN SMALL LETTER DOTLESS I (U+0131)
-    string smallLetterDotlessI = "\u0131";
+        // LATIN SMALL LETTER DOTLESS I (U+0131)
+        string smallLetterDotlessI = "\u0131";
 
-// Add the three strings to the list.
-    list.Add(capitalLetterI);
-    list.Add(smallLetterI);
-    list.Add(smallLetterDotlessI);
+        // Add the three strings to the list.
+        list.Add(capitalLetterI);
+        list.Add(smallLetterI);
+        list.Add(smallLetterDotlessI);
 
-// Display the original list order.
-    Display(list, "The original order of the list entries...");
+        // Display the original list order.
+        Display(list, "The original order of the list entries...");
 
-// Sort the list using the invariant culture.
-    list.Sort(invCmp);
-    Display(list, "Invariant culture...");
-    list.Sort(invICCmp);
-    Display(list, "Invariant culture, ignore case...");
+        // Sort the list using the invariant culture.
+        list.Sort(invCmp);
+        Display(list, "Invariant culture...");
+        list.Sort(invICCmp);
+        Display(list, "Invariant culture, ignore case...");
 
-// Sort the list using the current culture.
-    Console.WriteLine("The current culture is \"{0}\".", thisCulture.Name);
-    list.Sort(currCmp);
-    Display(list, "Current culture...");
-    list.Sort(currICCmp);
-    Display(list, "Current culture, ignore case...");
+        // Sort the list using the current culture.
+        Console.WriteLine("The current culture is \"{0}\".", thisCulture.Name);
+        list.Sort(currCmp);
+        Display(list, "Current culture...");
+        list.Sort(currICCmp);
+        Display(list, "Current culture, ignore case...");
 
-// Sort the list using the ordinal value of the character code points.
-    list.Sort(ordCmp);
-    Display(list, "Ordinal...");
-    list.Sort(ordICCmp);
-    Display(list, "Ordinal, ignore case...");
+        // Sort the list using the ordinal value of the character code points.
+        list.Sort(ordCmp);
+        Display(list, "Ordinal...");
+        list.Sort(ordICCmp);
+        Display(list, "Ordinal, ignore case...");
 
-// Sort the list using the Turkish culture, which treats LATIN SMALL LETTER 
-// DOTLESS I differently than LATIN SMALL LETTER I.
-    list.Sort(turkICComp);
-    Display(list, "Turkish culture, ignore case...");
+        // Sort the list using the Turkish culture, which treats LATIN SMALL LETTER 
+        // DOTLESS I differently than LATIN SMALL LETTER I.
+        list.Sort(turkICComp);
+        Display(list, "Turkish culture, ignore case...");
     }
 
     public static void Display(List<string> lst, string title)
     {
-    Char c;
-    int  codePoint;
-    Console.WriteLine(title);
-    foreach (string s in lst)
+        Char c;
+        int  codePoint;
+        Console.WriteLine(title);
+        foreach (string s in lst)
         {
-        c = s[0];
-        codePoint = Convert.ToInt32(c);
-        Console.WriteLine("0x{0:x}", codePoint); 
+            c = s[0];
+            codePoint = Convert.ToInt32(c);
+            Console.WriteLine("0x{0:x}", codePoint); 
         }
-    Console.WriteLine();
+        Console.WriteLine();
     }
 }
 /*
