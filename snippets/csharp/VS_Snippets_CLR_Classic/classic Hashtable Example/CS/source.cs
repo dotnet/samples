80c13f60c103b00bff9bf568e4e18426ebe91eb5
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-08-13 17:17:09 +0200

    Update source.cs

--- snippets/csharp/VS_Snippets_CLR_Classic/classic Hashtable Example/CS/source.cs
+++ snippets/csharp/VS_Snippets_CLR_Classic/classic Hashtable Example/CS/source.cs
@@ -1,28 +1,31 @@
-﻿// <Snippet1>
+// <Snippet1>
  using System;
  using System.Collections;
- public class SamplesHashtable  {
+ public class SamplesHashtable
+ {
  
-    public static void Main()  {
+    public static void Main()
+    {
  
        // Creates and initializes a new Hashtable.
-       Hashtable myHT = new Hashtable();
+       var myHT = new Hashtable();
        myHT.Add("First", "Hello");
        myHT.Add("Second", "World");
        myHT.Add("Third", "!");
  
        // Displays the properties and values of the Hashtable.
-       Console.WriteLine( "myHT" );
-       Console.WriteLine( "  Count:    {0}", myHT.Count );
-       Console.WriteLine( "  Keys and Values:" );
-       PrintKeysAndValues( myHT );
+       Console.WriteLine("myHT");
+       Console.WriteLine($"  Count:    {myHT.Count}");
+       Console.WriteLine("  Keys and Values:");
+       PrintKeysAndValues(myHT);
     }
  
  
-    public static void PrintKeysAndValues( Hashtable myHT )  {
+    public static void PrintKeysAndValues( Hashtable myHT )
+    {
        Console.WriteLine( "\t-KEY-\t-VALUE-" );
-       foreach ( DictionaryEntry de in myHT )
-          Console.WriteLine("\t{0}:\t{1}", de.Key, de.Value);
+       foreach (DictionaryEntry de in myHT)
+          Console.WriteLine($"\t{de.Key}:\t{de.Value}");
        Console.WriteLine();
     }
 
