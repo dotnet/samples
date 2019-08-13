bfe3fa765e13ff9b11bf0424371390385e53026d
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-08-13 17:08:41 +0200

    Update hashtable_ctordictionary.vb

--- snippets/visualbasic/VS_Snippets_CLR_System/system.Collections.Hashtable_ctorDictionary/VB/hashtable_ctordictionary.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.Collections.Hashtable_ctorDictionary/VB/hashtable_ctordictionary.vb
@@ -1,4 +1,4 @@
-﻿' The following code example creates hash tables using different Hashtable
+' The following code example creates hash tables using different Hashtable
 ' constructors and demonstrates the differences in the behavior of the hash
 ' tables, even if each one contains the same elements.
 
@@ -23,11 +23,7 @@ Public Class myCultureComparer
     Public Function Equals1(ByVal x As Object, ByVal y As Object) _
         As Boolean Implements IEqualityComparer.Equals
 
-        If (myComparer.Compare(x, y) = 0) Then
-            Return True
-        Else
-            Return False
-        End If
+        Return myComparer.Compare(x, y) = 0
     End Function
 
     Public Function GetHashCode1(ByVal obj As Object) _
@@ -60,9 +56,9 @@ Public Class SamplesHashtable
       Dim myHT3 As New Hashtable(mySL, New myCultureComparer(myCul))
 
       ' Search for a key in each hash table.
-      Console.WriteLine("first is in myHT1: {0}", myHT1.ContainsKey("first"))
-      Console.WriteLine("first is in myHT2: {0}", myHT2.ContainsKey("first"))
-      Console.WriteLine("first is in myHT3: {0}", myHT3.ContainsKey("first"))
+      Console.WriteLine($"first is in myHT1: {myHT1.ContainsKey("first")}")
+      Console.WriteLine($"first is in myHT2: {myHT2.ContainsKey("first")}")
+      Console.WriteLine($"first is in myHT3: {myHT3.ContainsKey("first")}")
 
    End Sub 'Main 
 
