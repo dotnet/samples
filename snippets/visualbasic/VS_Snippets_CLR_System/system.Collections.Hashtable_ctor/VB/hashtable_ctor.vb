f4957b7497e7baa4c34fe98e405ca9180ffcf3e7
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-08-13 17:00:30 +0200

    Use string interpolation

--- snippets/visualbasic/VS_Snippets_CLR_System/system.Collections.Hashtable_ctor/VB/hashtable_ctor.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.Collections.Hashtable_ctor/VB/hashtable_ctor.vb
@@ -1,4 +1,4 @@
-﻿' The following code example creates hash tables using different Hashtable
+' The following code example creates hash tables using different Hashtable
 ' constructors and demonstrates the differences in the behavior of the hash
 ' tables, even if each one contains the same elements.
 
@@ -90,10 +90,10 @@ Public Class SamplesHashtable
         myHT4.Add("THIRD", "!")
 
         ' Search for a key in each hash table.
-        Console.WriteLine("first is in myHT1: {0}", myHT1.ContainsKey("first"))
-        Console.WriteLine("first is in myHT2: {0}", myHT2.ContainsKey("first"))
-        Console.WriteLine("first is in myHT3: {0}", myHT3.ContainsKey("first"))
-        Console.WriteLine("first is in myHT4: {0}", myHT4.ContainsKey("first"))
+        Console.WriteLine($"first is in myHT1: {myHT1.ContainsKey("first")}")
+        Console.WriteLine($"first is in myHT2: {myHT2.ContainsKey("first")}")
+        Console.WriteLine($"first is in myHT3: {myHT3.ContainsKey("first")}")
+        Console.WriteLine($"first is in myHT4: {myHT4.ContainsKey("first")}")
 
     End Sub
 
