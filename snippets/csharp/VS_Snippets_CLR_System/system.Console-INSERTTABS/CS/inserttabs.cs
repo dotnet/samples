329c163ae9563f13f04c96f6f51f820c2d9a81f5
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-08-14 10:34:00 +0200

    Update inserttabs.cs

--- snippets/csharp/VS_Snippets_CLR_System/system.Console-INSERTTABS/CS/inserttabs.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.Console-INSERTTABS/CS/inserttabs.cs
@@ -13,43 +13,47 @@
 using System;
 using System.IO;
 
-public class InsertTabs {
+public class InsertTabs
+{
     private const int tabSize = 4;
     private const string usageText = "Usage: INSERTTABS inputfile.txt outputfile.txt";
-    public static int Main(string[] args) {
-        StreamWriter writer = null;
-
-        if (args.Length < 2) {
+    public static int Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
             Console.WriteLine(usageText);
             return 1;
         }
 
-        try {
+        try
+        {
             // Attempt to open output file.
-            writer = new StreamWriter(args[1]);
+             var writer = new StreamWriter(args[1]);
             // Redirect standard output from the console to the output file.
             Console.SetOut(writer);
             // Redirect standard input from the console to the input file.
             Console.SetIn(new StreamReader(args[0]));
         }
-        catch(IOException e) {
+        catch(IOException e)
+        {
             TextWriter errorWriter = Console.Error;
             errorWriter.WriteLine(e.Message);
             errorWriter.WriteLine(usageText);
             return 1;            
         }
         string line;
-        while ((line = Console.ReadLine()) != null) {
+        while ((line = Console.ReadLine()) != null)
+        {
             string newLine = line.Replace(("").PadRight(tabSize, ' '), "\t");
             Console.WriteLine(newLine);
         }
         writer.Close();
         // Recover the standard output stream so that a 
         // completion message can be displayed.
-        StreamWriter standardOutput = new StreamWriter(Console.OpenStandardOutput());
+        var standardOutput = new StreamWriter(Console.OpenStandardOutput());
         standardOutput.AutoFlush = true;
         Console.SetOut(standardOutput);
-        Console.WriteLine("INSERTTABS has completed the processing of {0}.", args[0]);
+        Console.WriteLine($"INSERTTABS has completed the processing of {args[0]}.");
         return 0;
     }
 }
