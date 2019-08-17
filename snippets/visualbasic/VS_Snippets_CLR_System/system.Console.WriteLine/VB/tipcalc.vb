55d867c9db1e5d479813310a95709f21257db0fd
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-08-17 08:50:31 +0200

    Update tipcalc.vb

--- snippets/visualbasic/VS_Snippets_CLR_System/system.Console.WriteLine/VB/tipcalc.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.Console.WriteLine/VB/tipcalc.vb
@@ -1,27 +1,23 @@
 ' <Snippet1>
-Public Class TipCalculator
-   Private Const tipRate As Double = 0.18
+Public Module TipCalculator
+    Private Const tipRate As Double = 0.18
    
-   Public Shared Sub Main()
-      System.Environment.ExitCode = Calculator(System.Environment.GetCommandLineArgs())
-   End Sub
-   
-   Public Shared Function Calculator(args() As String) As Integer
-      Dim billTotal As Double
-      If (args.Length < 2) OrElse (Not Double.TryParse(args(1), billTotal)) Then
-         Console.WriteLine("usage: TIPCALC total")
-         Return 1
-      End If
-     
-      Dim tip As Double = billTotal * tipRate
-      Console.WriteLine()
-      Console.WriteLine($"Bill total:{vbTab}{billTotal,8:c}")
-      Console.WriteLine($"Tip total/rate:{vbTab}{tip,8:c} ({tipRate:p1})")
-      Console.WriteLine("".PadRight(24, "-"c))
-      Console.WriteLine($"Grand total:{vbTab}{billTotal + tip,8:c}")
-      Return 0
-   End Function 
-End Class 
+    Public Shared Sub Main(args As String())
+        Dim billTotal As Double
+        If (args.Length < 2) OrElse (Not Double.TryParse(args(1), billTotal)) Then
+            Console.WriteLine("usage: TIPCALC total")
+            Return
+        End If
+
+        Dim tip As Double = billTotal * tipRate
+        Console.WriteLine()
+        Console.WriteLine($"Bill total:{vbTab}{billTotal,8:c}")
+        Console.WriteLine($"Tip total/rate:{vbTab}{tip,8:c} ({tipRate:p1})")
+        Console.WriteLine("".PadRight(24, "-"c))
+        Console.WriteLine($"Grand total:{vbTab}{billTotal + tip,8:c}")
+    End Sub
+
+End Module
 
 'Example Output:
 '---------------
