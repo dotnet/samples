8af2665a9c54d558a5ccb64c4303696480ac1ac1
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-08-17 09:33:58 +0200

    Update inserttabs.vb

--- snippets/visualbasic/VS_Snippets_CLR_System/system.Console-INSERTTABS/VB/inserttabs.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.Console-INSERTTABS/VB/inserttabs.vb
@@ -12,11 +12,11 @@
 ' <Snippet1>
 Imports System.IO
 
-Public Class InsertTabs
+Public Module InsertTabs
     Private Const tabSize As Integer = 4
     Private Const usageText As String = "Usage: INSERTTABS inputfile.txt outputfile.txt"
    
-    Public Shared Function Main(args() As String) As Integer
+    Public Function Main(args As String()) As Integer
         If args.Length < 2 Then
             Console.WriteLine(usageText)
             Return 1
@@ -25,13 +25,19 @@ Public Class InsertTabs
         Try
             ' Attempt to open output file.
             Using writer As New StreamWriter(args(1))
-                ' Redirect standard output from the console to the output file.
-                Console.SetOut(writer)
-			End Using
-			Using reader As New StreamReader(args(0))
-			    ' Redirect standard input from the console to the input file.
-                Console.SetIn(reader)
-			End Using
+                Using reader As New StreamReader(args(0))
+                    ' Redirect standard output from the console to the output file.
+                    Console.SetOut(writer)
+                    ' Redirect standard input from the console to the input file.
+                    Console.SetIn(reader)
+                    Dim line As String = Console.ReadLine()
+                    While line IsNot Nothing
+                        Dim newLine As String = line.Replace("".PadRight(tabSize, " "c), ControlChars.Tab)
+                        Console.WriteLine(newLine)
+                        line = Console.ReadLine()
+                    End While
+                End Using
+            End Using
         Catch e As IOException
             Dim errorWriter As TextWriter = Console.Error
             errorWriter.WriteLine(e.Message)
@@ -39,12 +45,6 @@ Public Class InsertTabs
             Return 1
         End Try
 
-        Dim line As String = Console.ReadLine()
-        While line IsNot Nothing
-            Dim newLine As String = line.Replace("".PadRight(tabSize, " "c), ControlChars.Tab)
-            Console.WriteLine(newLine)
-            line = Console.ReadLine()
-        End While
         ' Recover the standard output stream so that a 
         ' completion message can be displayed.
         Dim standardOutput As New StreamWriter(Console.OpenStandardOutput())
@@ -53,5 +53,5 @@ Public Class InsertTabs
         Console.WriteLine($"INSERTTABS has completed the processing of {args(0)}.")
         Return 0
     End Function 
-End Class
+End Module
 ' </Snippet1>
