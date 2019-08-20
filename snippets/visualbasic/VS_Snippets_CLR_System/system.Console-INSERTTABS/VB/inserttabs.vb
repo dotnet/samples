94551009ba753a83f62f2ef3af41437dc3d9188c
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-08-20 23:19:21 +0200

    Modernizing example (#1169)
    * Modernizing example
    
    * Update tipcalc.vb
    
    * Update reformat.cs
    
    * Update inserttabs.cs
    
    * Update inserttabs.cs
    
    * Update inserttabs.vb
    
    * Update inserttabs.cs
    
    * Update inserttabs.vb
    
    * Update inserttabs.vb
    
    * Update inserttabs.cs
    
    * Update snippets/csharp/VS_Snippets_CLR_System/system.Console-REFORMAT/CS/reformat.cs
    
    Co-Authored-By: Ron Petrusha <ronpet@microsoft.com>
    
    * Update reformat.cs
    
    * Update reformat.vb
    
    * Update tipcalc.cs
    
    * Update tipcalc.vb
    
    * Update reformat.vb
    
    * Update tipcalc.vb
    
    * Update tipcalc.cs
    
    * Update inserttabs.cs
    
    * Update tipcalc.vb
    
    * Update inserttabs.vb
    
    * Update reformat.cs
    
    * Update reformat.vb
    
    * Update snippets/visualbasic/VS_Snippets_CLR_System/system.Console.WriteLine/VB/tipcalc.vb
    
    Co-Authored-By: Ron Petrusha <ronpet@microsoft.com>

--- snippets/visualbasic/VS_Snippets_CLR_System/system.Console-INSERTTABS/VB/inserttabs.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.Console-INSERTTABS/VB/inserttabs.vb
@@ -1,4 +1,4 @@
-﻿' This sample opens a file whose name is passed to it as a parameter. 
+' This sample opens a file whose name is passed to it as a parameter. 
 ' It reads each line in the file and replaces every occurrence of 4 
 ' space characters with a tab character.
 '
@@ -12,46 +12,46 @@
 ' <Snippet1>
 Imports System.IO
 
-Public Class InsertTabs
-   Private Const tabSize As Integer = 4
-   Private Const usageText As String = "Usage: INSERTTABS inputfile.txt outputfile.txt"
+Public Module InsertTabs
+    Private Const tabSize As Integer = 4
+    Private Const usageText As String = "Usage: INSERTTABS inputfile.txt outputfile.txt"
    
-   Public Shared Function Main(args() As String) As Integer
-      Dim writer As StreamWriter = Nothing
-
-      If args.Length < 2 Then
-         Console.WriteLine(usageText)
-         Return 1
-      End If
+    Public Function Main(args As String()) As Integer
+        If args.Length < 2 Then
+            Console.WriteLine(usageText)
+            Return 1
+        End If
       
-      Try
-         ' Attempt to open output file.
-         writer = New StreamWriter(args(1))
-         ' Redirect standard output from the console to the output file.
-         Console.SetOut(writer)
-         ' Redirect standard input from the console to the input file.
-         Console.SetIn(New StreamReader(args(0)))
-      Catch e As IOException
-         Dim errorWriter As TextWriter = Console.Error
-         errorWriter.WriteLine(e.Message)
-         errorWriter.WriteLine(usageText)
-         Return 1
-      End Try
+        Try
+            ' Attempt to open output file.
+            Using writer As New StreamWriter(args(1))
+                Using reader As New StreamReader(args(0))
+                    ' Redirect standard output from the console to the output file.
+                    Console.SetOut(writer)
+                    ' Redirect standard input from the console to the input file.
+                    Console.SetIn(reader)
+                    Dim line As String = Console.ReadLine()
+                    While line IsNot Nothing
+                        Dim newLine As String = line.Replace("".PadRight(tabSize, " "c), ControlChars.Tab)
+                        Console.WriteLine(newLine)
+                        line = Console.ReadLine()
+                    End While
+                End Using
+            End Using
+        Catch e As IOException
+            Dim errorWriter As TextWriter = Console.Error
+            errorWriter.WriteLine(e.Message)
+            errorWriter.WriteLine(usageText)
+            Return 1
+        End Try
 
-      Dim line As String = Console.ReadLine()
-      While line IsNot Nothing
-         Dim newLine As String = line.Replace("".PadRight(tabSize, " "c), ControlChars.Tab)
-         Console.WriteLine(newLine)
-         line = Console.ReadLine()
-      End While
-      writer.Close()
-      ' Recover the standard output stream so that a 
-      ' completion message can be displayed.
-      Dim standardOutput As New StreamWriter(Console.OpenStandardOutput())
-      standardOutput.AutoFlush = True
-      Console.SetOut(standardOutput)
-      Console.WriteLine("INSERTTABS has completed the processing of {0}.", args(0))
-      Return 0
-   End Function 
-End Class
+        ' Recover the standard output stream so that a 
+        ' completion message can be displayed.
+        Dim standardOutput As New StreamWriter(Console.OpenStandardOutput())
+        standardOutput.AutoFlush = True
+        Console.SetOut(standardOutput)
+        Console.WriteLine($"INSERTTABS has completed the processing of {args(0)}.")
+        Return 0
+    End Function 
+End Module
 ' </Snippet1>
