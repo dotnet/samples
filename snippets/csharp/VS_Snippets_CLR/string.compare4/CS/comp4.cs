ea7fccf8eec96500671521e8e0628ec51f02bc44
WilliamAntonRohm <v-wiroh@microsoft.com>
2019-08-21 14:49:36 -0700

    updating System.String examples for Try .NET

--- snippets/csharp/VS_Snippets_CLR/string.compare4/CS/comp4.cs
+++ snippets/csharp/VS_Snippets_CLR/string.compare4/CS/comp4.cs
@@ -1,9 +1,9 @@
-﻿//<snippet1>
-// Sample for String.Compare(String, Int32, String, Int32, Int32, Boolean)
+﻿// Sample for String.Compare(String, Int32, String, Int32, Int32, Boolean)
 using System;
 
 class Sample {
     public static void Main() {
+    //<snippet1>
 //                 0123456
     String str1 = "MACHINE";
     String str2 = "machine";
@@ -26,16 +26,17 @@ class Sample {
     Console.Write("Substring '{0}' in '{1}' is ", str1.Substring(2, 2), str1);
     Console.Write("{0} ", str);
     Console.WriteLine("substring '{0}' in '{1}'.", str2.Substring(2, 2), str2);
+
+    /*
+    This example produces the following results:
+
+    str1 = 'MACHINE', str2 = 'machine'
+    Ignore case:
+    Substring 'CH' in 'MACHINE' is equal to substring 'ch' in 'machine'.
+
+    Honor case:
+    Substring 'CH' in 'MACHINE' is greater than substring 'ch' in 'machine'.
+    */
+    //</snippet1>
     }
 }
-/*
-This example produces the following results:
-
-str1 = 'MACHINE', str2 = 'machine'
-Ignore case:
-Substring 'CH' in 'MACHINE' is equal to substring 'ch' in 'machine'.
-
-Honor case:
-Substring 'CH' in 'MACHINE' is greater than substring 'ch' in 'machine'.
-*/
-//</snippet1>
