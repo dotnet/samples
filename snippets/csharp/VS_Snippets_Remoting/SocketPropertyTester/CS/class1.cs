20dd543ef3c53cec082c5953cdbd884e56240d07
Maira Wenzel <mairaw@microsoft.com>
2019-08-22 16:21:09 -0700

    enhance indentation

--- snippets/csharp/VS_Snippets_Remoting/SocketPropertyTester/CS/class1.cs
+++ snippets/csharp/VS_Snippets_Remoting/SocketPropertyTester/CS/class1.cs
@@ -9,25 +9,25 @@ namespace socketoption_examples
         //<Snippet1>
 		static void ConfigureTcpSocket(Socket tcpSocket)
 		{
-			// Don't allow another socket to bind to this port.
-			tcpSocket.ExclusiveAddressUse = true;
+            // Don't allow another socket to bind to this port.
+            tcpSocket.ExclusiveAddressUse = true;
 
-			// The socket will linger for 10 seconds after 
-                        // Socket.Close is called.
-			tcpSocket.LingerState = new LingerOption (true, 10);
+            // The socket will linger for 10 seconds after 
+            // Socket.Close is called.
+            tcpSocket.LingerState = new LingerOption (true, 10);
 
-			// Disable the Nagle Algorithm for this tcp socket.
-			tcpSocket.NoDelay = true;
+            // Disable the Nagle Algorithm for this tcp socket.
+            tcpSocket.NoDelay = true;
 
-			// Set the receive buffer size to 8k
-			tcpSocket.ReceiveBufferSize = 8192;
+            // Set the receive buffer size to 8k
+            tcpSocket.ReceiveBufferSize = 8192;
 
-			// Set the timeout for synchronous receive methods to 
-			// 1 second (1000 milliseconds.)
-			tcpSocket.ReceiveTimeout = 1000;
+            // Set the timeout for synchronous receive methods to 
+            // 1 second (1000 milliseconds.)
+            tcpSocket.ReceiveTimeout = 1000;
 
-			// Set the send buffer size to 8k.
-			tcpSocket.SendBufferSize = 8192;
+            // Set the send buffer size to 8k.
+            tcpSocket.SendBufferSize = 8192;
 
 			// Set the timeout for synchronous send methods
 			// to 1 second (1000 milliseconds.)			
@@ -39,32 +39,32 @@ namespace socketoption_examples
 			Console.WriteLine("Tcp Socket configured:");
 
 			Console.WriteLine("  ExclusiveAddressUse {0}", 
-						tcpSocket.ExclusiveAddressUse);
+                tcpSocket.ExclusiveAddressUse);
 
 			Console.WriteLine("  LingerState {0}, {1}", 
-					     tcpSocket.LingerState.Enabled, 
-				             tcpSocket.LingerState.LingerTime);
+                tcpSocket.LingerState.Enabled, 
+                tcpSocket.LingerState.LingerTime);
 
 			Console.WriteLine("  NoDelay {0}", 
-                                                tcpSocket.NoDelay);
+                tcpSocket.NoDelay);
 
 			Console.WriteLine("  ReceiveBufferSize {0}", 
-						tcpSocket.ReceiveBufferSize);
+                tcpSocket.ReceiveBufferSize);
 
 			Console.WriteLine("  ReceiveTimeout {0}", 
-						tcpSocket.ReceiveTimeout);
+                tcpSocket.ReceiveTimeout);
 
 			Console.WriteLine("  SendBufferSize {0}", 
-						tcpSocket.SendBufferSize);
+                tcpSocket.SendBufferSize);
 
 			Console.WriteLine("  SendTimeout {0}", 
-                                                tcpSocket.SendTimeout);
+                tcpSocket.SendTimeout);
 
 			Console.WriteLine("  Ttl {0}", 
-                                                tcpSocket.Ttl);
+                tcpSocket.Ttl);
 
-                        Console.WriteLine("  IsBound {0}", 
-                                                tcpSocket.IsBound);
+            Console.WriteLine("  IsBound {0}", 
+                tcpSocket.IsBound);
 
 			Console.WriteLine("");
 		}
@@ -73,21 +73,21 @@ namespace socketoption_examples
         //<Snippet2>
         static void ConfigureUdpSocket(Socket udpSocket)
 		{
-			// set the Don't Fragment flag.
-			udpSocket.DontFragment = true;
-			// Enable broadcast.
-			udpSocket.EnableBroadcast = true;
+            // set the Don't Fragment flag.
+            udpSocket.DontFragment = true;
+            // Enable broadcast.
+            udpSocket.EnableBroadcast = true;
 
-			// Disable multicast loopback.
-			udpSocket.MulticastLoopback = false;
+            // Disable multicast loopback.
+            udpSocket.MulticastLoopback = false;
 
-			Console.WriteLine("Udp Socket configured:");
-			Console.WriteLine("  DontFragment {0}", 
-                                                udpSocket.DontFragment);
-			Console.WriteLine("  EnableBroadcast {0}", 
-                                                udpSocket.EnableBroadcast);
+            Console.WriteLine("Udp Socket configured:");
+            Console.WriteLine("  DontFragment {0}", 
+                udpSocket.DontFragment);
+            Console.WriteLine("  EnableBroadcast {0}", 
+                udpSocket.EnableBroadcast);
 			Console.WriteLine("  MulticastLoopback {0}", 
-                                                udpSocket.MulticastLoopback);
+                udpSocket.MulticastLoopback);
 		}
         //</Snippet2>
 
