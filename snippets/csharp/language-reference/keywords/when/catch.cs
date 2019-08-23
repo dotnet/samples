95126a03a3b860c396247d7e079d6ff937a5dc28
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-08-23 18:51:42 +0200

    Modernizing code

--- snippets/csharp/language-reference/keywords/when/catch.cs
+++ snippets/csharp/language-reference/keywords/when/catch.cs
@@ -6,24 +6,28 @@ class Program
 {
    static void Main()
    {
-      Console.WriteLine(MakeRequest().Result);
+       Console.WriteLine(MakeRequest().Result);
    }
 
    public static async Task<string> MakeRequest()
    { 
-       var client = new System.Net.Http.HttpClient();
+       var client = new HttpClient();
        var streamTask = client.GetStringAsync("https://localHost:10000");
-       try {
+       try
+       {
            var responseText = await streamTask;
            return responseText;
        } 
-       catch (HttpRequestException e) when (e.Message.Contains("301")) {
+       catch (HttpRequestException e) when (e.Message.Contains("301"))
+       {
            return "Site Moved";
        }
-       catch (HttpRequestException e) when (e.Message.Contains("404")) {
+       catch (HttpRequestException e) when (e.Message.Contains("404"))
+       {
            return "Page Not Found";
        }
-       catch (HttpRequestException e) {
+       catch (HttpRequestException e)
+       {
            return e.Message;
        }
    }
