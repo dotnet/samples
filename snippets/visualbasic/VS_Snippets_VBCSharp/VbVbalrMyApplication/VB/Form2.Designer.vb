2a63be1158af395efcbcdf7c155beb7656bd3ee2
Paul M Cohen <paul1956@users.noreply.github.com>
2019-08-23 19:33:34 -0700

    Respond to feedback, restore autogenerated file and don't upload sln file

--- snippets/visualbasic/VS_Snippets_VBCSharp/VbVbalrMyApplication/VB/Form2.Designer.vb
+++ snippets/visualbasic/VS_Snippets_VBCSharp/VbVbalrMyApplication/VB/Form2.Designer.vb
@@ -11,8 +11,8 @@
     End Sub
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()>
-    Protected Overloads Overrides Sub Dispose(disposing As Boolean)
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
         If disposing AndAlso components IsNot Nothing Then
             components.Dispose()
         End If
@@ -23,7 +23,7 @@
     Private components As System.ComponentModel.IContainer
 
     'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
+    'It can be modified using the Windows Form Designer.
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
