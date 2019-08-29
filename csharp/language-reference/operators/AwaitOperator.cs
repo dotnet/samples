0586d9033aff3b24358e92c485b6a587e121ddfd
Petr Kulikov <petr.kulikov@gmail.com>
2019-08-29 16:37:55 +0200

    Update csharp/language-reference/operators/AwaitOperator.cs
    Co-Authored-By: Bill Wagner <wiwagn@microsoft.com>

--- csharp/language-reference/operators/AwaitOperator.cs
+++ csharp/language-reference/operators/AwaitOperator.cs
@@ -6,7 +6,7 @@ public class AwaitOperator
 {
     public static async Task Main()
     {
-        Task<int> downloading = DownloadDocsMainPage();
+        Task<int> downloading = DownloadDocsMainPageAsync();
         Console.WriteLine($"{nameof(Main)}: Launched downloading.");
 
         int bytesLoaded = await downloading;
