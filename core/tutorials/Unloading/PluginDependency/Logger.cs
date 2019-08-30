9683153b91dd09e701068b91ffc41036a5782a8f
Steve MacLean <Steve.MacLean@Microsoft.com>
2019-08-30 19:02:13 -0400

    Revert "Fix build error"
    This reverts commit 90d84c7970ddb13b36b748177cb48ada9cc78823.

---
+++ core/tutorials/Unloading/PluginDependency/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PluginDependency
+{
+    // This is a simple class to write message to console. It is present to demostrate
+    // how a dependency of the plugin gets loaded into the HostAssemblyLoadContext
+    public class Logger
+    {
+        public static void LogMessage(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
