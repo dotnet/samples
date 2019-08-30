00059a6e05f4de981bca8ef62efd0dbc6e489184
Ron Petrusha <ronpet@microsoft.com>
2019-08-30 13:43:03 -0700

    Update source.vb

--- snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData OleDbError.ToString Example/VB/source.vb
+++ snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData OleDbError.ToString Example/VB/source.vb
@@ -3,6 +3,7 @@ Imports System.Data
 Imports System.Data.OleDb
 Imports System.Data.Common
 Imports System.Windows.Forms
+
 Public Class Form1
     Inherits Form
     Protected DataSet1 As DataSet
@@ -19,4 +20,3 @@ Public Class Form1
  End Sub
 ' </Snippet1>
 End Class
-
