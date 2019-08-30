eb5f60b73460f7ec28ab429ec1ce627c09415437
Ron Petrusha <ronpet@microsoft.com>
2019-08-30 13:38:14 -0700

    Update source.vb

--- snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData OracleParameterCollection.Count/VB/source.vb
+++ snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData OracleParameterCollection.Count/VB/source.vb
@@ -3,6 +3,7 @@ Imports System.Data
 Imports System.Data.OracleClient
 Imports System.Data.Common
 Imports System.Windows.Forms
+
 Public Class Form1
     Inherits Form
     Protected DataSet1 As DataSet
