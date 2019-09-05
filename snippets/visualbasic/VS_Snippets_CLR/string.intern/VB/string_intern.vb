4f2511c30a46f28bbd736b5a8b3a1b186136b635
Stephen Harper (PACTERA TECHNOLOGIES INC) <v-stharp@microsoft.com>
2019-09-05 15:45:25 -0700

    adding in ron's suggestions for Visual Basic example for String.Intern

--- snippets/visualbasic/VS_Snippets_CLR/string.intern/VB/string_intern.vb
+++ snippets/visualbasic/VS_Snippets_CLR/string.intern/VB/string_intern.vb
@@ -1,18 +1,19 @@
-﻿Imports System.Text
+﻿'<snippet1>
+Imports System.Text
 
 Class Sample
 
     Public Shared Sub Main()
-        Dim s1 As [String] = "MyTest"
-        Dim s2 As [String] = New StringBuilder().Append("My").Append("Test").ToString()
-        Dim s3 As [String] = [String].Intern(s2)
+        Dim s1 As String = "MyTest"
+        Dim s2 As String = New StringBuilder().Append("My").Append("Test").ToString()
+        Dim s3 As String = String.Intern(s2)
         Console.WriteLine($"s1 = {s1}")
         Console.WriteLine($"s2 = {s2}")
         Console.WriteLine($"s3 = {s3}")
         Console.WriteLine($"Is s2 the same reference as s1?: {s2 Is s1}")
         Console.WriteLine($"Is s3 the same reference as s1?: {s3 Is s1}")
-    End Sub 'Main
-End Class 'Sample
+    End Sub
+End Class
 '
 's1 = MyTest
 's2 = MyTest
