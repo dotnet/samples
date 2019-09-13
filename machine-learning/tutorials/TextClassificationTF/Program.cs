9fb0dadaebeee63168a4d84772ec50bec19afee9
Nat Kershaw <nakersha@microsoft.com>
2019-09-13 14:37:55 -0700

    Fix snippet typo in TensorFlow text classification sample

--- machine-learning/tutorials/TextClassificationTF/Program.cs
+++ machine-learning/tutorials/TextClassificationTF/Program.cs
@@ -174,6 +174,6 @@ namespace TextClassificationTF
             [VectorType(FeatureLength)]
             public int[] Features { get; set; }
         }
-        // </SnippeteFixedLengthFeatures>
+        // </SnippetFixedLengthFeatures>
     }
 }
