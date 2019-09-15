e5acca3b1a31ac80135d29c0c70a1ab31e1229a9
Youssef Victor <youssefvictor00@gmail.com>
2019-09-15 18:52:52 +0200

    More fixes

--- snippets/visualbasic/VS_Snippets_CLR/File OpenRead/VB/file openread.vb
+++ snippets/visualbasic/VS_Snippets_CLR/File OpenRead/VB/file openread.vb
@@ -18,7 +18,7 @@ Public Class Test
 
     ' Open the stream and read it back.
     Using fs As FileStream = File.OpenRead(path)
-      Dim b(1024) As Byte
+      Dim b(1023) As Byte
       Dim temp As UTF8Encoding = New UTF8Encoding(True)
 
       Do While fs.Read(b, 0, b.Length) > 0
