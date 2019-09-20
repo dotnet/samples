1debe4779118fce0e6040ac233165214054510e6
William Anton Rohm <v-wiroh@microsoft.com>
2019-09-20 09:14:02 -0700

    updating for Try .NET (#1522)

--- snippets/csharp/VS_Snippets_CLR_System/system.Random/cs/array1.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.Random/cs/array1.cs
@@ -1,10 +1,10 @@
-﻿// <Snippet10>
-using System;
+﻿using System;
 
 public class Example
 {
    public static void Main()
    {
+      // <Snippet10>
       String[] cities = { "Atlanta", "Boston", "Chicago", "Detroit", 
                           "Fort Wayne", "Greensboro", "Honolulu", "Indianapolis", 
                           "Jersey City", "Kansas City", "Los Angeles", 
@@ -14,8 +14,9 @@ public class Example
       int index = rnd.Next(0, cities.Length);
       Console.WriteLine("Today's city of the day: {0}",
                         cities[index]);                           
+
+      // The example displays output like the following:
+      //   Today's city of the day: Honolulu
+      // </Snippet10>
    }
 }
-// The example displays output like the following:
-//   Today's city of the day: Honolulu
-// </Snippet10>
