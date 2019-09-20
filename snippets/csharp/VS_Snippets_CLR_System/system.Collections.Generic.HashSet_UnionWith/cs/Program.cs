1cec370b65a6e0d4cecb086c8ab05f709badc072
WilliamAntonRohm <v-wiroh@microsoft.com>
2019-09-20 15:26:11 -0700

    updating for Try .NET

--- snippets/csharp/VS_Snippets_CLR_System/system.Collections.Generic.HashSet_UnionWith/cs/Program.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.Collections.Generic.HashSet_UnionWith/cs/Program.cs
@@ -1,5 +1,4 @@
-﻿//<snippet01>
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 class Program
@@ -7,6 +6,7 @@ class Program
     //<snippet02>
     static void Main()
     {
+        //<snippet01>
         //<snippet03>
         HashSet<int> evenNumbers = new HashSet<int>();
         HashSet<int> oddNumbers = new HashSet<int>();
@@ -35,23 +35,23 @@ class Program
         Console.Write("numbers contains {0} elements: ", numbers.Count);
         DisplaySet(numbers);
 
+        void DisplaySet(HashSet<int> set)
+        {
+            Console.Write("{");
+            foreach (int i in set)
+            {
+                Console.Write(" {0}", i);
+            }
+            Console.WriteLine(" }");
+        }
+
+        /* This example produces output similar to the following:
+        * evenNumbers contains 5 elements: { 0 2 4 6 8 }
+        * oddNumbers contains 5 elements: { 1 3 5 7 9 }
+        * numbers UnionWith oddNumbers...
+        * numbers contains 10 elements: { 0 2 4 6 8 1 3 5 7 9 }
+        */
+        //</snippet01>
     }
     //</snippet02>
-
-    private static void DisplaySet(HashSet<int> set)
-    {
-        Console.Write("{");
-        foreach (int i in set)
-        {
-            Console.Write(" {0}", i);
-        }
-        Console.WriteLine(" }");
-    }
 }
-/* This example produces output similar to the following:
- * evenNumbers contains 5 elements: { 0 2 4 6 8 }
- * oddNumbers contains 5 elements: { 1 3 5 7 9 }
- * numbers UnionWith oddNumbers...
- * numbers contains 10 elements: { 0 2 4 6 8 1 3 5 7 9 }
- */
-//</snippet01>
