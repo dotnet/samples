c231f379a0a64c31f755be34abc7f875347f93cd
Andy De George <thraka.junk@outlook.com>
2019-09-20 13:04:20 -0700

    Add more WPF snippets to project

--- snippets/desktop-guide/wpf/data-binding-overview/csharp/App.xaml.cs
+++ snippets/desktop-guide/wpf/data-binding-overview/csharp/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
@@ -13,5 +14,6 @@ namespace bindings
     /// </summary>
     public partial class App : Application
     {
+        public ObservableCollection<AuctionItem> AuctionItems { get; } = new ObservableCollection<AuctionItem>();
     }
 }
