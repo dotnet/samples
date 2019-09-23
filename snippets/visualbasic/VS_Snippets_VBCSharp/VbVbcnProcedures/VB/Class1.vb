9201aed5730b5a2f43f5e813d9da9131d907c532
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-09-23 12:06:28 +0200

    Use PascalCase for method, remove ByVal, modernize code

--- snippets/visualbasic/VS_Snippets_VBCSharp/VbVbcnProcedures/VB/Class1.vb
+++ snippets/visualbasic/VS_Snippets_VBCSharp/VbVbcnProcedures/VB/Class1.vb
@@ -196,12 +196,11 @@ End Class
 
   '****************************************************************************
   '<Snippet51>
-  Function factorial(ByVal n As Integer) As Integer
+  Function Factorial(n As Integer) As Integer
       If n <= 1 Then
           Return 1
-      Else
-          Return factorial(n - 1) * n
       End If
+      Return Factorial(n - 1) * n
   End Function
   '</Snippet51>
 
