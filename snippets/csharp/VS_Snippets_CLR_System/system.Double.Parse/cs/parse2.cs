449b25166419db48502c6834c9fcd0cecef99441
Ron Petrusha <ronpet@microsoft.com>
2019-09-24 16:01:43 -0700

    Added round-tripping to example

--- snippets/csharp/VS_Snippets_CLR_System/system.Double.Parse/cs/parse2.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.Double.Parse/cs/parse2.cs
@@ -12,8 +12,7 @@ public class Example
          Console.WriteLine(Double.Parse(value));
       }   
       catch (OverflowException) {
-         Console.WriteLine("{0} is outside the range of the Double type.",
-                           value);
+         Console.WriteLine($"{value} is outside the range of the Double type.");
       }
       
       value = Double.MaxValue.ToString();
@@ -21,12 +20,23 @@ public class Example
          Console.WriteLine(Double.Parse(value));
       }
       catch (OverflowException) {
-         Console.WriteLine("{0} is outside the range of the Double type.",
-                           value);
+         Console.WriteLine($"{value} is outside the range of the Double type.");
+      }
+
+      // Format without the default precision.
+      value = Double.MinValue.ToString("G17");
+      try 
+      {
+          Console.WriteLine(Double.Parse(value));
+      }
+      catch (OverflowException)
+      {
+         Console.WriteLine($"{value} is outside the range of the Double type.");
       }
    // The example displays the following output:
    //    -1.79769313486232E+308 is outside the range of the Double type.
    //    1.79769313486232E+308 is outside the range of the Double type.
+   //    -1.79769313486232E+308   
    // </Snippet3>   
    }
 }
