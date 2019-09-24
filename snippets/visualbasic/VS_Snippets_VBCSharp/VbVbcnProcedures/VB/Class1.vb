38b5d66889ede5d71328ddf71b972dd893de22db
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-09-24 20:30:26 +0200

    Update snippets/visualbasic/VS_Snippets_VBCSharp/VbVbcnProcedures/VB/Class1.vb
    Co-Authored-By: Ron Petrusha <ronpet@microsoft.com>

--- snippets/visualbasic/VS_Snippets_VBCSharp/VbVbcnProcedures/VB/Class1.vb
+++ snippets/visualbasic/VS_Snippets_VBCSharp/VbVbcnProcedures/VB/Class1.vb
@@ -770,7 +770,7 @@ End Class
 
     '****************************************************************************
     '<Snippet1>
-    Function Hypotenuse(side1 As Single, side2 As Single) As Single
+    Function Hypotenuse(side1 As Double, side2 As Double) As Double
         Return Math.Sqrt((side1 ^ 2) + (side2 ^ 2))
     End Function
     '</Snippet1>
