db4cc4f71605f492c228acd7e9efea28d893c66e
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-09-24 17:13:09 +0200

    Fix code error (#1474)
    * Fix code error
    
    * Improve parallel c# file
    
    * Fix keywords casing
    
    * Update writebase64.vb
    
    * Update writebase64.cs
    
    * Update writebase64.vb

--- snippets/csharp/VS_Snippets_Data/XmlWriter.WriteBase64/CS/writebase64.cs
+++ snippets/csharp/VS_Snippets_Data/XmlWriter.WriteBase64/CS/writebase64.cs
@@ -4,32 +4,35 @@ using System.IO;
 using System.Xml;
 using System.Text;
 
-class TestBase64 {
+public static class TestBase64
+{
 
-    private const int bufferSize=4096;
+    private const int bufferSize = 4096;
 
-    public static void Main(String[] args) {
-
-        TestBase64 testBase64=new TestBase64();
-
-        //Check that the usage string is correct.
-        if (args.Length < 2 ) {
-            testBase64.Usage();
+    public static void Main(string[] args)
+    {
+        // Check that the usage string is correct.
+        if (args.Length < 2)
+        {
+            TestBase64.Usage();
             return;
         }
 
-        FileStream fileOld = new FileStream(args[0], FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read);
-        testBase64.EncodeXmlFile("temp.xml", fileOld);
+        var fileOld = new FileStream(args[0], FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read);
+        TestBase64.EncodeXmlFile("temp.xml", fileOld);
 
-        FileStream fileNew = new FileStream(args[1], FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+        var fileNew = new FileStream(args[1], FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
 
-        testBase64.DecodeOrignalObject("temp.xml", fileNew);
+        TestBase64.DecodeOrignalObject("temp.xml", fileNew);
 
-        //Compare the two files.
-        if (testBase64.CompareResult( fileOld, fileNew)) {
-            Console.WriteLine("The recreated binary file {0} is the same as {1}", args[1], args[0] );
-        } else {
-            Console.WriteLine("The recreated binary file {0} is not the same as {1}", args[1], args[0] );
+        // Compare the two files.
+        if (TestBase64.CompareResult(fileOld, fileNew))
+        {
+            Console.WriteLine($"The recreated binary file {args[1]} is the same as {args[0]}");
+        }
+        else
+        {
+            Console.WriteLine($"The recreated binary file {args[1]} is not the same as {args[0]}");
         }
 
         fileOld.Flush();
@@ -39,32 +42,40 @@ class TestBase64 {
 
     }
 
-    //Use the WriteBase64 method to create an XML document.  The object  
-    //passed by the user is encoded and included in the document.
-    public void EncodeXmlFile(String xmlFileName, FileStream fileOld) {
+    // Use the WriteBase64 method to create an XML document.  The object  
+    // passed by the user is encoded and included in the document.
+    public static void EncodeXmlFile(string xmlFileName, FileStream fileOld)
+    {
 
-        byte[] buffer = new byte[bufferSize];
-        int readByte=0;
+        var buffer = new byte[bufferSize];
+        int readByte = 0;
 
-        XmlTextWriter xw = new XmlTextWriter(xmlFileName, Encoding.UTF8);
+        var xw = new XmlTextWriter(xmlFileName, Encoding.UTF8);
         xw.WriteStartDocument();
         xw.WriteStartElement("root");
         // Create a Char writer.
-        BinaryReader br = new BinaryReader(fileOld);
+        var br = new BinaryReader(fileOld);
         // Set the file pointer to the end.
 
-        try {
-              do {
-                  readByte=br.Read(buffer, 0, bufferSize);
+        try
+        {
+              do
+              {
+                  readByte = br.Read(buffer, 0, bufferSize);
                   xw.WriteBase64(buffer, 0, readByte);
-              } while (bufferSize <= readByte );
+              } while (bufferSize <= readByte);
 
-        } catch (Exception ex) {
-            EndOfStreamException ex1= new EndOfStreamException();
+        }
+        catch (Exception ex)
+        {
+            var ex1 = new EndOfStreamException();
 
-            if (ex1.Equals(ex)) {
+            if (ex1.Equals(ex))
+            {
                 Console.WriteLine("We are at end of file");
-            } else {
+            }
+            else
+            {
                 Console.WriteLine(ex);
             }
         }
@@ -75,69 +86,76 @@ class TestBase64 {
         xw.Close();
     }
 
-    //Use the ReadBase64 method to decode the new XML document 
-    //and generate the original object.
-    public void DecodeOrignalObject(String xmlFileName, FileStream fileNew) {
+    // Use the ReadBase64 method to decode the new XML document 
+    // and generate the original object.
+    public static void DecodeOrignalObject(string xmlFileName, FileStream fileNew)
+    {
 
-        byte[] buffer = new byte[bufferSize];
-        int readByte=0;
+        var buffer = new byte[bufferSize];
+        int readByte = 0;
 
-        //Create a file to write the bmp back.
-        BinaryWriter bw = new BinaryWriter(fileNew);
+        // Create a file to write the bmp back.
+        var bw = new BinaryWriter(fileNew);
 
-        XmlTextReader tr = new XmlTextReader(xmlFileName);
+        var tr = new XmlTextReader(xmlFileName);
         tr.MoveToContent();
         Console.WriteLine(tr.Name);
 
-        do {
+        do
+        {
           readByte=tr.ReadBase64(buffer, 0, bufferSize);
           bw.Write(buffer, 0, readByte);
-        } while(readByte>=bufferSize);
+        } while(readByte >= bufferSize);
 
         bw.Flush();
 
     }
 
-    //Compare the two files.
-    public bool CompareResult(FileStream fileOld, FileStream fileNew) {
+    // Compare the two files.
+    public static bool CompareResult(FileStream fileOld, FileStream fileNew) {
 
-        int readByteOld=0;
-        int readByteNew=0;
-        int count, readByte=0;
+        int readByteOld = 0;
+        int readByteNew = 0;
+        int count, readByte =0 ;
 
-        byte[] bufferOld = new byte[bufferSize];
-        byte[] bufferNew = new byte[bufferSize];
+        var bufferOld = new byte[bufferSize];
+        var bufferNew = new byte[bufferSize];
 
 
-        BinaryReader binaryReaderOld = new BinaryReader(fileOld);
-        BinaryReader binaryReaderNew = new BinaryReader(fileNew);
+        var binaryReaderOld = new BinaryReader(fileOld);
+        var binaryReaderNew = new BinaryReader(fileNew);
 
         binaryReaderOld.BaseStream.Seek(0, SeekOrigin.Begin);
         binaryReaderNew.BaseStream.Seek(0, SeekOrigin.Begin);
 
 
-        do {
-          readByteOld=binaryReaderOld.Read(bufferOld, 0, bufferSize);
-          readByteNew=binaryReaderNew.Read(bufferNew, 0, bufferSize);
+        do
+        {
+          readByteOld = binaryReaderOld.Read(bufferOld, 0, bufferSize);
+          readByteNew = binaryReaderNew.Read(bufferNew, 0, bufferSize);
 
-          if (readByteOld!=readByteNew) {
+          if (readByteOld != readByteNew)
+          {
               return false;
           }
 
-          for (count=0; count <bufferSize; ++count) {
-              if (bufferOld[count]!=bufferNew[count]) {
+          for (count = 0; count < bufferSize; count++)
+          {
+              if (bufferOld[count] != bufferNew[count])
+              {
                   return false;
               }
           }
 
-        } while (count<readByte );
+        } while (count < readByte);
 
         return true;
 
     }
 
-    //Display the usage statement.
-    public void Usage() {
+    // Display the usage statement.
+    public static void Usage()
+    {
         Console.WriteLine("TestBase64 sourceFile, targetFile \n");
         Console.WriteLine("For example: TestBase64 winlogon.bmp, target.bmp\n");
     }
