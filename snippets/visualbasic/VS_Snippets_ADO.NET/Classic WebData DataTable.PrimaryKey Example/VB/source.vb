8d3e85441672046fe450defe8f6c251fbaed78e0
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-09-26 23:52:30 +0200

    Update source.vb

--- snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData DataTable.PrimaryKey Example/VB/source.vb
+++ snippets/visualbasic/VS_Snippets_ADO.NET/Classic WebData DataTable.PrimaryKey Example/VB/source.vb
@@ -30,7 +30,7 @@ Protected DataGrid1 As DataGrid
 
      ' Create column 1.
      column = New DataColumn()
-     column.DataType = System.Type.GetType("System.String")
+     column.DataType = Type.GetType("System.String")
      column.ColumnName= "FirstName"
 
      ' Add the column to the DataTable.Columns collection.
@@ -40,7 +40,7 @@ Protected DataGrid1 As DataGrid
  
      ' Create column 2 and add it to the array.
      column = New DataColumn()
-     column.DataType = System.Type.GetType("System.String")
+     column.DataType = Type.GetType("System.String")
      column.ColumnName = "LastName"
      table.Columns.Add(column)
 
