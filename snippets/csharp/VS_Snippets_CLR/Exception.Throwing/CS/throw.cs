50378396a747762efda8252992a90ac9f84573c8
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-09-27 09:52:31 +0200

    Update throw.cs

--- snippets/csharp/VS_Snippets_CLR/Exception.Throwing/CS/throw.cs
+++ snippets/csharp/VS_Snippets_CLR/Exception.Throwing/CS/throw.cs
@@ -4,30 +4,29 @@ using System.IO;
 
 public class ProcessFile
 {
-   public static void Main()
-      {
-      FileStream fs = null;
-      try   
-      {
-         //Opens a text tile.
-         fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
-         StreamReader sr = new StreamReader(fs);
-         string line;
+    public static void Main()
+    {
+        FileStream fs;
+        try   
+        {
+            // Opens a text tile.
+            fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+            var sr = new StreamReader(fs);
          
-         //A value is read from the file and output to the console.
-         line = sr.ReadLine();
-         Console.WriteLine(line);
-      }
-      catch(FileNotFoundException e)
-      {
-         Console.WriteLine("[Data File Missing] {0}", e);
-         throw new FileNotFoundException(@"[data.txt not in c:\temp directory]",e);
-      }
-      finally
-      {
-         if (fs != null) 
-            fs.Close();
-      }
-   }
+            // A value is read from the file and output to the console.
+            string line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+        catch(FileNotFoundException e)
+        {
+            Console.WriteLine($"[Data File Missing] {e}");
+            throw new FileNotFoundException(@"[data.txt not in c:\temp directory]", e);
+        }
+        finally
+        {
+            if (fs != null) 
+                fs.Close();
+        }
+    }
 }
 // </Snippet1>
