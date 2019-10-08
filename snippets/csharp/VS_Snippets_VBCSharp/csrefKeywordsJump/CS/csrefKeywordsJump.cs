293a3566207971ee388f12b65d2c9a17a232ac9f
David Fox <me@iamdavidfox.com>
2019-10-07 23:35:01 -0400

    New snippet to demonstrate loop->switch->break

--- snippets/csharp/VS_Snippets_VBCSharp/csrefKeywordsJump/CS/csrefKeywordsJump.cs
+++ snippets/csharp/VS_Snippets_VBCSharp/csrefKeywordsJump/CS/csrefKeywordsJump.cs
@@ -284,4 +284,43 @@ namespace csrefKeywordsJump
          a  b  c  d  e  f  g  h  i
      */
     //</Snippet7>  
+
+    //<snippet8>
+    class BreakFromSwitchInsideLoop
+    {
+        static void Main(string[] args)
+        {
+            // loop 1 to 3
+            for (int i = 1; i <= 3; i++)
+            {
+                switch(i)
+                {
+                    case 1:
+                        Console.WriteLine("Current value is 1");
+                        break;
+                    case 2:
+                        Console.WriteLine("Current value is 2");
+                        break;
+                    case 3:
+                        Console.WriteLine("Current value is 3");
+                        break;
+                    default:
+                        Console.WriteLine("This shouldn't happen.");
+                        break;
+                }
+            }
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    /*
+     * Output:
+        Current value is 1
+        Current value is 2
+        Current value is 3
+     */
+    //</snippet8>
 }
