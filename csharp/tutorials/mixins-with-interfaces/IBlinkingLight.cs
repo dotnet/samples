276c7a7f689f777ec7e4dea55da8df32f5630bc5
Bill Wagner <wiwagn@microsoft.com>
2019-10-09 14:25:42 -0400

    Virtual interface methods samples (#1615)
    * sample almost done
    
    Need to add more test code, and the pattern matching usage.
    
    * final test updates.
    
    * rename sample to match article

---
+++ csharp/tutorials/mixins-with-interfaces/IBlinkingLight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mixins_with_interfaces
+{
+    // <SnippetBlinkingLight>
+    public interface IBlinkingLight : ILight
+    {
+        public virtual async Task Blink(int duration, int repeatCount)
+        {
+            Console.WriteLine("using default interface method for IBlinkingLight.Blink");
+            for (int count = 0; count < repeatCount; count++)
+            {
+                SwitchOn();
+                await Task.Delay(duration);
+                SwitchOff();
+                await Task.Delay(duration);
+            }
+            Console.WriteLine("Done with default interface method for IBlinkingLight.Blink");
+        }
+    }
+    // </SnippetBlinkingLight>
+}
---
+++ csharp/tutorials/mixins-with-interfaces/ILight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mixins_with_interfaces
+{
+    // <SnippetPowerStatus>
+    public enum PowerStatus
+    {
+        NoPower,
+        ACPower,
+        FullBattery,
+        MidBattery,
+        LowBattery
+    }
+    // </SnippetPowerStatus>
+
+
+    // <SnippetILightInterface>
+    public interface ILight
+    {
+        void SwitchOn();
+        void SwitchOff();
+        bool IsOn();
+        public virtual PowerStatus Power() => PowerStatus.NoPower;
+    }
+    // </SnippetILightInterface>
+}
