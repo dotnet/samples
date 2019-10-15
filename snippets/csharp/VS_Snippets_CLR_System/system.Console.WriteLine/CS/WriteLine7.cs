6326463ecc14c7bfb6c50a9420b0296b2afdd555
William Anton Rohm <v-wiroh@microsoft.com>
2019-10-15 10:58:03 -0700

    Reverting Try .NET change

--- snippets/csharp/VS_Snippets_CLR_System/system.Console.WriteLine/CS/WriteLine7.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.Console.WriteLine/CS/WriteLine7.cs
@@ -1,14 +1,14 @@
-﻿using System;
+// <Snippet7>
+using System;
 
 public class Example
 {
    public static void Main()
    {
-      // <Snippet7>
       Console.WriteLine("Today's date: {0:D}", DateTime.Now);
-
-      // The example displays output like the following:
-      //       Today's date: Friday, April 1, 2019
-      // </Snippet7>
    }
 }
+// The example displays output like the following:
+//       Today's date: Friday, April 1, 2016
+// </Snippet7>
+
