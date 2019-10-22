393e1fe33d52591c21cfee841fd6fc063460e0b7
William Anton Rohm <v-wiroh@microsoft.com>
2019-10-22 09:41:16 -0700

    Update snippets/csharp/VS_Snippets_CLR_System/system.guid.ctor/cs/ctor2.cs
    Co-Authored-By: Maira Wenzel <mairaw@microsoft.com>

--- snippets/csharp/VS_Snippets_CLR_System/system.guid.ctor/cs/ctor2.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.guid.ctor/cs/ctor2.cs
@@ -7,7 +7,7 @@ public class Example
       // <Snippet2>
       var g = new Guid(0xA, 0xB, 0xC, 
                         new Byte[] { 0, 1, 2, 3, 4, 5, 6, 7 } );
-      Console.WriteLine("{0:B}", g);
+      Console.WriteLine($"{g:B}");
 
       // The example displays the following output:
       //        {0000000a-000b-000c-0001-020304050607}
