443b6701cd371f94b10e29874f5ab7a6e2f9244f
Tony Chan <tony_wc_chan@hotmail.com>
2019-10-23 08:29:41 -0700

    Update enumerable.cs to fix typo in Zip()

--- snippets/csharp/VS_Snippets_CLR_System/system.Linq.Enumerable/CS/enumerable.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.Linq.Enumerable/CS/enumerable.cs
@@ -3075,7 +3075,7 @@ namespace SequenceExamples
             int[] numbers = { 1, 2, 3, 4 };
             string[] words = { "one", "two", "three" };
 
-            var numbersAndWords = numbers.Zip(words, (first, second) => first + " " + second);
+            var numbersAndWords = numbers.Zip(numbers, words, (first, second) => first + " " + second);
 
             foreach (var item in numbersAndWords)
                 Console.WriteLine(item);
