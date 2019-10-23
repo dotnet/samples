c12767c1f954b889c246c7dfc8acfb4e880c1734
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-10-23 22:00:55 +0300

    Update guid samples (#1663)
    * Update guid samples
    
    * Update ctor2.cs
    
    * Update empty.cs
    
    * Update ng.cs
    
    * Update parseex1.cs
    
    * Update parseexactex1.cs
    
    * Update tryparseex1.cs
    
    * Update tobytearray3.cs
    
    * Update tryparseexactex1.cs

--- snippets/csharp/VS_Snippets_CLR_System/system.guid.ctor/cs/ctor2.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.guid.ctor/cs/ctor2.cs
@@ -2,15 +2,15 @@
 
 public class Example
 {
-   public static void Main()
-   {
-      // <Snippet2>
-      var g = new Guid(0xA, 0xB, 0xC, 
-                        new Byte[] { 0, 1, 2, 3, 4, 5, 6, 7 } );
-      Console.WriteLine($"{g:B}");
+    public static void Main()
+    {
+        // <Snippet2>
+        var g = new Guid(0xA, 0xB, 0xC, 
+                          new Byte[] { 0, 1, 2, 3, 4, 5, 6, 7 } );
+        Console.WriteLine($"{g:B}");
 
-      // The example displays the following output:
-      //        {0000000a-000b-000c-0001-020304050607}
-      // </Snippet2>
-   }
+        // The example displays the following output:
+        //        {0000000a-000b-000c-0001-020304050607}
+        // </Snippet2>
+    }
 }
