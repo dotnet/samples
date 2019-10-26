672083397211717d93a83972baa37bced5d2aa65
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-10-26 20:15:07 +0200

    Update WinSound.cs

--- snippets/csharp/VS_Snippets_VBCSharp/csProgGuideInterop/CS/WinSound.cs
+++ snippets/csharp/VS_Snippets_VBCSharp/csProgGuideInterop/CS/WinSound.cs
@@ -10,7 +10,7 @@ namespace WinSound
         private TextBox textBox1;
         private Button button1;
 
-        public Form1()  //constructor
+        public Form1()  // Constructor.
         {
             InitializeComponent();
         }
@@ -22,18 +22,18 @@ namespace WinSound
         public enum PlaySoundFlags : int
         {
             SND_SYNC = 0x0000,
-            SND_ASYNC = 0x0001, 
-            SND_NODEFAULT = 0x0002, 
-            SND_LOOP = 0x0008, 
+            SND_ASYNC = 0x0001,
+            SND_NODEFAULT = 0x0002,
+            SND_LOOP = 0x0008,
             SND_NOSTOP = 0x0010,
-            SND_NOWAIT = 0x00002000, 
-            SND_FILENAME = 0x00020000, 
-            SND_RESOURCE = 0x00040004 
+            SND_NOWAIT = 0x00002000,
+            SND_FILENAME = 0x00020000,
+            SND_RESOURCE = 0x00040004
         }
 
-        private void button1_Click (object sender, System.EventArgs e)
+        private void button1_Click(object sender, System.EventArgs e)
         {
-            OpenFileDialog dialog1 = new OpenFileDialog(); 
+            var dialog1 = new OpenFileDialog(); 
 
             dialog1.Title = "Browse to find sound file to play";
             dialog1.InitialDirectory = @"c:\";
@@ -43,11 +43,11 @@ namespace WinSound
             dialog1.FilterIndex = 2;
             dialog1.RestoreDirectory = true;
 
-            if(dialog1.ShowDialog() == DialogResult.OK) 
+            if (dialog1.ShowDialog() == DialogResult.OK) 
             { 
                 textBox1.Text = dialog1.FileName;
-                PlaySound (dialog1.FileName, new System.IntPtr(), PlaySoundFlags.SND_SYNC);
-            } 
+                PlaySound(dialog1.FileName, new System.IntPtr(), PlaySoundFlags.SND_SYNC);
+            }
         }
     }
 }
@@ -55,7 +55,7 @@ namespace WinSound
 
 
 //-----------------------------------------------------------------------------
-// Form1.Desgner.cs
+// Form1.Designer.cs
 //-----------------------------------------------------------------------------
 namespace WinSound
 {
