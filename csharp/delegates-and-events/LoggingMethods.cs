ce47b70067659166ce369d8a2c2e7f8fdf78c545
Sean Killeen <SeanKilleen@gmail.com>
2019-10-31 20:14:59 -0400

    Extract method into LoggingMethods static class

--- csharp/delegates-and-events/LoggingMethods.cs
+++ csharp/delegates-and-events/LoggingMethods.cs
@@ -1,1 +1,13 @@
 
+using System;
+
+namespace DelegatesAndEvents
+{
+    public static class LoggingMethods
+    {
+        public static void LogToConsole(string message)
+        {
+            Console.Error.WriteLine(message);
+        }
+    }
+}
