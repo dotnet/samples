c05eecb13eeec51eadc99ee32224dc9ecba03b63
iolazarc <47868039+iolazarc@users.noreply.github.com>
2019-11-01 13:00:40 -0700

    Fix socket leak in code sample for System.Net.Sockets.Socket.Close (#1685)

--- snippets/csharp/VS_Snippets_Remoting/Classic Socket.Close Example/CS/source.cs
+++ snippets/csharp/VS_Snippets_Remoting/Classic Socket.Close Example/CS/source.cs
@@ -8,9 +8,15 @@ public class Sample
     public static void SocketClose()
     {
 	    Socket aSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-// <Snippet1>
-        aSocket.Shutdown(SocketShutdown.Both);
-        aSocket.Close();
+        // <Snippet1>
+        try
+        {
+            aSocket.Shutdown(SocketShutdown.Both);
+        }
+        finally
+        {
+            aSocket.Close();
+        }
 // </Snippet1>
     }
     
--- snippets/visualbasic/VS_Snippets_Remoting/Classic Socket.Close Example/VB/source.vb
+++ snippets/visualbasic/VS_Snippets_Remoting/Classic Socket.Close Example/VB/source.vb
@@ -6,18 +6,21 @@ Imports System.Net.Sockets
 
 
 Public Class Sample
-    
-    Public Shared Sub SocketClose() 
+
+    Public Shared Sub SocketClose()
         Dim aSocket As New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
         ' <Snippet1>
-        aSocket.Shutdown(SocketShutdown.Both)
-        aSocket.Close()
-    
+        Try
+            aSocket.Shutdown(SocketShutdown.Both)
+        Finally
+            aSocket.Close()
+        End Try
+
     End Sub
-     ' </Snippet1>
-    
-    Public Shared Sub Main() 
+    ' </Snippet1>
+
+    Public Shared Sub Main()
         SocketClose()
-    
+
     End Sub
 End Class
