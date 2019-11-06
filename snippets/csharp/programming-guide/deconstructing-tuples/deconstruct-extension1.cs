dd22698b543c7e50946340c64adc05f07b074d4b
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-11-06 20:12:17 +0200

    Update deconstruct-extension1.cs

--- snippets/csharp/programming-guide/deconstructing-tuples/deconstruct-extension1.cs
+++ snippets/csharp/programming-guide/deconstructing-tuples/deconstruct-extension1.cs
@@ -4,112 +4,118 @@ using System.Reflection;
 
 public static class ReflectionExtensions
 {
-   public static void Deconstruct(this PropertyInfo p, out bool isStatic, 
-                                  out bool isReadOnly, out bool isIndexed,
-                                  out Type propertyType)
-   {
-      var getter = p.GetMethod;
-      
-      // Is the property read-only?
-      isReadOnly = ! p.CanWrite;
+    public static void Deconstruct(this PropertyInfo p, out bool isStatic,
+                                   out bool isReadOnly, out bool isIndexed,
+                                   out Type propertyType)
+    {
+        var getter = p.GetMethod;
 
-      // Is the property instance or static?
-      isStatic = getter.IsStatic;
-      
-      // Is the property indexed?
-      isIndexed = p.GetIndexParameters().Length > 0;
-      
-      // Get the property type.
-      propertyType = p.PropertyType;
-   }
+        // Is the property read-only?
+        isReadOnly = ! p.CanWrite;
 
-   public static void Deconstruct(this PropertyInfo p, out bool hasGetAndSet, 
-                                  out bool sameAccess, out string access,
-                                  out string getAccess, out string setAccess) 
-   {                                  
-      hasGetAndSet = sameAccess = false;
-      string getAccessTemp = null;
-      string setAccessTemp = null;
+        // Is the property instance or static?
+        isStatic = getter.IsStatic;
 
-      MethodInfo getter = null; 
-      if (p.CanRead) 
-         getter = p.GetMethod;
+        // Is the property indexed?
+        isIndexed = p.GetIndexParameters().Length > 0;
 
-      MethodInfo setter = null;
-      if (p.CanWrite) 
-          setter = p.SetMethod;
-   
-      if (setter != null && getter != null) 
-         hasGetAndSet = true;
+        // Get the property type.
+        propertyType = p.PropertyType;
+    }
 
-      if (getter != null) {
-         if (getter.IsPublic)
-            getAccessTemp = "public";
-         else if (getter.IsPrivate)
-            getAccessTemp = "private"; 
-         else if (getter.IsAssembly)
-            getAccessTemp = "internal";
-         else if (getter.IsFamily)
-            getAccessTemp = "protected";      
-         else if (getter.IsFamilyOrAssembly) 
-            getAccessTemp = "protected internal";
-      }
+    public static void Deconstruct(this PropertyInfo p, out bool hasGetAndSet,
+                                   out bool sameAccess, out string access,
+                                   out string getAccess, out string setAccess)
+    {
+        hasGetAndSet = sameAccess = false;
+        string getAccessTemp = null;
+        string setAccessTemp = null;
 
-      if (setter != null) {
-         if (setter.IsPublic)
-            setAccessTemp = "public";
-         else if (setter.IsPrivate)
-            setAccessTemp = "private"; 
-         else if (setter.IsAssembly)
-            setAccessTemp = "internal";
-         else if (setter.IsFamily)
-            setAccessTemp = "protected";      
-         else if (setter.IsFamilyOrAssembly) 
-            setAccessTemp = "protected internal";
-      }
+        MethodInfo getter = null;
+        if (p.CanRead) 
+            getter = p.GetMethod;
 
-      // Are the accessibility of the getter and setter the same?
-      if (setAccessTemp == getAccessTemp) {
-         sameAccess = true;
-         access = getAccessTemp;
-         getAccess = setAccess = String.Empty;
-      }   
-      else {
-         access = null;
-         getAccess = getAccessTemp;
-         setAccess = setAccessTemp;   
-      }
-   }
+        MethodInfo setter = null;
+        if (p.CanWrite) 
+            setter = p.SetMethod;
+
+        if (setter != null && getter != null)
+            hasGetAndSet = true;
+
+        if (getter != null)
+        {
+            if (getter.IsPublic)
+                getAccessTemp = "public";
+            else if (getter.IsPrivate)
+                getAccessTemp = "private";
+            else if (getter.IsAssembly)
+                getAccessTemp = "internal";
+            else if (getter.IsFamily)
+                getAccessTemp = "protected";
+            else if (getter.IsFamilyOrAssembly) 
+                getAccessTemp = "protected internal";
+        }
+
+        if (setter != null)
+        {
+            if (setter.IsPublic)
+                setAccessTemp = "public";
+            else if (setter.IsPrivate)
+                setAccessTemp = "private";
+            else if (setter.IsAssembly)
+                setAccessTemp = "internal";
+            else if (setter.IsFamily)
+                setAccessTemp = "protected";
+            else if (setter.IsFamilyOrAssembly)
+                setAccessTemp = "protected internal";
+        }
+
+        // Are the accessibility of the getter and setter the same?
+        if (setAccessTemp == getAccessTemp)
+        {
+            sameAccess = true;
+            access = getAccessTemp;
+            getAccess = setAccess = String.Empty;
+        }
+        else
+        {
+            access = null;
+            getAccess = getAccessTemp;
+            setAccess = setAccessTemp;
+        }
+    }
 }
 
 public class Example
 {
-   public static void Main()
-   {
-      Type dateType = typeof(DateTime);
-      PropertyInfo prop = dateType.GetProperty("Now");
-      var (isStatic, isRO, isIndexed, propType) = prop;
-      Console.WriteLine($"\nThe {dateType.FullName}.{prop.Name} property:");
-      Console.WriteLine($"   PropertyType: {propType.Name}");
-      Console.WriteLine($"   Static:       {isStatic}");
-      Console.WriteLine($"   Read-only:    {isRO}");
-      Console.WriteLine($"   Indexed:      {isIndexed}");
+    public static void Main()
+    {
+        Type dateType = typeof(DateTime);
+        PropertyInfo prop = dateType.GetProperty("Now");
+        var (isStatic, isRO, isIndexed, propType) = prop;
+        Console.WriteLine($"\nThe {dateType.FullName}.{prop.Name} property:");
+        Console.WriteLine($"   PropertyType: {propType.Name}");
+        Console.WriteLine($"   Static:       {isStatic}");
+        Console.WriteLine($"   Read-only:    {isRO}");
+        Console.WriteLine($"   Indexed:      {isIndexed}");
 
-      Type listType = typeof(List<>);
-      prop = listType.GetProperty("Item", 
-                                  BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
-      var (hasGetAndSet, sameAccess, accessibility, getAccessibility, setAccessibility) = prop;
-      Console.Write($"\nAccessibility of the {listType.FullName}.{prop.Name} property: ");
+        Type listType = typeof(List<>);
+        prop = listType.GetProperty("Item", 
+                                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+        var (hasGetAndSet, sameAccess, accessibility, getAccessibility, setAccessibility) = prop;
+        Console.Write($"\nAccessibility of the {listType.FullName}.{prop.Name} property: ");
 
-      if (!hasGetAndSet | sameAccess) {
-          Console.WriteLine(accessibility);
-      }
-      else {
-         Console.WriteLine($"\n   The get accessor: {getAccessibility}");
-         Console.WriteLine($"   The set accessor: {setAccessibility}");
-      }
-   }
-}      
+        if (!hasGetAndSet | sameAccess)
+        {
+            Console.WriteLine(accessibility);
+        }
+        else
+        {
+            Console.WriteLine($"\n   The get accessor: {getAccessibility}");
+            Console.WriteLine($"   The set accessor: {setAccessibility}");
+        }
+    }
+}
 // The example displays the following output:
 //       The System.DateTime.Now property:
 //          PropertyType: DateTime
