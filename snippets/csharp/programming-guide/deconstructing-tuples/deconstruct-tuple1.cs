8566e7e4ede23d36d611b26404744d3a93eb42e5
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2019-11-13 08:57:58 +0200

    Use allman braces and 4 spaces for indentation (#1723)
    * Use allman braces and 4 spaces for indentation
    
    * Update class-discard1.cs
    
    * Update deconstruct-ambiguous.cs
    
    * Update deconstruct-class1.cs
    
    * Update deconstruct-class1.cs
    
    * Update deconstruct-class2.cs
    
    * Update deconstruct-extension1.cs
    
    * Update deconstruct-tuple1.cs
    
    * Update deconstruct-tuple3.cs
    
    * Update deconstruct-tuple2.cs
    
    * Update deconstruct-tuple4.cs

--- snippets/csharp/programming-guide/deconstructing-tuples/deconstruct-tuple1.cs
+++ snippets/csharp/programming-guide/deconstructing-tuples/deconstruct-tuple1.cs
@@ -2,22 +2,22 @@
 
 public class Example
 {
-   public static void Main()
-   {
-       var result = QueryCityData("New York City");
-       
-       var city = result.Item1;
-       var pop = result.Item2;
-       var size = result.Item3;
-       
-       // Do something with the data.
-   }
+    public static void Main()
+    {
+        var result = QueryCityData("New York City");
 
-   private static (string, int, double) QueryCityData(string name)
-   {
-      if (name == "New York City")
-         return (name, 8175133, 468.48);
+        var city = result.Item1;
+        var pop = result.Item2;
+        var size = result.Item3;
 
-      return ("", 0, 0);
-   }
+         // Do something with the data.
+    }
+
+    private static (string, int, double) QueryCityData(string name)
+    {
+        if (name == "New York City")
+            return (name, 8175133, 468.48);
+
+        return ("", 0, 0);
+    }
 }
