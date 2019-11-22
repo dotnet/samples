4bbdf49fbd653168c40acc6978ee7bbb971cf04f
Maira Wenzel <mairaw@microsoft.com>
2019-11-22 01:38:23 -0800

    fix typo

--- snippets/csharp/VS_Snippets_CLR_System/system.Collections.Generic.HashSet_RemoveWhere/cs/Program.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.Collections.Generic.HashSet_RemoveWhere/cs/Program.cs
@@ -43,7 +43,7 @@ class Example
             Console.WriteLine(" }");
         }
 
-        // This example display the following output:
+        // This example displays the following output:
         //    numbers contains 20 elements: { 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 }
         //    numbers contains 10 elements: { 0 2 4 6 8 10 12 14 16 18 }
         //    numbers contains 9 elements: { 2 4 6 8 10 12 14 16 18 }
