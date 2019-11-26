7579589defa31fb9fa5e8bb86234a4c26f550539
Jeff Pierson <jeffrey.pierson@gmail.com>
2019-11-26 02:13:54 -0500

    Add Cyrillic alphabet / Unicode Console snippet
    Adds missing snippet demonstrating in F# how Console
    depends on code page in order to render characters for
    some Unicode characters.

---
+++ snippets/fsharp/VS_Snippets_CLR_System/system.console.class/fs/unicode1.fs
@@ -0,0 +1,26 @@
+﻿// <Snippet1>
+open System
+
+[<EntryPoint>]
+let main argv =
+    // Create a char List for the modern Cyrillic alphabet, 
+    // from U+0410 to U+044F.
+    let chars = [for codePoint in 0x0410..0x044F do Convert.ToChar(codePoint)]
+    
+    Console.WriteLine("Current code page: {0}\n", Console.OutputEncoding.CodePage)
+    // Display the characters.
+    for ch in chars do
+        Console.Write("{0}  ", ch)
+        if (Console.CursorLeft) >= 70
+            then Console.WriteLine()
+            else ()
+    
+    0
+
+// The example displays the following output:
+//    Current code page: 437
+//    
+//    ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?
+//    ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?
+//    ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?  ?
+// </Snippet1>
