6162e55395e61e81f2c4c131a3d0fedf72ba0d2d
Jeff Pierson <jeffrey.pierson@gmail.com>
2019-12-06 01:15:34 -0500

    Add grapheme1 snippet in F#

---
+++ snippets/fsharp/VS_Snippets_CLR_System/system.console.class/fs/grapheme1.fs
@@ -0,0 +1,16 @@
+﻿// <Snippet1>
+open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    use sw = new StreamWriter("chars1.txt")
+    let chars = [| '\u0061'; '\u0308' |]
+    let strng = String(chars)
+    sw.WriteLine(strng)
+    sw.Close()
+    0
+// The example produces the following output:
+//       ä
+// </Snippet1>
+
