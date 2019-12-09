590beba17661fc7876b01c94593bb7de5b8b97a5
pkulikov <petr.kulikov@gmail.com>
2019-12-09 06:47:55 +0100

    Closed the snippet tag

--- csharp/language-reference/builtin-types/EnumType.cs
+++ csharp/language-reference/builtin-types/EnumType.cs
@@ -50,7 +50,7 @@ namespace builtin_types
                 // Monday, Wednesday, Saturday
             }
         }
-        // <SnippetFlags>
+        // </SnippetFlags>
 
         // <SnippetConversions>
         public enum Season
