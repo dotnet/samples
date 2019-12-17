5fb94a309fc1352ada93ebaa9956a29430e6cb86
Kerry W. Lothrop <kerry@lothrop.de>
2019-12-17 13:17:59 +0100

    Fixed spacing in method call.

--- snippets/csharp/VS_Snippets_Data/XmlReader.Create/CS/XmlReader_Create.cs
+++ snippets/csharp/VS_Snippets_Data/XmlReader.Create/CS/XmlReader_Create.cs
@@ -47,7 +47,7 @@ XmlReader reader = XmlReader.Create(new StringReader(xmlFrag), settings, context
 // Create an XmlUrlResolver with the credentials necessary to access the Web server.
 XmlUrlResolver resolver = new XmlUrlResolver();
 System.Net.NetworkCredential myCred;
-myCred  = new System.Net.NetworkCredential(UserName,SecurelyStoredPassword,Domain);  
+myCred = new System.Net.NetworkCredential(UserName, SecurelyStoredPassword, Domain);  
 resolver.Credentials = myCred;
 
 XmlReaderSettings settings = new XmlReaderSettings();
