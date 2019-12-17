b34391ba9aeb1fb6ff5c5cdf083d56b96cf4362b
Kerry W. Lothrop <kerry@lothrop.de>
2019-12-17 22:53:15 +0100

    Addressed review comments

--- snippets/csharp/VS_Snippets_Data/XmlReader.Create/CS/XmlReader_Create.cs
+++ snippets/csharp/VS_Snippets_Data/XmlReader.Create/CS/XmlReader_Create.cs
@@ -45,12 +45,11 @@ XmlReader reader = XmlReader.Create(new StringReader(xmlFrag), settings, context
 
         //<snippet2>
 // Create an XmlUrlResolver with the credentials necessary to access the Web server.
-XmlUrlResolver resolver = new XmlUrlResolver();
-System.Net.NetworkCredential myCred;
-myCred = new System.Net.NetworkCredential(UserName, SecurelyStoredPassword, Domain);  
+var resolver = new XmlUrlResolver();
+var myCred = new NetworkCredential(UserName, SecurelyStoredPassword, Domain);  
 resolver.Credentials = myCred;
 
-XmlReaderSettings settings = new XmlReaderSettings();
+var settings = new XmlReaderSettings();
 settings.XmlResolver = resolver;
 
 // Create the reader.
