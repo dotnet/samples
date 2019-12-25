5fdc1abf4cfcc11915ceb64f92cd60244db4583d
Tom Dykstra <tdykstra@microsoft.com>
2019-12-24 16:20:08 -0800

    misc cleanup

--- snippets/core/system-text-json/csharp/ConvertPolymorphic.cs
+++ snippets/core/system-text-json/csharp/ConvertPolymorphic.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 
 namespace SystemTextJsonSamples
 {
-    class SerializePolymorphic
+    class ConvertPolymorphic
     {
         public static void Run()
         {
--- snippets/core/system-text-json/csharp/RoundtripImmutableStruct.cs
+++ snippets/core/system-text-json/csharp/RoundtripImmutableStruct.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Xml.Schema;
 
 namespace SystemTextJsonSamples
 {
