63f9fa67d850a5fadb0d277c7a2f6ef80cdaac4b
Tom Dykstra <tdykstra@microsoft.com>
2019-12-26 15:54:04 -0800

    correct utf8 file code

--- snippets/core/system-text-json/csharp/RoundtripToFileAsync.cs
+++ snippets/core/system-text-json/csharp/RoundtripToFileAsync.cs
@@ -11,7 +11,6 @@ namespace SystemTextJsonSamples
         public static async Task RunAsync()
         {
             string fileName = "WeatherForecastAsync.json";
-            string fileNameUtf8 = "WeatherForecastAsyncUtf8";
             WeatherForecast weatherForecast = WeatherForecastFactories.CreateWeatherForecast();
             weatherForecast.DisplayPropertyValues();
 
@@ -29,22 +28,6 @@ namespace SystemTextJsonSamples
                 weatherForecast = await JsonSerializer.DeserializeAsync<WeatherForecast>(fs);
             }
             // </SnippetDeserialize>
-            weatherForecast.DisplayPropertyValues();
-
-            using (FileStream fs = File.Create(fileNameUtf8))
-            {
-                using (StreamWriter writer = new StreamWriter(fs, Encoding.UTF8))
-                {
-                    await JsonSerializer.SerializeAsync(fs, weatherForecast);
-                }
-            }
-            Console.WriteLine($"The result is in {fileNameUtf8}\n");
-            using (FileStream fs = File.OpenRead(fileNameUtf8))
-            {
-                //System.Text.Json.JsonException: '0xEF' is invalid after a single JSON value. Expected end of data.
-                //weatherForecast = await JsonSerializer.DeserializeAsync<WeatherForecast>(fs);
-            }
-
             weatherForecast.DisplayPropertyValues();
         }
     }
