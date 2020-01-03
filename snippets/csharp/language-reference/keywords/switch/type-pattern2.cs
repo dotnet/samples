07e84a6d1be24200102497ee38c9de31857c17ab
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-01-03 12:08:46 +0200

    braces

--- snippets/csharp/language-reference/keywords/switch/type-pattern2.cs
+++ snippets/csharp/language-reference/keywords/switch/type-pattern2.cs
@@ -10,42 +10,48 @@ class Example
     {
         int[] values = { 2, 4, 6, 8, 10 };
         ShowCollectionInformation(values);
-        
+
         var names = new List<string>();
-        names.AddRange( new string[] { "Adam", "Abigail", "Bertrand", "Bridgette" } );
+        names.AddRange(new string[] { "Adam", "Abigail", "Bertrand", "Bridgette" });
         ShowCollectionInformation(names);
-        
+
         List<int> numbers = null;
         ShowCollectionInformation(numbers);
     }
-   
+
     private static void ShowCollectionInformation(object coll)
     {
-        if (coll is Array) {
+        if (coll is Array)
+        {
            Array arr = (Array) coll;
            Console.WriteLine($"An array with {arr.Length} elements.");
         }
-        else if (coll is IEnumerable<int>) {
+        else if (coll is IEnumerable<int>)
+        {
             IEnumerable<int> ieInt = (IEnumerable<int>) coll;
             Console.WriteLine($"Average: {ieInt.Average(s => s)}");
         }
-        else if (coll is IList) {
+        else if (coll is IList)
+        {
             IList list = (IList) coll;
             Console.WriteLine($"{list.Count} items");
         }
-        else if (coll is IEnumerable) { 
+        else if (coll is IEnumerable)
+        {
             IEnumerable ie = (IEnumerable) coll;
             string result = "";
             foreach (var e in ie) 
                result += $"{e} ";
             Console.WriteLine(result);
         }
-        else if (coll == null) { 
+        else if (coll == null)
+        {
             // Do nothing. 
         }
-        else {
+        else
+        {
             Console.WriteLine($"An instance of type {coll.GetType().Name}");
-        }   
+        }
     }
 }
 // The example displays the following output:
