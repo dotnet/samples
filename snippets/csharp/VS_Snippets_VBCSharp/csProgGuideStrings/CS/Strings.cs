11f5df281572196d052ff8a231f89890fa119cbf
NextTurn <45985406+NextTurn@users.noreply.github.com>
2020-01-06 00:00:00 +0800

    Fix compile errors

--- snippets/csharp/VS_Snippets_VBCSharp/csProgGuideStrings/CS/Strings.cs
+++ snippets/csharp/VS_Snippets_VBCSharp/csProgGuideStrings/CS/Strings.cs
@@ -176,29 +176,10 @@ namespace CsCsrefProgrammingStrings
     }
 
 
-            //<Snippet13>
-            // Date strings are interpreted according to the current culture.
-            // If the culture is en-US, this is interpreted as "January 8, 2008",
-            // but if the user's computer is fr-FR, this is interpreted as "August 1, 2008"
-            string date = "01/08/2008";
-            DateTime dt = Convert.ToDateTime(date);            
-            Console.WriteLine("Year: {0}, Month: {1}, Day: {2}", dt.Year, dt.Month, dt.Day);
-
-            // Specify exactly how to interpret the string.
-            IFormatProvider culture = new System.Globalization.CultureInfo("fr-FR", true);
-
-            // Alternate choice: If the string has been input by an end user, you might 
-            // want to format it according to the current culture:
-            // IFormatProvider culture = System.Threading.Thread.CurrentThread.CurrentCulture;
-            DateTime dt2 = DateTime.Parse(date, culture, System.Globalization.DateTimeStyles.AssumeLocal);
-            Console.WriteLine("Year: {0}, Month: {1}, Day {2}", dt2.Year, dt2.Month, dt2.Day);
-
-            /* Output (assuming first culture is en-US and second is fr-FR):
-                Year: 2008, Month: 1, Day: 8
-                Year: 2008, Month: 8, Day 1
-             */
-            //</Snippet13>
-
+    class ParseStrings
+    {
+        static void Main()
+        {
             //How to: Determine if a string represents a numeric value
             //<Snippet14> 
            
@@ -236,16 +217,21 @@ namespace CsCsrefProgrammingStrings
 
 
 
+    class UsingStringBuilder
+    {
+        static void Main()
+        {
+            //-------------------------------------------------------------------------
+            //<Snippet15>
+            System.Text.StringBuilder sb = new System.Text.StringBuilder("Rat: the ideal pet");
+            sb[0] = 'C';
+            System.Console.WriteLine(sb.ToString());
+            System.Console.ReadLine();
 
-    //-------------------------------------------------------------------------
-    //<Snippet15>
-    System.Text.StringBuilder sb = new System.Text.StringBuilder("Rat: the ideal pet");
-    sb[0] = 'C';
-    System.Console.WriteLine(sb.ToString());
-    System.Console.ReadLine();
-
-    //Outputs Cat: the ideal pet
-    //</Snippet15>
+            //Outputs Cat: the ideal pet
+            //</Snippet15>
+        }
+    }
 
 
     //-------------------------------------------------------------------------
@@ -636,6 +622,8 @@ namespace CsCsrefProgrammingStrings
 
     class StringNullReference
     {
+    static void Main()
+    {
         //<snippet27>
         string question = "hOW DOES mICROSOFT wORD DEAL WITH THE cAPS lOCK KEY?";
         System.Text.StringBuilder sb = new System.Text.StringBuilder(question);
@@ -652,6 +640,7 @@ namespace CsCsrefProgrammingStrings
         System.Console.WriteLine(corrected);
         // Output: How does Microsoft Word deal with the Caps Lock key?            
         //</snippet27>
+    }
     }
     //-------------------------------------------------------------------------
 
