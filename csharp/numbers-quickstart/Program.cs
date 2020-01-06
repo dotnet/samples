cfbdaa76efaadeaeef705396523d43ba0c872c3c
NextTurn <45985406+NextTurn@users.noreply.github.com>
2020-01-06 00:00:00 +0000

    Remove blank lines - access by line

--- csharp/numbers-quickstart/Program.cs
+++ csharp/numbers-quickstart/Program.cs
@@ -104,7 +104,6 @@ namespace NumbersInCSharp
             double radius = 2.50;
             double area = Math.PI * radius * radius;
             Console.WriteLine(area);
-            
 
         }
     }
