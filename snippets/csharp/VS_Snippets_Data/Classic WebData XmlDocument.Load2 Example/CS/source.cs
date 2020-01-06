055b7986a7211396d2b9336f590850c64ebdfd0a
Next Turn <45985406+NextTurn@users.noreply.github.com>
2020-01-07 01:02:22 +0800

    Remove broken and unreferenced snippets (#1883)

--- snippets/csharp/VS_Snippets_Data/Classic WebData XmlDocument.Load2 Example/CS/source.cs
+++ snippets/csharp/VS_Snippets_Data/Classic WebData XmlDocument.Load2 Example/CS/source.cs
@@ -22,29 +22,4 @@ public class Sample
     doc.Save(Console.Out);
   }
 }
-   // </Snippet1>
-
-    public XmlDocument LoadDocument(bool generateXML)
-    {
-    <Snippet2>
-    XmlDocument doc = new XmlDocument();
-    doc.PreserveWhitespace = true;
-    try { doc.Load("booksData.xml"); }
-    catch (System.IO.FileNotFoundException)
-        {
-            doc.LoadXml("<?xml version=\"1.0\"?> \n" +
-            "<books xmlns=\"http://www.contoso.com/books\"> \n" +
-            "  <book genre=\"novel\" ISBN=\"1-861001-57-8\" publicationdate=\"1823-01-28\"> \n" +
-            "    <title>Pride And Prejudice</title> \n" +
-            "    <price>24.95</price> \n" +
-            "  </book> \n" +
-            "  <book genre=\"novel\" ISBN=\"1-861002-30-1\" publicationdate=\"1985-01-01\"> \n" +
-            "    <title>The Handmaid's Tale</title> \n" +
-            "    <price>29.95</price> \n" +
-            "  </book> \n" +
-            "</books>");             
-        }
-    </Snippet2>        
-        return doc;
-        }
-
+// </Snippet1>
