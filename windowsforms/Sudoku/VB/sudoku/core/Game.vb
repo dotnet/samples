34093edb35bf518b9a4df7abeb39d7500b3a14cd
Paul M Cohen <paul1956@users.noreply.github.com>
2020-01-07 03:49:38 -0800

    Fix naming violations

--- windowsforms/Sudoku/VB/sudoku/core/Game.vb
+++ windowsforms/Sudoku/VB/sudoku/core/Game.vb
@@ -3,55 +3,55 @@
     Public Event ShowClues(grid()() As Integer)
     Public Event ShowSolution(grid()() As Integer)
 
-    Private ReadOnly _HRow(8) As List(Of Integer)
-    Private ReadOnly _VRow(8) As List(Of Integer)
-    Private ReadOnly _ThreeSquare(8) As List(Of Integer)
-    Private ReadOnly _Grid(8)() As Integer
+    Private ReadOnly _hRow(8) As List(Of Integer)
+    Private ReadOnly _vRow(8) As List(Of Integer)
+    Private ReadOnly _threeSquare(8) As List(Of Integer)
+    Private ReadOnly _grid(8)() As Integer
 
-    Private _R As Random
+    Private _r As Random
 
     Public Sub NewGame(rn As Random)
-        _R = rn
-        createNewGame()
+        _r = rn
+        CreateNewGame()
     End Sub
 
-    Private Sub initializeLists()
+    Private Sub InitializeLists()
         For x As Integer = 0 To 8
-            _HRow(x) = New List(Of Integer)(New Integer() {1, 2, 3, 4, 5, 6, 7, 8, 9})
-            _VRow(x) = New List(Of Integer)(New Integer() {1, 2, 3, 4, 5, 6, 7, 8, 9})
-            _ThreeSquare(x) = New List(Of Integer)(New Integer() {1, 2, 3, 4, 5, 6, 7, 8, 9})
+            _hRow(x) = New List(Of Integer)(New Integer() {1, 2, 3, 4, 5, 6, 7, 8, 9})
+            _vRow(x) = New List(Of Integer)(New Integer() {1, 2, 3, 4, 5, 6, 7, 8, 9})
+            _threeSquare(x) = New List(Of Integer)(New Integer() {1, 2, 3, 4, 5, 6, 7, 8, 9})
             Dim row(8) As Integer
-            _Grid(x) = row
+            _grid(x) = row
         Next
     End Sub
 
-    Private Sub createNewGame()
+    Private Sub CreateNewGame()
         Do
-            initializeLists()
+            InitializeLists()
             For y As Integer = 0 To 8
                 For x As Integer = 0 To 8
-                    _Grid(y)(x) = Nothing
+                    _grid(y)(x) = Nothing
                     Dim si As Integer = (y \ 3) * 3 + (x \ 3)
-                    Dim useful() As Integer = _HRow(y).Intersect(_VRow(x)).Intersect(_ThreeSquare(si)).ToArray
+                    Dim useful() As Integer = _hRow(y).Intersect(_vRow(x)).Intersect(_threeSquare(si)).ToArray
                     If useful.Any Then
-                        Dim randomNumber As Integer = useful(_R.Next(0, useful.Count))
-                        _HRow(y).Remove(randomNumber)
-                        _VRow(x).Remove(randomNumber)
-                        _ThreeSquare(si).Remove(randomNumber)
-                        _Grid(y)(x) = randomNumber
+                        Dim randomNumber As Integer = useful(_r.Next(0, useful.Count))
+                        _hRow(y).Remove(randomNumber)
+                        _vRow(x).Remove(randomNumber)
+                        _threeSquare(si).Remove(randomNumber)
+                        _grid(y)(x) = randomNumber
                         If y = 8 AndAlso x = 8 Then Exit Do
                     End If
                 Next
             Next
         Loop
 
-        RaiseEvent ShowClues(_Grid)
+        RaiseEvent ShowClues(_grid)
 
     End Sub
 
-    Public Sub showGridSolution()
+    Public Sub ShowGridSolution()
 
-        RaiseEvent ShowSolution(_Grid)
+        RaiseEvent ShowSolution(_grid)
 
     End Sub
 
