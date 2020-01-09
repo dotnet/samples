ee423d024bbe17456df8c2c1457e25db652966c4
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-01-09 22:47:01 +0500

    Improve Process.Exited sample (#1900)
    * Update processexitedevent.cs
    
    * Update processexitedevent.vb
    
    * Remove whitespaces
    
    * Remove whitespaces

--- snippets/visualbasic/VS_Snippets_CLR_System/system.Diagnostics.Process.EnableExited/VB/processexitedevent.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.Diagnostics.Process.EnableExited/VB/processexitedevent.vb
@@ -1,28 +1,24 @@
 ﻿'<snippet1>
 Imports System.Diagnostics
-Imports System.Threading
 
 Class PrintProcessClass
 
     Private WithEvents myProcess As Process
-    Private elapsedTime As Integer
-    Private eventHandled As Boolean
-
-    Public Event Exited As EventHandler
+    Private eventHandled As TaskCompletionSource(Of Boolean)
 
     ' Print a file with any known extension.
-    Sub PrintDoc(ByVal fileName As String)
+    Async Function PrintDoc(ByVal fileName As String) As Task
 
-        elapsedTime = 0
-        eventHandled = False
-
-        Using myProcess = New Process
+        eventHandled = New TaskCompletionSource(Of Boolean)()
+        myProcess = New Process
+        Using myProcess
             Try
                 ' Start a process to print a file and raise an event when done.
                 myProcess.StartInfo.FileName = fileName
                 myProcess.StartInfo.Verb = "Print"
                 myProcess.StartInfo.CreateNoWindow = True
                 myProcess.EnableRaisingEvents = True
+                AddHandler myProcess.Exited, New EventHandler(AddressOf myProcess_Exited)
                 myProcess.Start()
 
             Catch ex As Exception
@@ -30,30 +26,24 @@ Class PrintProcessClass
                 vbCrLf & ex.Message, fileName)
                 Return
             End Try
-        End Using
 
-        ' Wait for Exited event, but not more than 30 seconds.
-        Const SLEEP_AMOUNT As Integer = 100
-        Do While Not eventHandled
-            elapsedTime += SLEEP_AMOUNT
-            If elapsedTime > 30000 Then
-                Exit Do
-            End If
-            Thread.Sleep(SLEEP_AMOUNT)
-        Loop
-    End Sub
+            ' Wait for Exited event, but not more than 30 seconds.
+            Await Task.WhenAny(eventHandled.Task, Task.Delay(30000))
+        End Using
+    End Function
 
     ' Handle Exited event and display process information.
     Private Sub myProcess_Exited(ByVal sender As Object,
-            ByVal e As System.EventArgs) Handles myProcess.Exited
+            ByVal e As System.EventArgs)
 
-        eventHandled = True
         Console.WriteLine("Exit time:    {0}" & vbCrLf &
             "Exit code:    {1}" & vbCrLf & "Elapsed time: {2}",
-            myProcess.ExitTime, myProcess.ExitCode, elapsedTime)
+            myProcess.ExitTime, myProcess.ExitCode,
+            Math.Round((myProcess.ExitTime - myProcess.StartTime).TotalMilliseconds))
+        eventHandled.TrySetResult(True)
     End Sub
 
-    Shared Sub Main(ByVal args() As String)
+    Shared Sub Main(ByVal args As String())
 
         ' Verify that an argument has been entered.
         If args.Length <= 0 Then
@@ -63,7 +53,8 @@ Class PrintProcessClass
 
         ' Create the process and print the document.
         Dim myPrintProcess As New PrintProcessClass
-        myPrintProcess.PrintDoc(args(0))
+        myPrintProcess.PrintDoc(args(0)).Wait()
+
     End Sub
 End Class
 '</snippet1>
