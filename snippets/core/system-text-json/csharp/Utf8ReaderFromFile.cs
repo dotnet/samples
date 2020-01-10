9ce7c1bb8dd1850c2e56fe24359a9444a5d2d4b3
Tom Dykstra <tdykstra@microsoft.com>
2020-01-09 19:39:48 -0800

    Update sample code for new Migrate from Newtonsoft article (#1897)
    * polymorphic example
    
    * interface polymorphic
    
    * revise polymorphic interface example
    
    * feedback + refactor for clarity
    
    * shorten valuetextequals example
    
    * slice bom if present
    
    * uncomment utf8, comment utf16

--- snippets/core/system-text-json/csharp/Utf8ReaderFromFile.cs
+++ snippets/core/system-text-json/csharp/Utf8ReaderFromFile.cs
@@ -8,25 +8,33 @@ namespace SystemTextJsonSamples
     public class Utf8ReaderFromFile
     {
         private static readonly byte[] s_nameUtf8 = Encoding.UTF8.GetBytes("name");
+        private static ReadOnlySpan<byte> Utf8Bom => new byte[] { 0xEF, 0xBB, 0xBF };
         public static void Run()
         {
-            // Read as UTF-16 and transcode to UTF-8 to convert to a Span<byte>
-            string fileName = "Universities.json";
-            string jsonString = File.ReadAllText(fileName);
-            ReadOnlySpan<byte> jsonReadOnlySpan = Encoding.UTF8.GetBytes(jsonString);
+            // ReadAllBytes if the file encoding is UTF-8:
+            string fileName = "UniversitiesUtf8.json";
+            ReadOnlySpan<byte> jsonReadOnlySpan = File.ReadAllBytes(fileName);
 
-            // Or ReadAllBytes if the file encoding is UTF-8:
-            //string fileName = "UniversitiesUtf8.json";
-            //ReadOnlySpan<byte> jsonReadOnlySpan = File.ReadAllBytes(fileName);
+            // Read past the UTF-8 BOM bytes if a BOM exists.
+            if (jsonReadOnlySpan.StartsWith(Utf8Bom))
+            {
+                jsonReadOnlySpan = jsonReadOnlySpan.Slice(Utf8Bom.Length);
+            }
+
+            // Or read as UTF-16 and transcode to UTF-8 to convert to a ReadOnlySpan<byte>
+            //string fileName = "Universities.json";
+            //string jsonString = File.ReadAllText(fileName);
+            //ReadOnlySpan<byte> jsonReadOnlySpan = Encoding.UTF8.GetBytes(jsonString);
+
 
             int count = 0;
             int total = 0;
 
-            var json = new Utf8JsonReader(jsonReadOnlySpan, isFinalBlock: true, state: default);
+            var reader = new Utf8JsonReader(jsonReadOnlySpan);
 
-            while (json.Read())
+            while (reader.Read())
             {
-                JsonTokenType tokenType = json.TokenType;
+                JsonTokenType tokenType = reader.TokenType;
 
                 switch (tokenType)
                 {
@@ -34,11 +42,11 @@ namespace SystemTextJsonSamples
                         total++;
                         break;
                     case JsonTokenType.PropertyName:
-                        if (json.ValueTextEquals(s_nameUtf8))
+                        if (reader.ValueTextEquals(s_nameUtf8))
                         {
                             // Assume valid JSON, known schema
-                            json.Read();
-                            if (json.GetString().EndsWith("University"))
+                            reader.Read();
+                            if (reader.GetString().EndsWith("University"))
                             {
                                 count++;
                             }
