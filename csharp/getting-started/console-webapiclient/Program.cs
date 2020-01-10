83846ccff8d676f5ad87d5e3c9ffc9020bdae20f
Bill Wagner <wiwagn@microsoft.com>
2020-01-09 20:45:17 -0500

    remove an unused using
    The old Json namespace isn't needed anymore.

--- csharp/getting-started/console-webapiclient/Program.cs
+++ csharp/getting-started/console-webapiclient/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Runtime.Serialization.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
 
