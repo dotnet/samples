9eb323c99dce303bc7ea3a34f30f04c31ae8a93c
NextTurn <45985406+NextTurn@users.noreply.github.com>
2020-01-14 00:00:00 +0800

    Move snippet closing tag inside

--- snippets/csharp/VS_Snippets_CLR/IndentedTextWriterExample/CS/form1.cs
+++ snippets/csharp/VS_Snippets_CLR/IndentedTextWriterExample/CS/form1.cs
@@ -59,8 +59,8 @@ namespace IndentedTextWriterExample
                 //<Snippet6>
                 // Outputs a string using the WriteLineNoTabs method.
                 indentWriter.WriteLineNoTabs("This is a test phrase written with the IndentTextWriter.WriteLineNoTabs method.");
+                //</Snippet6>
             }
-            //</Snippet6>
 
             // Outputs a test string with a new-line character at the end.
             indentWriter.WriteLine("This is a test phrase. Current indentation level: "+level.ToString());           
