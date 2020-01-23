56e0a6e2402b3e4f969757495ba0539f80ac7119
Maira Wenzel <mairaw@microsoft.com>
2020-01-23 08:04:15 -0800

    split C++ and VB snippet versions (#1925)
    * split C++ and VB snippet versions
    
    * remove unused snippets

--- snippets/cpp/VS_Snippets_CLR/Process_StandardOutput/CPP/process_standardoutput.cpp
+++ snippets/cpp/VS_Snippets_CLR/Process_StandardOutput/CPP/process_standardoutput.cpp
@@ -1,48 +1,3 @@
-//<snippet1>
-#using <System.dll>
-
-using namespace System;
-using namespace System::IO;
-using namespace System::Diagnostics;
-
-ref class IORedirExample
-{
-public:
-    static void Main()
-    {
-        array<String^>^ args = Environment::GetCommandLineArgs();
-        if (args->Length > 1)
-        {
-            // This is the code for the spawned process
-            Console::WriteLine("Hello from the redirected process!");
-        }
-        else
-        {
-            // This is the code for the base process
-            Process^ myProcess = gcnew Process();
-            // Start a new instance of this program but specify the 'spawned' version.
-            ProcessStartInfo^ myProcessStartInfo = gcnew ProcessStartInfo(args[0], "spawn");
-            myProcessStartInfo->UseShellExecute = false;
-            myProcessStartInfo->RedirectStandardOutput = true;
-            myProcess->StartInfo = myProcessStartInfo;
-            myProcess->Start();
-            StreamReader^ myStreamReader = myProcess->StandardOutput;
-            // Read the standard output of the spawned process.
-            String^ myString = myStreamReader->ReadLine();
-            Console::WriteLine(myString);
-
-            myProcess->WaitForExit();
-            myProcess->Close();
-        }
-    }
-};
-
-int main()
-{
-    IORedirExample::Main();
-}
-//</snippet1>
-//<snippet2>
 using namespace System;
 using namespace System::IO;
 using namespace System::Diagnostics;
@@ -69,4 +24,3 @@ int main()
     Console::ReadLine();
     return 0;
 }
-//</snippet2>
