3b8a82d45aaa0d831cfd72c6612abd5921c459d9
Elinor Fung <47805090+elinor-fung@users.noreply.github.com>
2020-04-15 15:34:22 -0700

    Update HostWithHostFxr sample to call a non-default delegate type (#1984)

--- core/hosting/HostWithHostFxr/src/DotNetLib/Lib.cs
+++ core/hosting/HostWithHostFxr/src/DotNetLib/Lib.cs
@@ -13,7 +13,7 @@ namespace DotNetLib
             public IntPtr Message;
             public int Number;
         }
-        
+
         public static int Hello(IntPtr arg, int argLength)
         {
             if (argLength < System.Runtime.InteropServices.Marshal.SizeOf(typeof(LibArgs)))
@@ -22,14 +22,26 @@ namespace DotNetLib
             }
 
             LibArgs libArgs = Marshal.PtrToStructure<LibArgs>(arg);
+            Console.WriteLine($"Hello, world! from {nameof(Lib)} [count: {s_CallCount++}]");
+            PrintLibArgs(libArgs);
+            return 0;
+        }
+
+        public delegate void CustomEntryPointDelegate(LibArgs libArgs);
+        public static void CustomEntryPoint(LibArgs libArgs)
+        {
+            Console.WriteLine($"Hello, world! from {nameof(CustomEntryPoint)} in {nameof(Lib)}");
+            PrintLibArgs(libArgs);
+        }
+
+        private static void PrintLibArgs(LibArgs libArgs)
+        {
             string message = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
                 ? Marshal.PtrToStringUni(libArgs.Message)
                 : Marshal.PtrToStringUTF8(libArgs.Message);
 
-            Console.WriteLine($"Hello, world! from {nameof(Lib)} [count: {s_CallCount++}]");
             Console.WriteLine($"-- message: {message}");
             Console.WriteLine($"-- number: {libArgs.Number}");
-            return 0;
         }
     }
 }
