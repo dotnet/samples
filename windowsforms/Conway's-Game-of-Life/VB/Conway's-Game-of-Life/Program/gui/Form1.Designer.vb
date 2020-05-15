b0a365e433774676bd8d2e6fa8e2cdc3d56783f7
Rich Lander <rlander@microsoft.com>
2020-05-15 11:50:08 -0700

    Update title bar text

--- windowsforms/Conway's-Game-of-Life/VB/Conway's-Game-of-Life/Program/gui/Form1.Designer.vb
+++ windowsforms/Conway's-Game-of-Life/VB/Conway's-Game-of-Life/Program/gui/Form1.Designer.vb
@@ -79,7 +79,8 @@ Partial Class Form1
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Conway's Game of Life"
+        dim title As string = $"Conway's Game of Life -- {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription} ({System.Runtime.InteropServices.RuntimeInformation.OSArchitecture})"
+        Me.Text = title
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
