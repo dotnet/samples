ea59c18a63f77d96b2344f975d94771e32d08350
David Pine <david.pine@microsoft.com>
2020-05-15 15:01:53 -0500

    Removed priority example, as it was wrong

--- csharp/unit-testing/MSTest.Project/ByAlphabeticalOrder.cs
+++ csharp/unit-testing/MSTest.Project/ByAlphabeticalOrder.cs
@@ -10,20 +10,20 @@ namespace MSTest.Project
         public static bool Test3Called;
 
         [TestMethod]
-        public void Test1()
-        {
-            Test1Called = true;
-
-            Assert.IsFalse(Test2Called);
-            Assert.IsFalse(Test3Called);
-        }
-
-        [TestMethod]
         public void Test2()
         {
             Test2Called = true;
 
             Assert.IsTrue(Test1Called);
+            Assert.IsFalse(Test3Called);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            Test1Called = true;
+
+            Assert.IsFalse(Test2Called);
             Assert.IsFalse(Test3Called);
         }
 
