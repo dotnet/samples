4d0ddd0d70921f71b0c84f7f497c783c299c1789
David Pine <david.pine@microsoft.com>
2020-06-16 10:44:56 -0500

    Added unit testing code coverage samples

---
+++ csharp/unit-testing-code-coverage/XUnit.Coverlet.Collector/PrimeServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Numbers;
+using Xunit;
+
+namespace XUnit.Coverlet
+{
+    public class PrimeServiceTests
+    {
+        readonly PrimeService _primeService;
+
+        public PrimeServiceTests() => _primeService = new PrimeService();
+
+        [
+            Theory,
+            InlineData(-1), InlineData(0), InlineData(1)
+        ]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value) =>
+            Assert.False(_primeService.IsPrime(value), $"{value} should not be prime");
+
+        [
+            Theory,
+            InlineData(2), InlineData(3), InlineData(5), InlineData(7)
+        ]
+        public void IsPrime_PrimesLessThan10_ReturnTrue(int value) =>
+            Assert.True(_primeService.IsPrime(value), $"{value} should be prime");
+
+        [
+            Theory,
+            InlineData(4), InlineData(6), InlineData(8), InlineData(9)
+        ]
+        public void IsPrime_NonPrimesLessThan10_ReturnFalse(int value) =>
+            Assert.False(_primeService.IsPrime(value), $"{value} should not be prime");
+    }
+}
