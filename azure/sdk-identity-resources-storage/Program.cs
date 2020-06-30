5c05856443dfe8d62775e4c982e67ca4f3f23dbc
Cam Soper <cam.soper@microsoft.com>
2020-06-29 19:43:51 -0500

    Sample app for track 2 SDKs

---
+++ azure/sdk-identity-resources-storage/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Azure;
+using Azure.Identity;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
+using Azure.ResourceManager.Storage;
+using Azure.ResourceManager.Storage.Models;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+using AccessTier = Azure.ResourceManager.Storage.Models.AccessTier;
+using Sku = Azure.ResourceManager.Storage.Models.Sku;
+
+namespace AzureIdentityStorageExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string subscriptionId = Environment.GetEnvironmentVariable("AZURE_SUBSCRIPTION_ID");
+
+            DefaultAzureCredential credentials = new DefaultAzureCredential();
+
+            ResourcesManagementClient resourcesManagementClient = new ResourcesManagementClient(subscriptionId, credentials);
+            StorageManagementClient storageManagementClient = new StorageManagementClient(subscriptionId, credentials);
+
+            // Create a Resource Group
+            string resourceGroupName = await CreateResourceGroup(resourcesManagementClient);
+
+            // Create a Storage account
+            string storageName = await CreateStorageAccount(storageManagementClient, resourceGroupName);
+
+            // Create a container and upload a blob
+            await UploadBlobToStorageAccountUsingClientConnectionString(storageManagementClient, resourceGroupName, storageName);
+
+            Console.WriteLine("Press any key to continue and delete the resources...");
+            Console.ReadKey(true);
+
+            await DeleteResourceGroup(resourcesManagementClient, resourceGroupName);
+
+            return;
+        }
+
+        private static async Task DeleteResourceGroup(ResourcesManagementClient resourcesManagementClient, string resourceGroupName)
+        {
+            Console.WriteLine($"Deleting resource group {resourceGroupName}...");
+            ResourceGroupsDeleteOperation deleteOperation = await resourcesManagementClient.ResourceGroups.StartDeleteAsync(resourceGroupName);
+            await deleteOperation.WaitForCompletionAsync();
+            Console.WriteLine("Done!");
+        }
+
+        private static async Task<string> CreateStorageAccount(StorageManagementClient storageManagementClient, string rgName)
+        {
+            Console.WriteLine("Creating a new storage account...");
+
+            string storageAccountName = await GetStorageAccountName(storageManagementClient);
+            StorageAccountCreateParameters parms = new StorageAccountCreateParameters(
+                        new Sku("Standard_LRS"),
+                        Kind.BlobStorage,
+                        "West US");
+            parms.AccessTier = AccessTier.Hot;
+
+            StorageAccountsCreateOperation createStorageAccount =
+                await storageManagementClient.StorageAccounts.StartCreateAsync(rgName, storageAccountName, parms);
+                        await createStorageAccount.WaitForCompletionAsync();
+            Console.WriteLine($"Done creating account {storageAccountName}.");
+
+            return storageAccountName;
+        }
+
+        private static async Task<string> GetStorageAccountName(StorageManagementClient storageManagementClient)
+        {
+            bool foundAvailableName = false;
+            string storageAccountName = string.Empty;
+
+            while (!foundAvailableName)
+            {
+                storageAccountName = RandomName("storage", 20);
+                Response<CheckNameAvailabilityResult> availability =
+                    await storageManagementClient.StorageAccounts.CheckNameAvailabilityAsync(
+                        new StorageAccountCheckNameAvailabilityParameters(storageAccountName));
+
+                if (availability.Value.NameAvailable.GetValueOrDefault())
+                {
+                    foundAvailableName = true;
+                }
+            }
+            return storageAccountName;
+        }
+
+        private static async Task UploadBlobToStorageAccountUsingClientConnectionString(StorageManagementClient storageManagementClient, string resourceGroupName, string storageName)
+        {
+            Console.WriteLine("Creating a container and uploading a blob...");
+
+            string uploadFileName = "dotnet-bot.png";
+            string containerName = "images";
+            string connectionString = await GetStorageConnectionString(storageManagementClient, resourceGroupName, storageName);
+
+            var containerClient = new BlobContainerClient(connectionString, containerName);
+            await containerClient.CreateIfNotExistsAsync(publicAccessType: PublicAccessType.Blob);
+
+            BlobClient blobClient = containerClient.GetBlobClient(uploadFileName);
+            await blobClient.UploadAsync(uploadFileName);
+
+            Console.WriteLine($"Your blob is at https://{storageName}.blob.core.windows.net/{containerName}/{uploadFileName}");
+        }
+
+
+        private static async Task<string> GetStorageConnectionString(StorageManagementClient storageManagementClient, string resourceGroupName, string storageAccountName)
+        {
+            Response<StorageAccountListKeysResult> keys = await storageManagementClient.StorageAccounts.ListKeysAsync(resourceGroupName, storageAccountName);
+            StorageAccountKey storageKey = keys.Value.Keys[0];
+            return $"DefaultEndpointsProtocol=https;AccountName={storageAccountName};AccountKey={storageKey.Value};EndpointSuffix=core.windows.net;";
+        }
+
+        private static async Task<string> CreateResourceGroup(ResourcesManagementClient resourcesManagementClient)
+        {
+            string resourceGroupName = RandomName("rg", 20);
+            Console.WriteLine($"Creating resource group {resourceGroupName}...");
+            await resourcesManagementClient.ResourceGroups.CreateOrUpdateAsync(resourceGroupName, new ResourceGroup("West US"));
+            Console.WriteLine("Done!");
+
+            return resourceGroupName;
+        }
+
+        static string RandomName(string prefix, int maxLen)
+        {
+            var random = new Random();
+            string s = prefix;
+            for (int i = 0; i < (maxLen - prefix.Length); i++)
+                s = String.Concat(s, random.Next(10).ToString());
+            return s;            
+        }
+    }
+}
---
+++ azure/sdk-identity-resources-storage/README.md
@@ -0,0 +1,63 @@
+﻿## Prerequisites
+* An [Azure subscription]().
+
+## Preparing to run the sample
+Use the [Azure Cloud Shell](https://shell.azure.com) to create/get client secret credentials.
+
+ * Create a service principal and configure its access to Azure resources:
+    ```bash
+    az ad sp create-for-rbac -n <your-application-name> --skip-assignment
+    ```
+    Output:
+    ```json
+    {
+        "appId": "generated-app-ID",
+        "displayName": "dummy-app-name",
+        "name": "http://dummy-app-name",
+        "password": "random-password",
+        "tenant": "tenant-ID"
+    }
+    ```
+* Use the returned credentials above to set the following environment variables:
+
+    |variable name|value
+    |-|-
+    |`AZURE_CLIENT_ID`|service principal's app id
+    |`AZURE_TENANT_ID`|id of the principal's Azure Active Directory tenant
+    |`AZURE_CLIENT_SECRET`|one of the service principal's client secrets
+
+* Get the account details of the subscription you want to use for this sample.
+    ```bash
+    az account show
+    ```
+
+    Output:
+    ```json
+    {
+      "environmentName": "AzureCloud",
+      "homeTenantId": "tenant-id",
+      "id": "subscription-id",
+      "isDefault": true,
+      "managedByTenants": [],
+      "name": "subscriptionName",
+      "state": "Enabled",
+      "tenantId": "tenant-id",
+      "user": {
+        "cloudShellID": true,
+        "name": "user@contoso.com",
+        "type": "user"
+      }
+    }
+    ```
+
+* Set the `AZURE_SUBSCRIPTION_ID` environment variable using the `id` property of the information retrieved in the previous step.
+
+    |variable name|value
+    |-|-
+    |`AZURE_SUBSCRIPTION_ID`|service principal's app id
+
+## Run the sample
+
+```bash
+dotnet run
+```
