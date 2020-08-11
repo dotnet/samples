28255a57cc303f4503b12efa5c861c8abd2694c3
David Pine <david.pine@microsoft.com>
2020-08-11 13:16:10 -0500

    Added sync, serial-async, and parallel-async examples.

--- async/async-and-await/cs/ParallelAsyncExample/MainWindow.xaml.cs
+++ async/async-and-await/cs/ParallelAsyncExample/MainWindow.xaml.cs
@@ -20,18 +20,38 @@ namespace ParallelAsyncExample
             "https://docs.microsoft.com/powershell",
             "https://docs.microsoft.com/dotnet",
             "https://docs.microsoft.com/aspnet/core",
-            "https://docs.microsoft.com/windows"
+            "https://docs.microsoft.com/windows",
+            "https://docs.microsoft.com/office",
+            "https://docs.microsoft.com/enterprise-mobility-security",
+            "https://docs.microsoft.com/visualstudio",
+            "https://docs.microsoft.com/microsoft-365",
+            "https://docs.microsoft.com/sql",
+            "https://docs.microsoft.com/dynamics365",
+            "https://docs.microsoft.com/surface",
+            "https://docs.microsoft.com/xamarin",
+            "https://docs.microsoft.com/azure/devops",
+            "https://docs.microsoft.com/system-center",
+            "https://docs.microsoft.com/graph",
+            "https://docs.microsoft.com/education",
+            "https://docs.microsoft.com/gaming",
         };
 
-        async void OnStartButtonClick(object sender, RoutedEventArgs e)
+        void OnStartButtonClick(object sender, RoutedEventArgs e)
         {
             _startButton.IsEnabled = false;
             _resultsTextBox.Clear();
 
-            await SumPageSizesAsync();
-            await UpdateResultText($"\nControl returned to {nameof(OnStartButtonClick)}.");
+            Task.Run(StartSumPageSizesAsync);
+        }
 
-            _startButton.IsEnabled = true;
+        async Task StartSumPageSizesAsync()
+        {
+            await SumPageSizesAsync();
+            await Dispatcher.BeginInvoke(() =>
+            {
+                _resultsTextBox.Text += $"\nControl returned to {nameof(OnStartButtonClick)}.";
+                _startButton.IsEnabled = true;
+            });
         }
 
         async Task SumPageSizesAsync()
@@ -41,33 +61,33 @@ namespace ParallelAsyncExample
 
             IEnumerable<Task<int>> downloadTasksQuery =
                 from url in _urlList
-                select ProcessURLAsync(url, _client);
+                select ProcessUrlAsync(url, _client);
 
             Task<int>[] downloadTasks = downloadTasksQuery.ToArray();
 
             int[] lengths = await Task.WhenAll(downloadTasks);
             int total = lengths.Sum();
 
-            stopwatch.Stop();
-            await UpdateResultText($"\nTotal bytes returned:  {total:#,#}");
-            await UpdateResultText($"\nElapsed time:          {stopwatch.Elapsed}\n");
+            await Dispatcher.BeginInvoke(() =>
+            {
+                stopwatch.Stop();
+
+                _resultsTextBox.Text += $"\nTotal bytes returned:  {total:#,#}";
+                _resultsTextBox.Text += $"\nElapsed time:          {stopwatch.Elapsed}\n";
+            });
         }
 
-        async Task<int> ProcessURLAsync(string url, HttpClient client)
+        async Task<int> ProcessUrlAsync(string url, HttpClient client)
         {
             byte[] byteArray = await client.GetByteArrayAsync(url);
-            await DisplayResults(url, byteArray);
+            await DisplayResultsAsync(url, byteArray);
 
             return byteArray.Length;
         }
 
-        Task DisplayResults(string url, byte[] content) =>
-            UpdateResultText($"{url,-60} {content.Length,10:#,#}\n");
-
-        Task UpdateResultText(string text) =>
-            Dispatcher.BeginInvoke(
-                () => _resultsTextBox.Text += text,
-                DispatcherPriority.SystemIdle)
+        Task DisplayResultsAsync(string url, byte[] content) =>
+            Dispatcher.BeginInvoke(() =>
+                _resultsTextBox.Text += $"{url,-60} {content.Length,10:#,#}\n")
                       .Task;
 
         protected override void OnClosed(EventArgs e) => _client?.Dispose();
