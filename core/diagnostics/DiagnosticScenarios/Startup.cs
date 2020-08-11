0d64ca1e05e72dd33be796c0bacab9fc1e171dc3
David Pine <david.pine@microsoft.com>
2020-08-11 13:40:07 -0500

    Oops, missed these...

--- core/diagnostics/DiagnosticScenarios/Startup.cs
+++ core/diagnostics/DiagnosticScenarios/Startup.cs
@@ -25,7 +25,7 @@ namespace DiagnosticScenarios
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers(options => options.Filters.Add<LogRequestTimeFilterAttribute>())
+            services.AddControllers()
                 .AddNewtonsoftJson();
         }
 
