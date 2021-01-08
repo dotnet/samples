cecac8b1df687ad991c0a64c8cc002b9f5cc4bf5
vitek-karas <vitek.karas@microsoft.com>
2021-01-08 05:11:04 -0800

    Update the sample to target netcoreapp3.1
    It generates warnings because it targets out-of-support 3.0

--- core/extensions/AppWithPlugin/AppWithPlugin/Program.cs
+++ core/extensions/AppWithPlugin/AppWithPlugin/Program.cs
@@ -21,12 +21,12 @@ namespace AppWithPlugin
 
                 string[] pluginPaths = new string[]
                 {
-                    @"HelloPlugin\bin\Debug\netcoreapp3.0\HelloPlugin.dll",
-                    @"JsonPlugin\bin\Debug\netcoreapp3.0\JsonPlugin.dll",
-                    @"XcopyablePlugin\bin\Debug\netcoreapp3.0\XcopyablePlugin.dll",
-                    @"OldJsonPlugin\bin\Debug\netcoreapp2.1\OldJsonPlugin.dll",
-                    @"FrenchPlugin\bin\Debug\netcoreapp2.1\FrenchPlugin.dll",
-                    @"UVPlugin\bin\Debug\netcoreapp2.1\UVPlugin.dll",
+                    @"HelloPlugin\bin\Debug\netcoreapp3.1\HelloPlugin.dll",
+                    @"JsonPlugin\bin\Debug\netcoreapp3.1\JsonPlugin.dll",
+                    @"XcopyablePlugin\bin\Debug\netcoreapp3.1\XcopyablePlugin.dll",
+                    @"OldJsonPlugin\bin\Debug\netcoreapp3.1\OldJsonPlugin.dll",
+                    @"FrenchPlugin\bin\Debug\netcoreapp3.1\FrenchPlugin.dll",
+                    @"UVPlugin\bin\Debug\netcoreapp3.1\UVPlugin.dll",
                 };
 
                 IEnumerable<ICommand> commands = pluginPaths.SelectMany(pluginPath =>
