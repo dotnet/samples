a39cf54a6f3890fcee953c63a929391a9b82382d
Bill Wagner <wiwagn@microsoft.com>
2021-02-16 13:53:47 -0500

    Finish getstarted tutorials (#4330)
    * upgrade to .NET 5
    
    Top level statements,
    new TFM
    
    * update to .NET 5
    
    Add top level statements
    Add missing samples from article.
    
    * update to .NET 5
    
    Use top level statements for simplicity

--- csharp/numbers-quickstart/Program.cs
+++ csharp/numbers-quickstart/Program.cs
@@ -1,110 +1,108 @@
 ﻿using System;
 
-namespace NumbersInCSharp
+WorkWithIntegers();
+OrderPrecedence();
+TestLimits();
+WorkWithDoubles();
+WorkWithDecimals();
+
+double radius = 2.50;
+double area = Math.PI * radius * radius;
+Console.WriteLine(area);
+
+void WorkWithIntegers()
 {
-    class Program
-    {
-        static void WorkingWithIntegers()
-        {
-            int a = 18;
-            int b = 6;
-            int c = a + b;
-            Console.WriteLine(c);
-            c = a - b;
-            Console.WriteLine(c);
-            c = a * b;
-            Console.WriteLine(c);
-            c = a / b;
-            Console.WriteLine(c);
-        }
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
 
-        static void OrderPrecedence()
-        {
-            int a = 5;
-            int b = 4;
-            int c = 2;
-            int d = a + b * c;
-            Console.WriteLine(d);
 
-            d = (a + b) * c;
-            Console.WriteLine(d);
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
 
-            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
-            Console.WriteLine(d);
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
 
-            int e = 7;
-            int f = 4;
-            int g = 3;
-            int h = (e + f) / g;
-            Console.WriteLine(h);
-        }
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
 
-        static void TestLimits()
-        {
-            int a = 7;
-            int b = 4;
-            int c = 3;
-            int d = (a + b) / c;
-            int e = (a + b) % c;
-            Console.WriteLine($"quotient: {d}");
-            Console.WriteLine($"remainder: {e}");
+void OrderPrecedence()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
 
-            int max = int.MaxValue;
-            int min = int.MinValue;
-            Console.WriteLine($"The range of integers is {min} to {max}");
+    d = (a + b) * c;
+    Console.WriteLine(d);
 
-            int what = max + 3;
-            Console.WriteLine($"An example of overflow: {what}");
-        }
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
 
-        static void WorkWithDoubles()
-        {
-            double a = 5;
-            double b = 4;
-            double c = 2;
-            double d = (a + b) / c;
-            Console.WriteLine(d);
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
+}
 
-            double e = 19;
-            double f = 23;
-            double g = 8;
-            double h = (e + f) / g;
-            Console.WriteLine(h);
+void TestLimits()
+{
+    int a = 7;
+    int b = 4;
+    int c = 3;
+    int d = (a + b) / c;
+    int e = (a + b) % c;
+    Console.WriteLine($"quotient: {d}");
+    Console.WriteLine($"remainder: {e}");
 
-            double max = double.MaxValue;
-            double min = double.MinValue;
-            Console.WriteLine($"The range of double is {min} to {max}");
+    int max = int.MaxValue;
+    int min = int.MinValue;
+    Console.WriteLine($"The range of integers is {min} to {max}");
 
-            double third = 1.0 / 3.0;
-            Console.WriteLine(third);
-        }
+    int what = max + 3;
+    Console.WriteLine($"An example of overflow: {what}");
+}
 
-        static void Main(string[] args)
-        {
-            WorkingWithIntegers();
+void WorkWithDoubles()
+{
+    double a = 5;
+    double b = 4;
+    double c = 2;
+    double d = (a + b) / c;
+    Console.WriteLine(d);
 
-            OrderPrecedence();
+    double e = 19;
+    double f = 23;
+    double g = 8;
+    double h = (e + f) / g;
+    Console.WriteLine(h);
 
-            TestLimits();
+    double max = double.MaxValue;
+    double min = double.MinValue;
+    Console.WriteLine($"The range of double is {min} to {max}");
 
-            WorkWithDoubles();
+    double third = 1.0 / 3.0;
+    Console.WriteLine(third);
+}
 
-            decimal min = decimal.MinValue;
-            decimal max = decimal.MaxValue;
-            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+void WorkWithDecimals()
+{
+    decimal min = decimal.MinValue;
+    decimal max = decimal.MaxValue;
+    Console.WriteLine($"The range of the decimal type is {min} to {max}");
 
-            double a = 1.0;
-            double b = 3.0;
-            Console.WriteLine(a / b);
+    double a = 1.0;
+    double b = 3.0;
+    Console.WriteLine(a / b);
 
-            decimal c = 1.0M;
-            decimal d = 3.0M;
-            Console.WriteLine(c / d);
-
-            double radius = 2.50;
-            double area = Math.PI * radius * radius;
-            Console.WriteLine(area);
-
-        }
-    }
+    decimal c = 1.0M;
+    decimal d = 3.0M;
+    Console.WriteLine(c / d);
 }
