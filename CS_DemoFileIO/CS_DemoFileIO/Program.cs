1d166f60f13efdea601038cb45ffab4a44791ef9
Shweta Navalgund <shwetan1097@gmail.com>
2021-05-04 09:14:50 +0530

    Added DemoHashing and DemoFileIO samples

---
+++ CS_DemoFileIO/CS_DemoFileIO/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CS_DemoFileIO
+{
+    class Program
+    {
+        
+
+        static void WriteBinaryFile(string data)
+        {
+            FileStream stream = new FileStream("SampleBinaryFile.bin", FileMode.Create, FileAccess.Write);
+            BinaryWriter writer = new BinaryWriter(stream);
+            try
+            {
+                writer.Write(true);
+                writer.Write(DateTime.Now.ToString());
+                writer.Write(1000);
+                writer.Write(data);
+                writer.Flush();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                writer.Close();
+                writer.Dispose();
+                stream.Close();
+                stream.Dispose();
+            }
+        }
+
+        static void ReadBinaryFile()
+        {
+            FileStream stream = new FileStream("SampleBinaryFile.bin", FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(stream);
+            try
+            {
+                Console.WriteLine(reader.ReadBoolean());
+                Console.WriteLine(DateTime.Parse(reader.ReadString()));
+                Console.WriteLine(reader.ReadInt32());
+                Console.WriteLine(reader.ReadString());
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                reader.Close();
+                reader.Dispose();
+                stream.Close();
+                stream.Dispose();
+            }
+        }
+
+        static void WriteTextFile(string data)
+        {
+            FileStream stream = new FileStream("SampleBinaryFile.bin", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+            try
+            {
+                writer.WriteLine(data);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                writer.Close();
+                writer.Dispose();
+                stream.Close();
+                stream.Dispose();
+            }
+        }
+
+        static void ReadTextFile()
+        {
+            FileStream stream = new FileStream("SampleBinaryFile.bin", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            try
+            {
+                string data = reader.ReadToEnd();
+                Console.WriteLine(data);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                reader.Close();
+                reader.Dispose();
+                stream.Close();
+                stream.Dispose();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter the input : ");
+            //string input = Console.ReadLine();
+            //WriteBinaryFile(input);
+
+            //ReadBinaryFile();
+
+            //Console.Write("Enter the input : ");
+            //string input = Console.ReadLine();
+            //WriteTextFile(input);
+
+            //ReadTextFile();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CS_DemoHashing/CS_DemoHashing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Security.Cryptography;
+using static System.Text.Encoding;
+
+namespace CS_DemoHashing
+{
+    class Program
+    {
+        static string ComputeHash(string input)
+        {
+            HashAlgorithm sha = SHA256.Create();
+            var hashData = sha.ComputeHash(UTF8.GetBytes(input));
+            return UTF8.GetString(hashData);
+        }
+
+        static void Main(string[] args)
+        {
+            string input= "This is very very long test string!";
+            var hash = ComputeHash(input);
+            WriteLine($"The original data : {input}\n Hashed data : {hash}");
+            WriteLine(ComputeHash("This is very very long test string!") == hash);
+
+            ReadKey();
+        }
+    }
+}
