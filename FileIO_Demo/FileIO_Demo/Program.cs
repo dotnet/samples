172e3dc928adfcc46d7ed662b9dd44e0a6389b1c
Insiya Kanchwala <insikanchwala53@gmail.com>
2021-05-04 09:14:33 +0530

    Added JSON Serialization Sample and File IO sample

---
+++ FileIO_Demo/FileIO_Demo/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO_Demo
+{
+    class Program
+    {
+        static void WriteBinaryFile(string data) 
+        {
+            //write to binary file (bin--debug--.bin file is present here)
+            FileStream stream = new FileStream("SampleBinaryFile.bin", FileMode.Create, FileAccess.Write);
+            BinaryWriter writer = new BinaryWriter(stream);
+            try
+            {
+                writer.Write(true);
+                writer.Write(DateTime.Now.ToString());
+                writer.Write(10000);
+                writer.Write(data);
+                writer.Flush();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                writer.Close();
+                writer.Dispose();
+                stream.Close();
+                stream.Dispose();
+            }
+        }
+
+        static void ReadBinaryfile()
+        {
+            //read from binary file
+            FileStream stream = new FileStream("SampleBinaryFile.bin", FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(stream);
+            try
+            {
+                Console.WriteLine(reader.ReadBoolean());
+                Console.WriteLine(DateTime.Parse(reader.ReadString()));
+                Console.WriteLine(reader.ReadInt32());
+                Console.WriteLine(reader.ReadString());
+            }
+            catch (IOException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                reader.Close();
+                reader.Dispose();
+                stream.Close();
+                stream.Dispose();
+            }
+        }
+
+        static void WriteTextFile(string data)
+        {
+            FileStream stream = new FileStream("SampleTextFile.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+            try
+            {
+                writer.Write(data);
+                writer.Flush();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                writer.Close();
+                writer.Dispose();
+                stream.Close();
+                stream.Dispose();
+            }
+        }
+
+        static void ReadTextFile()
+        {
+            //read from binary file
+            FileStream stream = new FileStream("SampleTextFile.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            try
+            {
+                Console.WriteLine(reader.ReadToEnd());
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                reader.Close();
+                reader.Dispose();
+                stream.Close();
+                stream.Dispose();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //to write in binary file
+            //Console.Write("Enter the Input Data: ");
+            //string input = Console.ReadLine();
+            //WriteBinaryFile(input);
+
+            //to read from binary file
+            //ReadBinaryfile();
+
+            //to write in text file
+            //Console.Write("Enter the Input Data: ");
+            //string input = Console.ReadLine();
+            //WriteTextFile(input);
+
+            //to read from text file
+            ReadTextFile();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ JSON_Serialization/JSON_Serialization/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace JSON_Serialization
+{
+    //Command : "Install-Package Newtonsoft.josn" to install JSON
+    //[Serializable]
+    class Person 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+    } 
+    class Program
+    {
+        //JSON Serialization using JsonConvert
+        static void JsonSerializationJsonConvert() 
+        {
+            Person p = new Person()
+            {
+                Id = 101,
+                Name = "Insiya",
+                DOB = DateTime.Parse("10-Apr-1997")
+            };
+            string PersonData = JsonConvert.SerializeObject(p);
+            Console.WriteLine($"JSON Serialized Person : {PersonData}");
+        }
+
+        static void JsonDeSerializationJsonConvert()
+        {
+            Person p = JsonConvert.DeserializeObject<Person>("{'Id':101,'Name':'Insiya','DOB':'1997 - 04 - 10T00: 00:00'}");
+            Console.WriteLine($"JSON De-Serialized Person - ID : {p.Id}, Name : {p.Name}, DOB : {p.DOB}");
+        }
+
+        //JSON Serialization using JsonSerializer
+        static void JsonSerializationUsingJsonSerializer() 
+        {
+            Person p = new Person() 
+            { 
+                Id = 101, 
+                Name = "Robert", 
+                DOB = DateTime.Parse("10-Apr-1997") 
+            };
+            //StreamWriter sw = new StreamWriter("JSON.txt");
+            //JsonWriter writer = new JsonTextWriter(sw);
+            //JsonSerializer serializer = new JsonSerializer();
+            //serializer.Serialize(writer, p);
+            using (StreamWriter sw = new StreamWriter("JSON1.txt"))
+            {
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(writer, p);
+                }
+            }
+
+        }
+
+        static void JsonDeSerializationUsingJsonSerializer()
+        {
+           
+            StreamReader sr = new StreamReader("JSON1.txt");
+            JsonReader reader = new JsonTextReader(sr);
+            JsonSerializer serializer = new JsonSerializer();
+            Person p = serializer.Deserialize<Person>(reader);
+            Console.WriteLine($"ID : {p.Id}, Name : {p.Name}, DOB : {p.DOB}");
+        }
+
+        //JSON Serialization using DataContractJsonSerializer
+        static void JsonS_DataContractJsonSerializer()
+        {
+            Person p = new Person() { Id = 101, Name = "Robert", DOB = DateTime.Parse("10-Apr-1997") };
+            Stream s = new FileStream("JSON2.txt", FileMode.Create, FileAccess.Write);
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(Person));
+            js.WriteObject(s, p);
+            s.Close();
+
+        }
+
+        static void JsonDS_DataContractJsonSerializer()
+        {
+            Stream s = new FileStream("JSON2.txt", FileMode.Open, FileAccess.Read);
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(Person));
+            Person p = js.ReadObject(s) as Person;
+            s.Close();
+            Console.WriteLine($"ID : {p.Id}, Name : {p.Name}, DOB : {p.DOB}");
+        }
+
+        static void Main(string[] args)
+        {
+            //JSON Serialization using JsonConvert
+            //JsonSerializationJsonConvert();
+            //Console.WriteLine("JSON Serialization Completed using JSONConvert.");
+            //JsonDeSerializationJsonConvert();
+
+            //JSON Serialization using JsonSerializer
+            //JsonSerializationUsingJsonSerializer();
+            //Console.WriteLine("JSON Serialization Completed using JsonSerializer.");
+            //JsonDeSerializationUsingJsonSerializer();
+
+            //JSON Serialization using DataContractJsonSerializer
+            //JsonS_DataContractJsonSerializer();
+            //Console.WriteLine("JSON Serialization Completed using DataContractJsonSerializer.");
+            //JsonDS_DataContractJsonSerializer();
+            //Console.ReadKey();
+
+            //hw - try josnSerializer and josnDeSerializer method from pdf and run code
+        }
+    }
+}
