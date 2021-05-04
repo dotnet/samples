58f42a44802051cfa9ffe246f05f2d2a84540bd5
shraddha <shraddhagadankush0603@gmail.com>
2021-05-04 09:14:22 +0530

    Added Regex sample and local functions c# 7.0

---
+++ CS_DemoRegEx/CS_DemoRegEx/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CS_DemoRegEx
+{
+    class Program
+    {
+        
+     public static bool IsValid(string value)
+        {
+            //return Regex.IsMatch(value, @"^[a-zA-Z0-9]*$");
+            Regex regex = new Regex (@"^[a-zA-Z0-9]*$");
+            return regex.IsMatch(value);
+        }
+
+         private static void ShowMatch(string text, string expression)
+        {
+            Console.WriteLine("The Expression:" +expression);
+            MatchCollection matchcollection = Regex.Matches(text, expression);
+            foreach(Match match in matchcollection)
+            { 
+                Console.WriteLine(match);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(IsValid("DotNetLearningAndDevelopment2019"));
+            // Console.WriteLine(IsValid("DotNet Learning And Development 2019"));
+            //string input = "The sun sets in south west during winter";
+            //Console.WriteLine("Matching words that start with 's':");
+            //ShowMatch(input, @"\bS\S*");
+            //string input = "welcome to CapGemini";
+            //string pattern= 2"\s+";
+            //string replacement = "-";
+
+            //Regex regex = new Regex(pattern);
+            //string result = regex.Replace(input, replacement);
+
+            //Console.WriteLine($"Original String : {input}");
+            //Console.WriteLine($"Replacement String : {result}");
+
+            string operation = "3 * 5 = 15";
+            var operands = Regex.Split(operation, @"\s+");
+            foreach(var operand in operands)
+            {
+                Console.WriteLine(operand);
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Cs_DemoRefLocalsandReturnsCs7/Cs_DemoRefLocalsandReturnsCs7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Cs_DemoRefLocalsandReturnsCs7
+{
+    class Program
+    {
+        static ref char FindCharReference(char[] arr, in char v) //ref return in c# 7.0
+        { 
+            for (int i = 0; i < arr.Length; i++)
+            { 
+                if(arr[i]==v)
+                {
+                    return ref arr[i]; // //ref return in c# 7.0
+                }
+
+            }
+            throw new IndexOutOfRangeException($"{v} is not found");
+
+         }
+        static void Main(string[] args)
+        {
+            char[] arr = { 'h','y','d','e','r','a','b','a','d'};
+            WriteLine($"Before : {new string(arr)}");
+            char c = 'a';
+            ref char charref = ref FindCharReference(arr, in c);
+            charref = 'A';
+            WriteLine($"After : {new string(arr)}");
+
+            ReadKey();
+        }
+    }
+}
