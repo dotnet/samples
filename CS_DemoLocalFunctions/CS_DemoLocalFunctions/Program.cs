3c53e8fcf428b7d05259c94d013e8ec37e6ec5cc
Jagrutee Dhanawade <jagrutee98@gmail.com>
2021-05-04 15:33:06 +0530

    Serialization sample and Local function sample file added

---
+++ CS_DemoLocalFunctions/CS_DemoLocalFunctions/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DemoLocalFunctions
+{
+    class Calc
+    {
+        public float DoCalc(float a, float b, char op)
+        {
+            float Add()
+            {
+                return a + b;
+            }
+
+            float Mult()
+            {
+                return a * b;
+            }
+
+            float Subtract()
+            {
+                return a - b;
+            }
+
+            float Div()
+            {
+                return a / b;
+            }
+
+            float Mod()
+            {
+                return a % b;
+            }
+
+            switch (op)
+            {
+                case '+':
+                    return Add();
+                case '*':
+                    return Mult();
+                case '-':
+                    return Subtract();
+                case '/':
+                    return Div();
+                case '%':
+                    return Mod();
+
+                default:
+                    Console.WriteLine($"Invalid Operator - '{op}'");
+                    return 0.0f;
+            }
+            //Console.WriteLine($"Invalid Operator - '{op}'");
+            //return 0.0f;
+        }
+
+        //public float DoCalc(float a, float b, char op)
+        //{
+        //    switch (op)
+        //    {
+        //        case '+':
+        //            return Add(a, b);
+        //        case '*':
+        //            return Mult(a, b);
+        //        case '-':
+        //            return Subtract(a, b);
+        //        case '/':
+        //            return Div(a, b);
+        //        case '%':
+        //            return Mod(a, b);
+
+        //        default:
+        //            Console.WriteLine($"Invalid Operator - '{op}'");
+        //            return 0.0f;
+        //    }
+        //    //Console.WriteLine($"Invalid Operator - '{op}'");
+        //    //return 0.0f;
+        //}
+
+        //private float Add(float a, float b)
+        //{
+        //    return a + b;
+        //}
+
+        //private float Mult(float a, float b)
+        //{
+        //    return a * b;
+        //}
+
+        //private float Subtract(float a, float b)
+        //{
+        //    return a - b;
+        //}
+
+        //private float Div(float a, float b)
+        //{
+        //    return a / b;
+        //}
+
+        //private float Mod(float a, float b)
+        //{
+        //    return a % b;
+        //}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+            Console.Write("Enter first number : ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            float b = float.Parse(Console.ReadLine());
+            Console.Write("Enter the operator (+, -, *, /, %) : ");
+            char op = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{a} {op} {b} = {calc.DoCalc(a, b, op):f2}");
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ DemoSerialization/DemoSerialization/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+
+namespace DemoSerialization
+{
+    [Serializable]
+    public class Person:ISerializable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Age { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(SerializationInfo info, StreamingContext context)
+        {
+            this.Id =(int) info.GetValue("Identity", typeof(int));
+            this.Name =(string) info.GetValue("FullName", typeof(string));
+            this.Age =(float) info.GetValue("Age", typeof(float));
+        }
+
+        //public Person(int Id, string Name, float Age)
+        //{
+        //    this.Id = Id;
+        //    this.Name = Name;
+        //    this.Age = Age;
+        //}
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Identity", Id);
+            info.AddValue("FullName", Name);
+            info.AddValue("Age", Age);
+        }
+
+        public override string ToString()
+        {
+            return $"Id : {Id}, Name : {Name}, Age : {Age}";
+        }
+    }
+    class Program
+    {
+        static void SerializeBinary()
+        {
+            Person p = new Person()
+            {
+                Id=101,
+                Name="James",
+                Age=45.5f
+            };
+
+            using (FileStream fs=new FileStream("PersonData.bin",FileMode.Create,FileAccess.Write))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(fs, p);
+                fs.Close();
+            }
+        }
+
+        static Person DeserializeBinary()
+        {
+            Person p ;
+
+            using (FileStream fs = new FileStream("PersonData.bin", FileMode.Open, FileAccess.Read))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                p = formatter.Deserialize(fs) as Person;
+                fs.Close();
+            }
+            return p;
+        }
+
+        static void SerializeSOAP()
+        {
+            Person p = new Person()
+            {
+                Id = 101,
+                Name = "James",
+                Age = 45.5f
+            };
+
+            using (FileStream fs = new FileStream("PersonDataSoap.xml", FileMode.Create, FileAccess.Write))
+            {
+                SoapFormatter formatter = new SoapFormatter();
+                formatter.Serialize(fs, p);
+                fs.Close();
+            }
+        }
+
+        static Person DeserializeSOAP()
+        {
+            Person p;
+
+            using (FileStream fs = new FileStream("PersonDataSoap.xml", FileMode.Open, FileAccess.Read))
+            {
+                SoapFormatter formatter = new SoapFormatter();
+                p = formatter.Deserialize(fs) as Person;
+                fs.Close();
+            }
+            return p;
+        }
+
+        static void SerializeXML()
+        {
+            Person p = new Person()
+            {
+                Id = 101,
+                Name = "James",
+                Age = 45.5f
+            };
+
+            using (FileStream fs = new FileStream("PersonDataXML.xml", FileMode.Create, FileAccess.Write))
+            {
+                XmlSerializer xs = new XmlSerializer(p.GetType());
+                xs.Serialize(fs, p);
+                fs.Close();
+            }
+        }
+
+        static Person DeserializeXML()
+        {
+            Person p;
+
+            using (FileStream fs = new FileStream("PersonDataXML.xml", FileMode.Open, FileAccess.Read))
+            {
+                XmlSerializer xs = new XmlSerializer(typeof(Person));
+                p = xs.Deserialize(fs) as Person;
+                fs.Close();
+            }
+            return p;
+        }
+
+        static void Main(string[] args)
+        {
+            SerializeBinary();
+            var p = DeserializeBinary();
+            Console.WriteLine($"From Binary : {p}");
+            SerializeSOAP();
+            p = DeserializeSOAP();
+            Console.WriteLine($"From SOAP : {p}");
+            SerializeXML();
+            p = DeserializeXML();
+            Console.WriteLine($"From XML : {p}");
+
+            Console.ReadKey();
+        }
+    }
+}
