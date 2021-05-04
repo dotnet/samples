c98aa9ed28b1da0f86bd6fe5012ea54a6c4560ce
Purva Ghodke <ghodkepurva@gmail.com>
2021-05-04 09:14:43 +0530

    added Json Serialization sample and local functions C# 7.0 samples

---
+++ CS_DemoJsonSerialization/CS_DemoJsonSerialization/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CS_DemoJsonSerialization
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+    class Program
+    {
+        static void JsonSerialize()
+        {
+            Person p = new Person()
+            {
+                Id = 101,
+                Name = "bob",
+                DateOfBirth = DateTime.Parse("20-Apr-1976")
+            };
+            string personData = JsonConvert.SerializeObject(p);
+            Console.WriteLine($"Json Serialized Person:{personData}");
+        }
+
+        static void JsonDeserialize()
+        {
+            
+            Person p = JsonConvert.DeserializeObject<Person>("{'Id':101,'Name':'bob','DateOfBirth':'1976-04-20T00:00:00'}");
+            Console.WriteLine($"Id:{p.Id},Name:{p.Name},DateOfBirth:{p.DateOfBirth}"); 
+        }
+        static void Main(string[] args)
+        {
+            JsonSerialize();
+            Console.WriteLine("\n\nSerialization completed.....");
+            JsonDeserialize();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CS_DemoLocalFunctionInCS7/CS_DemoLocalFunctionInCS7/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CS_DemoLocalFunctionInCS7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("enter a first number: ");
+            float a = float.Parse(ReadLine());
+
+            Write("enter second number: ");
+            float b = float.Parse(ReadLine());
+
+            Write("enter the operator: ");
+            char op = char.Parse(ReadLine());
+
+            Calc calc = new Calc();
+            WriteLine($"{a} {op} {b}={calc.DoCalc(a, b, op):f2}");
+
+            ReadKey();
+
+        }
+    }
+
+    class Calc
+    {
+        public float DoCalc(float a, float b, char op)
+        {
+                 float Mod()
+                {
+                    return a % b;
+                }
+
+                float Div()
+                {
+                    return a / b;
+                }
+
+               float Mult()
+                {
+                    return a * b;
+                }
+
+                float Subtract()
+                {
+                    return a - b;
+                }
+
+                 float Add()
+                {
+                    return a + b;
+                }
+            
+            switch (op)
+            {
+                //case '+':return Add(a, b);
+                //case '-':return Subtract(a, b);
+                //case '*':return Mult(a, b);
+                //case '/':return Div(a, b);
+                //case '%':return Mod(a, b);
+                //default:WriteLine($"invalid operator - '{op}'");
+                case '+': return Add();
+                case '-': return Subtract();
+                case '*': return Mult();
+                case '/': return Div();
+                case '%': return Mod();
+                default:WriteLine($"invalid operator - '{op}'");
+
+                    
+
+                    return 0.0f;
+            }            
+        }
+
+        //private float Mod(float a, float b)
+        //{
+        //    return a%b;
+        //}
+
+        //private float Div(float a, float b)
+        //{
+        //    return a/b;
+        //}
+
+        //private float Mult(float a, float b)
+        //{
+        //    return a*b;
+        //}
+
+        //private float Subtract(float a, float b)
+        //{
+        //    return a-b;
+        //}
+
+        //private float Add(float a, float b)
+        //{
+        //    return a+b;
+        //}
+    }
+}
