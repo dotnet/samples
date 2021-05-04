62dc3219b4593bce88145ad5be5f34b217bb1193
ToraskarB <bhaktitoraskar26@gmail.com>
2021-05-04 09:18:08 +0530

    CS_Demo_Serialization/

---
+++ CS_Demo_Serialization/CS_Demo_Serialization/DemoGraphics/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Demo_Serialization.DemoGraphics
+{
+    [Serializable]
+    public class Person
+    {
+        public int Id { get; set; }//prop double tab
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; } 
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age 
+        {
+            get
+            {
+                var age = new DateTime(DateTime.Now.Subtract(DateOfBirth).Ticks).Year - 1;
+                return age;
+            }
+                
+        }
+
+
+    }
+}
---
+++ CS_Demo_Serialization/CS_Demo_Serialization/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS_Demo_Serialization.DemoGraphics;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace CS_Demo_Serialization
+{
+    class Program
+    {
+
+        static void Binary_Serialize()
+        {
+            Person p = new Person()
+            {
+                Id = 101,
+                FirstName = "James",
+                LastName = "John",
+                MiddleName = " Herry",
+                DateOfBirth = DateTime.Parse("20-Apr-19978")
+            };
+            FileStream fs = new FileStream("PersonData.bin", FileMode.Create, FileAccess.Write);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fs, p);
+            fs.Close();
+            fs.Dispose();
+        }
+
+            static void Main (string[] args)
+            {
+                Binary_Serialize();
+
+            Console.WriteLine(" Binary Serialization CCompleted Sucessfully");
+                Console.ReadKey();
+            }
+
+            
+        
+    }
+}
