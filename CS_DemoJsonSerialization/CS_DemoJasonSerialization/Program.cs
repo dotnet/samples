750a3a64fe0cd9b5f308a65b6432ab3a6e75d092
Ananya Sinha <ansinha27@gmail.com>
2021-05-04 09:15:02 +0530

    Added Json sample and local functions sample c#7.0

---
+++ CS_DemoJsonSerialization/CS_DemoJasonSerialization/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CS_DemoJasonSerialization
+{
+
+    class Person
+    {
+        public int  Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+    }
+    class Program
+    {
+        static void JsonSerialize()
+        {
+            Person p = new Person()
+            {
+                Id = 101,
+                Name = "James",
+                DateOfBirth = DateTime.Parse("20-Apr-1976")
+        };
+            string personData = JsonConvert.SerializeObject(p);
+            Console.WriteLine($"Json Serialized Person : {personData}");
+
+        }
+
+        static void JsonDeserialize()
+        {
+            // Person p = JsonConvert.DeserializeObject<Person>("{'Id' : 101, 'Name' : 'James', 'DateOfBirth':'1976-04-20T00:00:00'}");
+            Person p = JsonConvert.DeserializeObject<Person>("{'Id' : 101, 'Name' : 'James', 'DateOfBirth':'1976-04-20T00:00:00'}");
+            Console.WriteLine($"Id : {p.Id}, Name : {p.Name}, DateOfBirth : {p.DateOfBirth}");
+        }
+        static void Main(string[] args)
+        {
+            JsonSerialize();
+            Console.WriteLine("\nSerialization completed....");
+            JsonDeserialize();
+            Console.ReadKey();
+        }
+    }
+}
