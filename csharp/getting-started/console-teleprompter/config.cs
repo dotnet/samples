662ca903620e730b0c7dc7c8fbefd4fc5dbc615b
Tom Dykstra <tdykstra@microsoft.com>
2021-10-27 11:35:36 -0700

    update to .net 6

--- csharp/getting-started/console-teleprompter/config.cs
+++ csharp/getting-started/console-teleprompter/config.cs
@@ -1,27 +1,27 @@
 ﻿using static System.Math;
 
-namespace TeleprompterConsole
+namespace TeleprompterConsole;
+
+internal class TelePrompterConfig
 {
-    internal class TelePrompterConfig
+    private object lockHandle = new object();
+    public int DelayInMilliseconds { get; private set; } = 200;
+
+    public void UpdateDelay(int increment) // negative to speed up
     {
-        private object lockHandle = new object();
-        public int DelayInMilliseconds { get; private set; } = 200;
-
-        public void UpdateDelay(int increment) // negative to speed up
+        var newDelay = Min(DelayInMilliseconds + increment, 1000);
+        newDelay = Max(newDelay, 20);
+        lock (lockHandle)
         {
-            var newDelay = Min(DelayInMilliseconds + increment, 1000);
-            newDelay = Max(newDelay, 20);
-            lock (lockHandle)
-            {
-                DelayInMilliseconds = newDelay;
-            }
-        }
-
-        public bool Done { get; private set; }
-
-        public void SetDone()
-        {
-            Done = true;
+            DelayInMilliseconds = newDelay;
         }
     }
+
+    public bool Done { get; private set; }
+
+    public void SetDone()
+    {
+        Done = true;
+    }
 }
+
