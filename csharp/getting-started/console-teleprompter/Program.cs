1be2bf3fc6b11d7b29a25b2cdefcd10b7741388b
Tom Dykstra <tdykstra@microsoft.com>
2021-11-01 08:30:53 -0700

    update to .net 6 (#4839)

--- csharp/getting-started/console-teleprompter/Program.cs
+++ csharp/getting-started/console-teleprompter/Program.cs
@@ -1,79 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
+﻿namespace TeleprompterConsole;
 
-namespace TeleprompterConsole
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            RunTeleprompter().Wait();
-        }
+        RunTeleprompter().Wait();
+    }
 
-        private static async Task RunTeleprompter()
-        {
-            var config = new TelePrompterConfig();
-            var displayTask = ShowTeleprompter(config);
+    private static async Task RunTeleprompter()
+    {
+        var config = new TelePrompterConfig();
+        var displayTask = ShowTeleprompter(config);
 
-            var speedTask = GetInput(config);
-            await Task.WhenAny(displayTask, speedTask);
-        }
+        var speedTask = GetInput(config);
+        await Task.WhenAny(displayTask, speedTask);
+    }
 
-        private static async Task ShowTeleprompter(TelePrompterConfig config)
+    private static async Task ShowTeleprompter(TelePrompterConfig config)
+    {
+        var words = ReadFrom("sampleQuotes.txt");
+        foreach (var word in words)
         {
-            var words = ReadFrom("sampleQuotes.txt");
-            foreach (var word in words)
+            Console.Write(word);
+            if (!string.IsNullOrWhiteSpace(word))
             {
-                Console.Write(word);
-                if (!string.IsNullOrWhiteSpace(word))
-                {
-                    await Task.Delay(config.DelayInMilliseconds);
-                }
+                await Task.Delay(config.DelayInMilliseconds);
             }
-            config.SetDone();
         }
+        config.SetDone();
+    }
 
-        private static async Task GetInput(TelePrompterConfig config)
+    private static async Task GetInput(TelePrompterConfig config)
+    {
+        Action work = () =>
         {
-            Action work = () =>
+            do
             {
-                do
-                {
-                    var key = Console.ReadKey(true);
-                    if (key.KeyChar == '>')
-                        config.UpdateDelay(-10);
-                    else if (key.KeyChar == '<')
-                        config.UpdateDelay(10);
-                    else if (key.KeyChar == 'X' || key.KeyChar == 'x')
-                        config.SetDone();
-                } while (!config.Done);
-            };
-            await Task.Run(work);
-        }
-        static IEnumerable<string> ReadFrom(string file)
+                var key = Console.ReadKey(true);
+                if (key.KeyChar == '>')
+                    config.UpdateDelay(-10);
+                else if (key.KeyChar == '<')
+                    config.UpdateDelay(10);
+                else if (key.KeyChar == 'X' || key.KeyChar == 'x')
+                    config.SetDone();
+            } while (!config.Done);
+        };
+        await Task.Run(work);
+    }
+    static IEnumerable<string> ReadFrom(string file)
+    {
+        string? line;
+        using (var reader = File.OpenText(file))
         {
-            string line;
-            using (var reader = File.OpenText(file))
+            while ((line = reader.ReadLine()) != null)
             {
-                while ((line = reader.ReadLine()) != null)
+                var words = line.Split(' ');
+                var lineLength = 0;
+                foreach (var word in words)
                 {
-                    var words = line.Split(' ');
-                    var lineLength = 0;
-                    foreach (var word in words)
+                    yield return word + " ";
+                    lineLength += word.Length + 1;
+                    if (lineLength > 70)
                     {
-                        yield return word + " ";
-                        lineLength += word.Length + 1;
-                        if (lineLength > 70)
-                        {
-                            yield return Environment.NewLine;
-                            lineLength = 0;
-                        }
+                        yield return Environment.NewLine;
+                        lineLength = 0;
                     }
-                    yield return Environment.NewLine;
                 }
+                yield return Environment.NewLine;
             }
         }
     }
 }
+
