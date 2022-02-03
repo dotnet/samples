7a3e333fa508cb55236b5ec0553fca4cd52e8e4d
Andy De George (adegeo) <adegeo@microsoft.com>
2022-02-03 11:48:31 -0800

    Add code created via templates tutorial

---
+++ core/tutorials/cli-templates-create-item-template/templates/extensions/CommonExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static string Reverse(this string value)
+        {
+            var tempArray = value.ToCharArray();
+            Array.Reverse(tempArray);
+            return new string(tempArray);
+        }
+    }
+}
