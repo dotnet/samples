c142acb01975bdaf1800510cab973905efa7edb4
farukonfly <farukonfly@outlook.com>
2022-06-07 23:07:48 +0800

    An example of adding a parent class reference to a child class object

--- csharp/versioning/override/Program.cs
+++ csharp/versioning/override/Program.cs
@@ -23,9 +23,21 @@ class Program
     {
         MyBaseClass b = new MyBaseClass();
         MyDerivedClass d = new MyDerivedClass();
+        MyBaseClass bPointd = new MyDerivedClass();
 
-        Console.WriteLine("Base Method One: {0}", b.MethodOne());
-        Console.WriteLine("Derived Method One: {0}", d.MethodOne());
+        Console.WriteLine("Base Method One: {0}", b.MethodOne());  //Base Method One: Method One
+        Console.WriteLine("Derived Method One: {0}", d.MethodOne()); //Derived Method One: Derived Method One
+        
+            // public class MyDerivedClass : MyBaseClass
+            // {
+            //     public /* override */ string MethodOne()
+            //     {
+            //         return "Derived Method One";
+            //     }
+            // } 
+        // If this method doesn't have the override keyword,
+        // Console.WriteLine("Base Ponit Derive:{0}",bPointd.MethodOne()); //Base Ponit Derive:Method One
+        Console.WriteLine("Base Ponit Derive:{0}",bPointd.MethodOne()); //Base Ponit Derive:Derived Method One
     }
     #endregion
 }
