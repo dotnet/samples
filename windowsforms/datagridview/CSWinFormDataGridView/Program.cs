6a099ae3a4b6cf81dd174713f142e4de6cf52c52
David Pine <david.pine@microsoft.com>
2023-01-03 10:46:21 -0600

    Use upgrade-assistant on a few bits

--- windowsforms/datagridview/CSWinFormDataGridView/Program.cs
+++ windowsforms/datagridview/CSWinFormDataGridView/Program.cs
@@ -14,6 +14,7 @@ namespace CSWinFormDataGridView
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
 
             Application.Run(new CustomDataGridViewColumn.MainForm());
