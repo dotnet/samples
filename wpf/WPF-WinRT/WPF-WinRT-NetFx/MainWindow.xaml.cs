5831723de333ee240855450ccae44ee8f3f07fef
David Pine <david.pine@microsoft.com>
2023-01-04 11:40:36 -0600

    Adjust build bits

--- wpf/WPF-WinRT/WPF-WinRT-NetFx/MainWindow.xaml.cs
+++ wpf/WPF-WinRT/WPF-WinRT-NetFx/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPF_WinRT_NetFx
 {
@@ -27,8 +14,10 @@ namespace WPF_WinRT_NetFx
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+#pragma warning disable CA1416 // Validate platform compatibility
             label1.Content = new Windows.Devices.Input.TouchCapabilities().TouchPresent == 1 ?
                                     "Touch Present" : "Touch not present";
+#pragma warning restore CA1416 // Validate platform compatibility
         }
     }
 }
