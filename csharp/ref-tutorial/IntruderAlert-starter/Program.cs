bf695dcbebe6fb36420099e944b31fce384f4914
Bill Wagner <wiwagn@microsoft.com>
2023-01-23 15:20:39 -0500

    Add ref safety sample.
    This sample has the starter app and finished application for the tutorial added in dotnet/docs#32997
    
    The tutorial shows the reader how to analyze when your application is allocating memory unnecessarily, and how you can use the profiler tools and the C# language `ref` safety features to improve performance.

---
+++ csharp/ref-tutorial/IntruderAlert-starter/Program.cs
@@ -0,0 +1,38 @@
+﻿using IntruderAlert;
+
+Console.WriteLine("Press <return> to start simulation");
+Console.ReadLine();
+var room = new Room("gallery");
+var r = new Random();
+
+int counter = 0;
+
+room.TakeMeasurements(
+    m =>
+    {
+        Console.WriteLine(room.Debounce);
+        Console.WriteLine(room.Average);
+        Console.WriteLine();
+        counter++;
+        return counter < 20000;
+    });
+
+counter = 0;
+room.TakeMeasurements(
+    m =>
+    {
+        Console.WriteLine(room.Debounce);
+        Console.WriteLine(room.Average);
+        room.Intruders += (room.Intruders, r.Next(5)) switch
+        {
+            ( > 0, 0) => -1,
+            ( < 3, 1) => 1,
+            _ => 0
+        };
+
+        Console.WriteLine($"Current intruders: {room.Intruders}");
+        Console.WriteLine($"Calculated intruder risk: {room.RiskStatus}");
+        Console.WriteLine();
+        counter++;
+        return counter < 200000;
+    });
