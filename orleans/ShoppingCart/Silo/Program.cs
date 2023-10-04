9abff9c336fb13a2b5e71a53c81710951a027b17
Sidney Andrews <sidandrews@microsoft.com>
2023-10-04 13:20:28 -0400

    Revamped Program.cs to match Orleans quickstart

--- orleans/ShoppingCart/Silo/Program.cs
+++ orleans/ShoppingCart/Silo/Program.cs
@@ -1,43 +1,63 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT License.
 
-await Host.CreateDefaultBuilder(args)
-    .UseOrleans(
-        (context, builder) =>
-        {
-            if (context.HostingEnvironment.IsDevelopment())
-            {
-                builder.UseLocalhostClustering()
-                    .AddMemoryGrainStorage("shopping-cart")
-                    .AddStartupTask<SeedProductStoreTask>();
-            }
-            else
-            {
-                var endpointAddress =
-                    IPAddress.Parse(context.Configuration["WEBSITE_PRIVATE_IP"] ?? "");
-                var strPorts =
-                    (context.Configuration["WEBSITE_PRIVATE_PORTS"] ?? "").Split(',');
-                if (strPorts.Length < 2)
-                    throw new Exception("Insufficient private ports configured.");
-                var (siloPort, gatewayPort) =
-                    (int.Parse(strPorts[0]), int.Parse(strPorts[1]));
-                var connectionString =
-                    context.Configuration["ORLEANS_AZURE_STORAGE_CONNECTION_STRING"];
+var builder = WebApplication.CreateBuilder(args);
 
-                builder
-                    .ConfigureEndpoints(endpointAddress, siloPort, gatewayPort)
-                    .Configure<ClusterOptions>(
-                        options =>
-                        {
-                            options.ClusterId = "ShoppingCartCluster";
-                            options.ServiceId = nameof(ShoppingCartService);
-                        }).UseAzureStorageClustering(                    
-                    options => options.ConfigureTableServiceClient(connectionString));
-                builder.AddAzureTableGrainStorage(
-                    "shopping-cart",                    
-                    options => options.ConfigureTableServiceClient(connectionString));
-            }
-        })
-    .ConfigureWebHostDefaults(
-        webBuilder => webBuilder.UseStartup<Startup>())
-    .RunConsoleAsync();
+builder.Services.AddMudServices();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<ShoppingCartService>();
+builder.Services.AddSingleton<InventoryService>();
+builder.Services.AddSingleton<ProductService>();
+builder.Services.AddScoped<ComponentStateChangedObserver>();
+builder.Services.AddSingleton<ToastService>();
+builder.Services.AddLocalStorageServices();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Host.UseOrleans((_, builder) =>
+    {
+        builder
+            .UseLocalhostClustering()
+            .AddMemoryGrainStorage("shopping-cart")
+            .AddStartupTask<SeedProductStoreTask>();
+    });
+}
+else
+{
+    builder.Host.UseOrleans((context, builder) =>
+    {
+        var connectionString = context.Configuration["ORLEANS_AZURE_COSMOS_DB_CONNECTION_STRING"]!;
+
+        builder.Configure<ClusterOptions>(options =>
+        {
+            options.ClusterId = "ShoppingCartCluster";
+            options.ServiceId = nameof(ShoppingCartService);
+        });
+
+        builder
+            .UseCosmosClustering(o => o.ConfigureCosmosClient(connectionString))
+            .AddCosmosGrainStorage("shopping-cart", o => o.ConfigureCosmosClient(connectionString));
+    });
+}
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+await app.RunAsync();
