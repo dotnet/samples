2d0bcddd4a43f616e01d92924dbc088723679528
Cam Soper <cam.soper@microsoft.com>
2024-01-19 14:57:52 -0600

    Adding ft232h samples

---
+++ iot/ft232h/ft232h.i2c/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Device.I2c;
+using Iot.Device.Bmxx80;
+using Iot.Device.Bmxx80.PowerMode;
+using Iot.Device.Ft232H;
+using Iot.Device.FtCommon;
+
+var devices = FtCommon.GetDevices();
+var ft232h = new Ft232HDevice(devices[0]);
+var i2cSettings = new I2cConnectionSettings(0, Bme280.SecondaryI2cAddress);
+
+using var i2cDevice = ft232h.CreateI2cDevice(i2cSettings);
+using var bme280 = new Bme280(i2cDevice);
+
+int measurementTime = bme280.GetMeasurementDuration();
+
+while (true)
+{
+    Console.Clear();
+
+    bme280.SetPowerMode(Bmx280PowerMode.Forced);
+    Thread.Sleep(measurementTime);
+
+    bme280.TryReadTemperature(out var tempValue);
+    bme280.TryReadPressure(out var preValue);
+    bme280.TryReadHumidity(out var humValue);
+    bme280.TryReadAltitude(out var altValue);
+
+    Console.WriteLine($"Temperature: {tempValue.DegreesCelsius:0.#}\u00B0C");
+    Console.WriteLine($"Pressure: {preValue.Hectopascals:#.##} hPa");
+    Console.WriteLine($"Relative humidity: {humValue.Percent:#.##}%");
+    Console.WriteLine($"Estimated altitude: {altValue.Meters:#} m");
+
+    Thread.Sleep(1000);
+}
---
+++ iot/ft232h/ft232h.spi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Device.Gpio;
+using System.Device.Spi;
+using Iot.Device.Adc;
+using Iot.Device.Ft232H;
+using Iot.Device.FtCommon;
+
+var devices = FtCommon.GetDevices();
+var ft232h = new Ft232HDevice(devices[0]);
+var hardwareSpiSettings = new SpiConnectionSettings(0, 3) { ClockFrequency = 1_000_000, DataBitLength = 8, ChipSelectLineActiveState = PinValue.Low };
+using SpiDevice spi = ft232h.CreateSpiDevice(hardwareSpiSettings);
+using var mcp = new Mcp3008(spi);
+while (true)
+{
+    Console.Clear();
+    double value = mcp.Read(0);
+    Console.WriteLine($"{value}");
+    Console.WriteLine($"{Math.Round(value/10.23, 1)}%");
+    Thread.Sleep(500);
+}
