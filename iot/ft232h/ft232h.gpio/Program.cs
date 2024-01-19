c5400b0eff18cc8b18aee85c9804f345754db92d
Cam Soper <cam.soper@microsoft.com>
2024-01-19 15:09:24 -0600

    Adding ft232h samples (#6583)

---
+++ iot/ft232h/ft232h.gpio/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Device.Gpio;
+using Iot.Device.Ft232H;
+using Iot.Device.FtCommon;
+
+Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
+
+var ft232h = new Ft232HDevice(FtCommon.GetDevices()[0]);
+var controller = ft232h.CreateGpioController();
+
+int pin = Ft232HDevice.GetPinNumberFromString("D7");
+controller.OpenPin(pin, PinMode.Output);
+bool ledOn = true;
+while (true)
+{
+    controller.Write(pin, ledOn ? PinValue.High : PinValue.Low);
+    Thread.Sleep(1000);
+    ledOn = !ledOn;
+}
