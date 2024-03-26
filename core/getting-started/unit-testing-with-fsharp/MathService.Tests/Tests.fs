0bc25b49aba232d75835921525c0694073008c5b
Daniel Tsvetkov <rextor92@gmail.com>
2024-03-27 00:10:48 +0200

    Chores
    updated all projects in the getting-started folder to .NET 8 for consistency
    Removed obsolete example in the F# tests

--- core/getting-started/unit-testing-with-fsharp/MathService.Tests/Tests.fs
+++ core/getting-started/unit-testing-with-fsharp/MathService.Tests/Tests.fs
@@ -6,14 +6,6 @@ open Xunit
 open MathService
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
-
-//[<Fact>]
-//let ``Fail every time`` () = Assert.True(false)
-
-
-[<Fact>]
 let ``Sequence of Evens returns empty collection`` () =
     let expected = Seq.empty<int>
     let actual = MyMath.squaresOfOdds [2; 4; 6; 8; 10]
