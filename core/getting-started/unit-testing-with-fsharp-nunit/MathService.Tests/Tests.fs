26d9f1acde3e192a88a32ce99f37ecccf4ac3430
Daniel Tsvetkov <rextor92@gmail.com>
2024-03-27 00:27:39 +0200

    Moved change to proper NUnit test

--- core/getting-started/unit-testing-with-fsharp-nunit/MathService.Tests/Tests.fs
+++ core/getting-started/unit-testing-with-fsharp-nunit/MathService.Tests/Tests.fs
@@ -6,14 +6,6 @@ open NUnit.Framework
 open MathService
 
 [<Test>]
-let ``My test`` () =
-    Assert.True(true)
-
-//[<Test>]
-//let ``Fail every time`` () = Assert.True(false)
-
-
-[<Test>]
 let ``Sequence of Evens returns empty collection`` () =
     let expected = Seq.empty<int>
     let actual = MyMath.squaresOfOdds [2; 4; 6; 8; 10]
