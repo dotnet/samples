beb44d20199342622afb0cac6377020ddb41df2d
Andy De George (adegeo) <adegeo@microsoft.com>
2024-04-09 16:21:43 -0700

    add net 8 sample

---
+++ windowsforms/matching-game/net8-windows/cs/MatchingGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
