03b665ba4714d7b1b527c04d83f92c982a3529d0
verdie-g <gregoire.verdier@gmail.com>
2025-05-18 15:13:35 -0400

    Update logging

--- core/diagnostics/DiagnosticScenarios/Program.cs
+++ core/diagnostics/DiagnosticScenarios/Program.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 var app = builder.Build();
