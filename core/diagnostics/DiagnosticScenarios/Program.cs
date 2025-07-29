6aaf6357442295a8f0c858d26ce86a38f29ec021
Grégoire <gregoire.verdier@gmail.com>
2025-07-29 21:43:14 +0200

    Upgrade DiagnosticScenarios to .NET 9 (#7054)
    * Upgrade DiagnosticScenarios to .NET 9
    
    * Update logging

--- core/diagnostics/DiagnosticScenarios/Program.cs
+++ core/diagnostics/DiagnosticScenarios/Program.cs
@@ -1,26 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 
-namespace DiagnosticScenarios
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var app = builder.Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
