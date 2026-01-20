e68142a99083fb664059cd5580e0835eb17ebe45
Ivan Pehsterski <peshterskiivan@gmail.com>
2026-01-20 05:43:47 +0200

    Add tests in Calculator app in orleans

---
+++ orleans/SimpleCalculator/SimpleCalculator.Test/MethodsTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace SimpleCalculator.Test;
+
+public class Tests
+{
+    [Test]
+    public void SumMethod_ShouldReturnCorrectValue()
+    {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.Sum(4, 5);
+
+        Assert.That(result, Is.EqualTo(9));
+    }
+
+    [Test]
+    public void SubstractMethod_ShouldReturnCorrectValue()
+    {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.Substract(4, 5);
+
+        Assert.That(result, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void DevideMethod_ShouldReturnCorrectValue()
+    {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.Devide(4, 5);
+
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void MultiplyMethod_ShouldReturnCorrectValue()
+    {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.Multiply(4, 5);
+
+        Assert.That(result, Is.EqualTo(20));
+    }
+}
+
